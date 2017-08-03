8b2b78e871c91287018779c29689929db0bf35a4
Carlos Villuendas <carlosvillu@gmail.com>
2017-08-03 14:03:28 +0200

    fix(sui-studio): search for component.js just in case
    ISSUES CLOSED: #76

--- packages/sui-studio/src/components/documentation/try-require.js
+++ packages/sui-studio/src/components/documentation/try-require.js
@@ -4,18 +4,30 @@ const reqComponentsReadme =
   require.context(`bundle-loader?lazy!raw-loader!${__BASE_DIR__}/components`, true, /^\.\/\w+\/\w+\/README\.md?/)
 
 // https://webpack.github.io/docs/loaders.html#loader-order
-const reqComponentsSrc =
+const reqComponentsSrcIndex =
   require.context(`!!bundle-loader?lazy!raw-loader!${__BASE_DIR__}/components`, true, /^\.\/\w+\/\w+\/src\/index\.jsx?/)
+
+const reqComponentsSrcComponent =
+  require.context(`!!bundle-loader?lazy!raw-loader!${__BASE_DIR__}/components`, true, /^\.\/\w+\/\w+\/src\/component\.jsx?/)
 
 const tryRequire = ({category, component}) => {
   const src = new Promise(resolve => {
     require.ensure([], () => {
       let bundler
-      try {
-        bundler = reqComponentsSrc(`./${category}/${component}/src/index.js`)
-      } catch (e) {
-        bundler = reqComponentsSrc(`./${category}/${component}/src/index.jsx`)
+
+      if (!bundler && reqComponentsSrcComponent.keys().includes(`./${category}/${component}/src/component.js`)) {
+        bundler = reqComponentsSrcComponent(`./${category}/${component}/src/component.js`)
       }
+      if (!bundler && reqComponentsSrcComponent.keys().includes(`./${category}/${component}/src/component.jsx`)) {
+        bundler = reqComponentsSrcComponent(`./${category}/${component}/src/component.jsx`)
+      }
+      if (!bundler && reqComponentsSrcIndex.keys().includes(`./${category}/${component}/src/index.js`)) {
+        bundler = reqComponentsSrcIndex(`./${category}/${component}/src/index.js`)
+      }
+      if (!bundler && reqComponentsSrcIndex.keys().includes(`./${category}/${component}/src/index.jsx`)) {
+        bundler = reqComponentsSrcIndex(`./${category}/${component}/src/index.jsx`)
+      }
+
       bundler(src => resolve(src))
     })
   })
