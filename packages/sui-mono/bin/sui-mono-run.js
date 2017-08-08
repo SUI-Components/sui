c774e2334ee89bcaa249c802c34feb00228b9524
Ignacio Tolstoy <ignacio.tolstoy@scmspain.com>
2017-08-08 14:23:14 +0200

    feat(sui-mono): add commands for link and run

--- packages/sui-mono/bin/sui-mono-run.js
+++ packages/sui-mono/bin/sui-mono-run.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+/* eslint no-console:0 */
+
 const path = require('path')
 const program = require('commander')
 const config = require('../src/config')
@@ -5,7 +8,20 @@ const { serialSpawn } = require('@schibstedspain/sui-helpers/cli')
 const PACKAGES_DIR = path.join(process.cwd(), config.getPackagesFolder())
 const cwds = config.getScopes().map(pkg => path.join(PACKAGES_DIR, pkg))
 
-program.parse(process.argv)
+program
+  .on('--help', () => {
+    console.log('  Description:')
+    console.log('')
+    console.log('    Runs the given command on all the packages')
+    console.log('')
+    console.log('  Examples:')
+    console.log('')
+    console.log('    $ sui-mono run npm i')
+    console.log('    $ sui-mono --help')
+    console.log('    $ sui-mono -h')
+    console.log('')
+  })
+  .parse(process.argv)
 
 serialSpawn(cwds.map(getTaskArray))
   .then(code => process.exit(code))
