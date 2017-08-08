6e600c554fbde49997d8d02301c1ae46ef7310af
Miguel Ángel Durán <miguel@miduga.es>
2017-08-08 14:54:00 +0200

    fix(sui-hoc): wrong way to construct childContextTypes

--- packages/sui-hoc/src/withContext.js
+++ packages/sui-hoc/src/withContext.js
@@ -2,7 +2,7 @@ import React, {Component, PropTypes} from 'react'
 
 export default context => Target => class extends Component {
   static displayName = `withContext(${Target.displayName})`
-  static childContextTypes = () => Object.keys(context).reduce((acc, key) => {
+  static childContextTypes = Object.keys(context).reduce((acc, key) => {
     acc[key] = PropTypes.object
     return acc
   }, {})
