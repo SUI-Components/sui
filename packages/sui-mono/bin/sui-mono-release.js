51260b65709e4e89de9a01938595773428f397c6
Ignacio Tolstoy <ignacio.tolstoy@scmspain.com>
2017-08-09 08:46:22 +0200

    release(sui-mono): v1.16.0

--- packages/sui-mono/bin/sui-mono-release.js
+++ packages/sui-mono/bin/sui-mono-release.js
@@ -49,7 +49,7 @@ const releaseEachPkg = ({pkg, code} = {}) => {
       ['npm', ['--no-git-tag-version', 'version', `${RELEASE_CODES[code]}`]],
       ['git', ['add', cwd]],
       ['git', ['commit -m "release(' + packageScope + '): v$(node -p -e "require(\'./package.json\')".version)"']],
-      ['git', ['tag -a ' + tagPrefix + '$(node -p -e "require(\'./package.json\')".version)" -m "v$(node -p -e \"require(\'./package.json\')".version)\""']], // eslint-disable-line no-useless-escape
+      ['git', ['tag -a ' + tagPrefix + '$(node -p -e "require(\'./package.json\')".version) -m \"v$(node -p -e "require(\'./package.json\')".version)\"']], // eslint-disable-line no-useless-escape
       !pkgInfo.private && ['npm', ['publish', `--access=${publishAccess}`]],
       ['git', ['push', 'origin', 'HEAD']]
     ].filter(Boolean)
