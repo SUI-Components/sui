3c8272291e3544f061f031a49b6d2316f6ca6b42
David Almeida (Soyun) <david.almeida@soyun.es>
2017-08-28 17:35:23 +0200

    feat(sui-studio): add alias set to now deploys
    Deploy can now be done to a fixed url
    
    ISSUES CLOSED: #92

--- packages/sui-studio/bin/sui-studio-deploy.js
+++ packages/sui-studio/bin/sui-studio-deploy.js
@@ -2,7 +2,13 @@
 /* eslint no-console:0 no-template-curly-in-string:0 */
 
 const program = require('commander')
+const clipboardy = require('clipboardy')
 const { getSpawnPromise } = require('@schibstedspain/sui-helpers/cli')
+const getDeployId = function (deployUrl, name) {
+  let re = new RegExp(`https?://(${name}-[A-Za-z0-9]+).now.sh`, 'g')
+  let result = re.exec(deployUrl)
+  return result && result[1]
+}
 
 program
   .usage('-n my-components')
@@ -10,7 +16,7 @@ program
   .on('--help', () => {
     console.log('  Description:')
     console.log('')
-    console.log('    Build current studio and deploy to new.sh')
+    console.log('  Deploy current build of the studio to now.sh')
     console.log('')
     console.log('  Examples:')
     console.log('')
@@ -28,9 +34,23 @@ if (typeof program.name !== 'string') {
 const deployName = program.name
 
 getSpawnPromise('now', ['rm', deployName, '--yes', '-t ${NOW_TOKEN}'])
-  .catch(() => {}) // This will return error on the first deploy
+  .catch(() => {}) // To bypass now rm error on the first deploy
   .then(() => getSpawnPromise(
     'now', ['./public', '--name=' + deployName, '--static', '-t ${NOW_TOKEN}']
   ))
+  // Obtain deployment url copied to clipboard by now
+  .then(res => clipboardy.read())
+  // Parse deployment name to make the alias point to it
+  .then((deployUrl) => {
+    let deployId = getDeployId(deployUrl, deployName)
+    if (deployId) {
+      return getSpawnPromise(
+        'now', ['alias', deployId, deployName, '-t ${NOW_TOKEN}']
+      )
+    } else {
+      console.error('Deploy crashed for ' + deployName)
+      return 1
+    }
+  })
   .then(code => process.exit(code))
   .catch(code => process.exit(code))
