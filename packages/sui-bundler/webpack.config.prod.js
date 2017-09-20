11c37ac36725462db0a22973853764a29b0db4cd
David Almeida <davidbarna@gmail.com>
2017-09-20 08:59:53 +0200

    fix(sui-bundler): replace references to sui-studio by s-ui/studio

--- packages/sui-bundler/webpack.config.prod.js
+++ packages/sui-bundler/webpack.config.prod.js
@@ -133,7 +133,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules(?!\/@s-ui\/sui-studio\/src)/,
+        exclude: /node_modules(?!\/@s-ui\/studio\/src)/,
         loader: 'babel-loader',
         query: {
           presets: ['sui']
