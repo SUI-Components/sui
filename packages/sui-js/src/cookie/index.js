f50f39aef856141e8d8d3bee00cc03e07aefe397
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2018-01-03 14:04:07 +0100

    feat(sui-js): add getjson method

--- packages/sui-js/src/cookie/index.js
+++ packages/sui-js/src/cookie/index.js
@@ -3,11 +3,13 @@ import jsCookie from 'js-cookie'
 
 const parse = (cookie) => parseCookie(cookie)
 const get = (key) => jsCookie.get(key)
+const getJSON = (key) => jsCookie.getJSON(key)
 const set = (key, val) => jsCookie.set(key, val)
 
 const cookie = {
   parse,
   get,
+  getJSON,
   set
 }
 
