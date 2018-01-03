31879e48c7cf45cfdbc6132a3d8cfc1ecfc7a5e5
Miguel Ángel Durán <miguel@miduga.es>
2018-01-03 14:15:38 +0100

    feat(sui-lint): allow shortCircuit and ternaries for non unused expressions

--- packages/sui-lint/eslintrc.js
+++ packages/sui-lint/eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'chai-friendly'
   ],
   'rules': {
-    'chai-friendly/no-unused-expressions': 2,
+    'chai-friendly/no-unused-expressions': ['error', { 'allowShortCircuit': true, 'allowTernary': true }],
     'no-console': 'warn',
     'no-debugger': 'error',
     'no-nested-ternary': 'warn',
