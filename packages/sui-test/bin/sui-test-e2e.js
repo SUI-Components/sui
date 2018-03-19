caaa180666d9505a8d33409c4ace782c0ad0d4a5
davidbarna <davidbarna@gmail.com>
2018-03-19 14:55:43 +0100

    refactor(sui-test): prettier changes

--- packages/sui-test/bin/sui-test-e2e.js
+++ packages/sui-test/bin/sui-test-e2e.js
@@ -8,7 +8,8 @@ const { getSpawnPromise, showError } = require('@s-ui/helpers/cli')
 const CYPRESS_FOLDER_PATH = path.resolve(__dirname, 'cypress')
 const TESTS_FOLDER = process.cwd() + '/test-e2e'
 const SCREENSHOTS_FOLDER = process.cwd() + '/.tmp/test-e2e/screenshots'
-const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 Chrome/65.0.3325.146 Safari/537.36'
+const DEFAULT_USER_AGENT =
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 Chrome/65.0.3325.146 Safari/537.36'
 const HELP_MESSAGE = `
   Description:
     Run end-to-end tests with Cypress
@@ -17,14 +18,24 @@ const HELP_MESSAGE = `
     $ sui-test e2e --baseUrl=http://www.github.com
     $ sui-test e2e --baseUrl=http://www.github.com --gui`
 
-const objectToCommaString = obj => Object.keys(obj)
-  .map(key => `${key}='${obj[key]}'`)
-  .join(',')
+const objectToCommaString = obj =>
+  Object.keys(obj)
+    .map(key => `${key}='${obj[key]}'`)
+    .join(',')
 
 program
-  .option('-B, --baseUrl <baseUrl>', 'URL of the site to execute tests (in ./test-e2e/) on.')
-  .option('-S, --screenshotsOnError', 'Take screenshots of page on any failure.')
-  .option('-U, --userAgentAppend <userAgentAppend>', 'Append string to UserAgent header.')
+  .option(
+    '-B, --baseUrl <baseUrl>',
+    'URL of the site to execute tests (in ./test-e2e/) on.'
+  )
+  .option(
+    '-S, --screenshotsOnError',
+    'Take screenshots of page on any failure.'
+  )
+  .option(
+    '-U, --userAgentAppend <userAgentAppend>',
+    'Append string to UserAgent header.'
+  )
   .option('-G, --gui', 'Run the tests in GUI mode.')
   .on('--help', () => console.log(HELP_MESSAGE))
   .parse(process.argv)
@@ -41,11 +52,8 @@ if (screenshotsOnError) {
   cypressConfig.screenshotsFolder = SCREENSHOTS_FOLDER
 }
 
-getSpawnPromise(
-  require.resolve('cypress/bin/cypress'),
-  [
-    gui ? 'open' : 'run',
-    '--config=' + objectToCommaString(cypressConfig),
-    '--project=' + CYPRESS_FOLDER_PATH
-  ]
-).catch(showError)
+getSpawnPromise(require.resolve('cypress/bin/cypress'), [
+  gui ? 'open' : 'run',
+  '--config=' + objectToCommaString(cypressConfig),
+  '--project=' + CYPRESS_FOLDER_PATH
+]).catch(showError)
