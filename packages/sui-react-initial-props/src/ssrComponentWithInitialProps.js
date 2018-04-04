f77caf24bede6a056f79a3057dafc56fa6a971dc
Carlos Villuendas <carlosvillu@gmail.com>
2018-04-04 19:36:42 +0200

    feat(sui-react-initial-props): option getInitialProps in ssr too

--- packages/sui-react-initial-props/src/ssrComponentWithInitialProps.js
+++ packages/sui-react-initial-props/src/ssrComponentWithInitialProps.js
@@ -2,19 +2,25 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import withContext from '@s-ui/hoc/lib/withContext'
 
-export default function ssrComponentWithInitialProps ({ Target, context, renderProps }) {
+const EMPTY_FUNC = async () => ({})
+
+export default function ssrComponentWithInitialProps ({
+  Target,
+  context,
+  renderProps
+}) {
   // get the page with the initialProps
-  const pageComponent = renderProps.components[renderProps.components.length - 1]
+  const pageComponent =
+    renderProps.components[renderProps.components.length - 1]
   // use the getInitialProps from the page to retrieve the props to initialize
-  return pageComponent
-    .getInitialProps(context)
-    .then(initialProps => {
-      // create the App component with the context, the initialProps and the Target Component
-      const App = withContext({ ...context, initialProps })(Target)
-      // render our whole application with the needed props and get the html string
-      const reactString = ReactDOMServer.renderToString(
-        <App {...renderProps} initialProps={initialProps} />
-      )
-      return { initialProps, reactString }
-    })
+  const getInitialProps = pageComponent.getInitialProps || EMPTY_FUNC
+  return getInitialProps(context).then(initialProps => {
+    // create the App component with the context, the initialProps and the Target Component
+    const App = withContext({ ...context, initialProps })(Target)
+    // render our whole application with the needed props and get the html string
+    const reactString = ReactDOMServer.renderToString(
+      <App {...renderProps} initialProps={initialProps} />
+    )
+    return { initialProps, reactString }
+  })
 }
