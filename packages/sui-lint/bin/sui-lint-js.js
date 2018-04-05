052b4de5d7662fa58e678cbeaaf75a26da7baf26
davidbarna <davidbarna@gmail.com>
2018-04-05 20:08:41 +0200

    feat(sui-lint): ignore by default .gitignore file patterns

--- packages/sui-lint/bin/sui-lint-js.js
+++ packages/sui-lint/bin/sui-lint-js.js
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
 
-const {executeLintingCommand} = require('../src/helpers')
+const {
+  executeLintingCommand,
+  getFileLinesAsArray,
+  getIgnorePatterns
+} = require('../src/helpers')
 const BIN_PATH = require.resolve('eslint/bin/eslint')
 const CONFIG_PATH = require.resolve('../eslintrc.js')
+const IGNORE_PATTERNS = ['lib', 'dist', 'public', 'node_modules']
+const GIT_IGNORE_PATH = `${process.cwd()}/.gitignore`
+
+const patterns = IGNORE_PATTERNS.concat(getFileLinesAsArray(GIT_IGNORE_PATH))
 
 executeLintingCommand(BIN_PATH, [
-  '-c', CONFIG_PATH,
-  '--ext', 'js',
-  '--ext', 'jsx',
-  '--ignore-pattern', 'lib',
-  '--ignore-pattern', 'dist',
-  '--ignore-pattern', 'public',
-  '--ignore-pattern', 'node_modules',
+  `-c ${CONFIG_PATH}`,
+  '--ext js',
+  '--ext jsx',
+  ...getIgnorePatterns(patterns),
   './'
 ])
--- packages/sui-lint/src/helpers.js
+++ packages/sui-lint/src/helpers.js
@@ -7,16 +7,44 @@
  * @return {ChildProcess}
  */
 function executeLintingCommand (binPath, args) {
-  const {spawn} = require('child_process')
-  const [,, ...processArgs] = process.argv
+  const { spawn } = require('child_process')
+  const [, , ...processArgs] = process.argv
 
   if (processArgs.find(arg => arg === '-c')) {
     console.log('[sui-lint] Dont use your own config file. Remove `-c` flag')
     process.exit(1)
   }
 
-  return spawn(binPath, args.concat(processArgs), { shell: true, stdio: 'inherit' })
-    .on('exit', code => process.exit(code))
+  return spawn(binPath, args.concat(processArgs), {
+    shell: true,
+    stdio: 'inherit'
+  }).on('exit', code => process.exit(code))
+}
+
+/**
+ * Get lines of a text file as an array of strings
+ * @param {String} path
+ * @returns {Array<String>}
+ */
+const getFileLinesAsArray = path => {
+  const { existsSync, readFileSync } = require('fs')
+  return existsSync(path)
+    ? readFileSync(path, 'utf8')
+      .toString()
+      .split('\n')
+      .filter(Boolean)
+    : []
+}
+
+/**
+ * Get eslint --ignore-pattern patterns options
+ * @param {Array<String>} patterns
+ * @returns {Array<String>} ['--ignore-pattern', 'folder/', ...]
+ */
+const getIgnorePatterns = patterns => {
+  return patterns.filter(Boolean).map(pattern => `--ignore-pattern ${pattern}`)
 }
 
 exports.executeLintingCommand = executeLintingCommand
+exports.getFileLinesAsArray = getFileLinesAsArray
+exports.getIgnorePatterns = getIgnorePatterns
