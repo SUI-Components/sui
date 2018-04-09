216fa614b3344c84466779422a33d7abf8d195d1
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-04-09 18:23:35 +0200

    feat(sui-bundler): adapt production configuration to be as development
    BREAKING CHANGE:
    
    Stop supporting a way to load fonts, images and other files on your code

--- packages/sui-bundler/webpack.config.prod.js
+++ packages/sui-bundler/webpack.config.prod.js
@@ -139,50 +139,12 @@ module.exports = {
         }
       },
       {
-        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
-        loader: 'url-loader?name=[name].[ext]'
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=[name].[ext]'
-      },
-      {
-        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=[name].[ext]'
-      },
-      {
-        test: /\.svg(\?v=\d+.\d+.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml&name=[name].[ext]'
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/i,
-        loader: 'file-loader?name=[name].[hash:6].[ext]'
-      },
-      {
-        test: /\.ico$/,
-        loader: 'file-loader?name=[name].[ext]'
-      },
-      {
         test: /(\.css|\.scss)$/,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => {
-                return [
-                  require('autoprefixer')
-                ]
-              }
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              importer: require('node-sass-json-importer')
-            }
-          }
+          'postcss-loader',
+          'sass-loader'
         ]
       }
     ]
