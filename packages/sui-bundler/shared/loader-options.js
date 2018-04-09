5b6c358a7a68b731c7d827165e630d48453f0e49
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-04-09 18:23:03 +0200

    feat(sui-bundler): return loaderOptionsPlugin to be used

---
+++ packages/sui-bundler/shared/loader-options.js
@@ -0,0 +1,13 @@
+// Contains loaders options to avoid duplication between
+// webpack prod and dev configs
+
+const jsonImporter = require('node-sass-json-importer')
+
+module.exports = {
+  'sass-loader': {
+    importer: jsonImporter
+  },
+  'postcss-loader': {
+    plugins: (loader) => [require('autoprefixer')()]
+  }
+}
