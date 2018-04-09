689d13521a2702a142f11895e933d5fc43625459
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-04-09 18:24:53 +0200

    refactor(sui-studio): complete refactor about how we load files for studio

--- packages/sui-studio/src/components/demo/try-require.js
+++ packages/sui-studio/src/components/demo/try-require.js
@@ -2,16 +2,13 @@
 
 const reqComponentsSrc =
   require.context(`bundle-loader?lazy!${__BASE_DIR__}/components`, true, /^\.\/\w+\/\w+\/src\/index\.jsx?/)
-const reqComponentsPkg =
-  require.context(`bundle-loader?${__BASE_DIR__}/components`, true, /^\.\/\w+\/\w+\/package\.json?/)
-const reqComponentsPlayGround =
-  require.context(`bundle-loader?lazy!raw-loader!${__BASE_DIR__}/demo`, true, /^.*\/playground/)
-const reqContextPlayGround =
-  require.context(`bundle-loader?lazy-loader!${__BASE_DIR__}/demo`, true, /^.*\/context\.js/)
-const reqRouterPlayGround =
-  require.context(`bundle-loader?lazy-loader!${__BASE_DIR__}/demo`, true, /^.*\/routes\.js/)
-const reqEventsPlayGround =
-  require.context(`bundle-loader?lazy-loader!${__BASE_DIR__}/demo`, true, /^.*\/events\.js/)
+
+const requireFile = async ({ defaultValue, importFile }) => {
+  const file = await importFile().catch(_ => defaultValue)
+  return typeof file === 'undefined'
+    ? Promise.reject(new Error('Error requiring file'))
+    : file.default
+}
 
 const tryRequire = async ({category, component}) => {
   const exports = new Promise(resolve => {
@@ -26,61 +23,28 @@ const tryRequire = async ({category, component}) => {
     })
   })
 
-  const pkg = new Promise(resolve => {
-    require.ensure([], () => {
-      try {
-        const bundler = reqComponentsPkg(`./${category}/${component}/package.json`)
-        bundler(pkg => resolve(pkg))
-      } catch (e) {
-        return resolve({dependencies: {}})
-      }
-    })
+  const pkg = requireFile({
+    importFile: () => import(`${__BASE_DIR__}/components/${category}/${component}/package.json`),
+    defaultValue: { dependencies: {} }
   })
 
-  const playground = new Promise((resolve, reject) => {
-    require.ensure([], () => {
-      try {
-        const bundler = reqComponentsPlayGround(`./${category}/${component}/playground`)
-        bundler(playground => resolve(playground))
-      } catch (e) {
-        return reject(e)
-      }
-    })
+  const playground = requireFile({
+    importFile: () => import(`!raw-loader!${__BASE_DIR__}/demo/${category}/${component}/playground`)
   })
 
-  // const context = new Promise(resolve => {
-  //   require.ensure([], () => {
-  //     try {
-  //       const bundler = reqContextPlayGround(`./${category}/${component}/context.js`)
-  //       bundler(context => resolve(context))
-  //     } catch (e) {
-  //       return resolve(false)
-  //     }
-  //   })
-  // })
-
-  const context = (await import(`${__BASE_DIR__}/demo/${category}/${component}/context.js`)).default
-
-  const routes = new Promise(resolve => {
-    require.ensure([], () => {
-      try {
-        const bundler = reqRouterPlayGround(`./${category}/${component}/routes.js`)
-        bundler(routes => resolve(routes))
-      } catch (e) {
-        return resolve(false)
-      }
-    })
+  const context = requireFile({
+    defaultValue: false,
+    importFile: () => import(`${__BASE_DIR__}/demo/${category}/${component}/context.js`)
   })
 
-  const events = new Promise(resolve => {
-    require.ensure([], () => {
-      try {
-        const bundler = reqEventsPlayGround(`./${category}/${component}/events.js`)
-        bundler(events => resolve(events))
-      } catch (e) {
-        return resolve(false)
-      }
-    })
+  const routes = requireFile({
+    defaultValue: false,
+    importFile: () => import(`${__BASE_DIR__}/demo/${category}/${component}/routes.js`)
+  })
+
+  const events = requireFile({
+    defaultValue: false,
+    importFile: () => import(`${__BASE_DIR__}/demo/${category}/${component}/events.js`)
   })
 
   return Promise.all([exports, playground, context, routes, events, pkg])
