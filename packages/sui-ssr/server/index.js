ce6b2c31e38e1348191e37b973916e4a6aac753f
Carlos Villuendas <carlosvillu@gmail.com>
2018-04-12 10:12:05 +0200

    feat(sui-ssr): add very basic health check end point

--- packages/sui-ssr/server/index.js
+++ packages/sui-ssr/server/index.js
@@ -7,6 +7,10 @@ const { PORT = 3000 } = process.env
 app.use(express.static('statics'))
 app.use(express.static('public', { index: false }))
 
+app.get('/_health', (req, res) =>
+  res.status(200).json({ uptime: process.uptime() })
+)
+
 app.get('*', ssr)
 
 app.listen(PORT, () => console.log(`Server up & runnig port ${PORT}`))
