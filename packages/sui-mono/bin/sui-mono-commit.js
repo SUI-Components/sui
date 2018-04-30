0157d0f4a7385f65afb5765a30a72890bb04ab25
Miguel Ángel Durán <miduga@gmail.com>
2018-04-30 16:17:37 +0200

    feat(sui-mono): better error handling when no files staged for commiting

--- packages/sui-mono/bin/sui-mono-commit.js
+++ packages/sui-mono/bin/sui-mono-commit.js
@@ -1,11 +1,58 @@
 /* eslint no-console:0 */
 const path = require('path')
+const {exec} = require('child_process')
 const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap
 
-bootstrap({
-  debug: false,
-  cliPath: path.join(process.cwd(), 'node_modules/commitizen'),
-  config: {
-    path: './node_modules/@s-ui/cz'
+/**
+ * Get a list of the modified files by the user
+ * @param {boolean} checkIfStaged Determine if we should change if the modified file is staged
+ */
+function getDiffedFiles ({ checkIfStaged = false } = {}) {
+  return new Promise((resolve, reject) => {
+    let command = 'git diff --name-only'
+    if (checkIfStaged) command += ' --cached'
+    exec(command, (err, stdout) => {
+      if (err) return reject(err)
+      const output = stdout || ''
+
+      resolve({
+        hasFiles: output.trim().length > 0,
+        files: output
+      })
+    })
+  })
+}
+
+/**
+ * Check if the user has stagged files
+ */
+async function checkHasStagedFiles () {
+  const staged = await getDiffedFiles({ checkIfStaged: true })
+  return staged.hasFiles
+}
+
+/**
+ * Start the commiting process, making some verifications to avoid further problems for the user
+ */
+async function initCommit () {
+  const hasFiles = await checkHasStagedFiles()
+  if (hasFiles === false) {
+    console.log('No files added to staging! Did you forget to run git add?\n')
+    const modified = await getDiffedFiles()
+    if (modified.hasFiles) {
+      console.log('Showing the files that you could add:')
+      console.log(modified.files)
+    }
+    return
   }
-})
+
+  bootstrap({
+    debug: false,
+    cliPath: path.join(process.cwd(), 'node_modules/commitizen'),
+    config: {
+      path: './node_modules/@s-ui/cz'
+    }
+  })
+}
+
+initCommit()
