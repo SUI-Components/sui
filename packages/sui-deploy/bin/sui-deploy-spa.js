1ff876024b83164125416901e06bb5c3e4c04d69
davidbarna <davidbarna@gmail.com>
2018-05-16 12:53:01 +0200

    fix(sui-deploy): upgrade warning

--- packages/sui-deploy/bin/sui-deploy-spa.js
+++ packages/sui-deploy/bin/sui-deploy-spa.js
@@ -3,10 +3,13 @@
 
 const program = require('commander')
 const NowClient = require('now-client')
-const { getSpawnPromise } = require('@s-ui/helpers/cli')
-const {writeFile, removeFile} = require('@s-ui/helpers/file')
+const { getSpawnPromise, showWarning } = require('@s-ui/helpers/cli')
+const { writeFile, removeFile } = require('@s-ui/helpers/file')
 const DEFAULT_FOLDER = './public'
-const writePackageJson = (name) => writeFile(pkgFilePath, `{
+const writePackageJson = name =>
+  writeFile(
+    pkgFilePath,
+    `{
   "name": "@sui-deploy/${name}",
   "scripts": {
     "start": "serve . --single"
@@ -14,7 +17,8 @@ const writePackageJson = (name) => writeFile(pkgFilePath, `{
   "dependencies": {
     "serve": "latest"
   }
-}`)
+}`
+  )
 
 program
   .usage(`[options] <name> [folder]`)
@@ -43,19 +47,36 @@ const nowTokenOption = '-t $NOW_TOKEN'
 const now = new NowClient(process.env.NOW_TOKEN)
 const pkgFilePath = buildFolder + '/package.json'
 
+showWarning(`Your are currently using a deprecated version of sui-deploy.`)
+showWarning(
+  `Please upgrade to version 2 or superior as soon as possible for SECURE deployments.`
+)
+
 getSpawnPromise('now', ['rm', deployName, '--yes', nowTokenOption])
   .catch(() => {}) // To bypass now rm error on the first deploy
   .then(() => writePackageJson(deployName)) // Add package.json for SPA server
-  .then(() => getSpawnPromise(
-    'now', [DEFAULT_FOLDER, '--name=' + deployName, '--npm', nowTokenOption]
-  ))
+  .then(() =>
+    getSpawnPromise('now', [
+      DEFAULT_FOLDER,
+      '--name=' + deployName,
+      '--npm',
+      nowTokenOption
+    ])
+  )
   .then(() => removeFile(pkgFilePath)) // Remove package.json only used by now.sh
   .then(() => now.getDeployments())
   .then(deployments => deployments.filter(d => d.name === deployName)[0].url)
   // Parse deployment name to make the alias point to it
-  .then((deployId) => deployId
-    ? getSpawnPromise('now', ['alias', deployId, deployName, nowTokenOption])
-    : Promise.reject(new Error('Deploy crashed for ' + deployName))
+  .then(
+    deployId =>
+      deployId
+        ? getSpawnPromise('now', [
+          'alias',
+          deployId,
+          deployName,
+          nowTokenOption
+        ])
+        : Promise.reject(new Error('Deploy crashed for ' + deployName))
   )
   .catch(err => {
     console.log(err.message || err)
