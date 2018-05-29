295bd9b97b3a8f0060fd0b326d851bbcfb56f109
David Almeida <davidbarna@gmail.com>
2018-05-29 02:24:28 -0700

    fix(sui-studio): add require(path) where needed

--- packages/sui-studio/bin/sui-studio-clean-modules.js
+++ packages/sui-studio/bin/sui-studio-clean-modules.js
@@ -1,6 +1,7 @@
 /* eslint no-console:0 */
 
 const colors = require('colors')
+const path = require('path')
 const {remove} = require('fs-extra')
 const BASE_DIR = process.cwd()
 const cwds = require('./walker').componentsFullPath(BASE_DIR)
