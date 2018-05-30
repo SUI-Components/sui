f7ffe55f9fd8de9c682ac1905330713e246e12cb
davidbarna <davidbarna@gmail.com>
2018-05-30 12:17:03 +0200

    refactor(sui-studio-utils): format with new linting rules

--- packages/sui-studio-utils/src/i18n/index.js
+++ packages/sui-studio-utils/src/i18n/index.js
@@ -5,7 +5,7 @@ const DEFAULT_CULTURE = 'es-ES'
 const DEFAULT_CURRENCY = 'EUR'
 
 const initRosseta = (literals, config = {}) => {
-  const i18n = new Rosetta({ adapter: new Polyglot() })
+  const i18n = new Rosetta({adapter: new Polyglot()})
   i18n.languages = literals
   i18n.culture = config.culture || DEFAULT_CULTURE
   i18n.currency = config.currency || DEFAULT_CURRENCY
@@ -13,6 +13,7 @@ const initRosseta = (literals, config = {}) => {
   return i18n
 }
 
-export default ({ literalsUseCase, dictionary, config }) =>
-  literalsUseCase ? literalsUseCase.execute().then(literals => initRosseta(literals, config))
-    : new Promise((resolve) => resolve(initRosseta(dictionary, config)))
+export default ({literalsUseCase, dictionary, config}) =>
+  literalsUseCase
+    ? literalsUseCase.execute().then(literals => initRosseta(literals, config))
+    : new Promise(resolve => resolve(initRosseta(dictionary, config)))
