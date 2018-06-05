054a0da31245312eb76f956f448605c63f999f2f
Miguel Ángel Durán <miduga@gmail.com>
2018-06-05 15:42:09 +0200

    feat(sui-decorators): remove cache tracking functionality

--- packages/sui-decorators/src/decorators/cache/tracker/index.js
+++
@@ -1,8 +0,0 @@
-import isNode from '../../../helpers/isNode'
-import BrowserTracker from './BrowserTracker'
-import NodeTracker from './NodeTracker'
-
-// FIXME:
-// http://webpack.github.io/docs/configuration.html#resolve-packagealias
-// https://github.com/defunctzombie/package-browser-field-spec#replace-specific-files---advanced
-export default (isNode ? NodeTracker : BrowserTracker)
