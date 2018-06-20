1fbb54d07c8c2b8f94407dd0f2a27b07e0cf53cd
davidbarna <davidbarna@gmail.com>
2018-06-20 13:04:11 +0200

    fix(sui-mono): fix wrong release messages in windows

--- packages/sui-mono/bin/sui-mono-release.js
+++ packages/sui-mono/bin/sui-mono-release.js
@@ -74,15 +74,7 @@ const releaseEachPkg = ({pkg, code} = {}) => {
 
     let releaseCommands = [
       ['npm', ['--no-git-tag-version', 'version', `${RELEASE_CODES[code]}`]],
-      ['git', ['add', cwd]],
-      [
-        'git',
-        [
-          'commit -m "release(' +
-            packageScope +
-            '): v$(node -p -e "require(\'./package.json\')".version)"'
-        ]
-      ]
+      ['git', ['add', cwd]]
     ]
     let docCommands = [
       [suiMonoBinPath, ['changelog', cwd]],
@@ -96,9 +88,18 @@ const releaseEachPkg = ({pkg, code} = {}) => {
     ].filter(Boolean)
 
     serialSpawn(releaseCommands, {cwd})
+      .then(() => {
+        // Create release commit
+        const {version} = getPackageJson(cwd, true)
+        return serialSpawn(
+          [['git', [`commit -m "release(${packageScope}): v${version}"`]]],
+          {cwd}
+        )
+      })
       .then(() => serialSpawn(docCommands))
       .then(() => {
-        const {version} = getPackageJson(BASE_DIR)
+        // Create release tag
+        const {version} = getPackageJson(cwd)
         return serialSpawn(
           [['git', [`tag -a ${tagPrefix}${version} -m "v${version}"`]]],
           {cwd}
