ed30901871f5bdebffe1882c5f8c3090518ba42a
Carlos Villuendas <carlosvillu@gmail.com>
2018-08-10 11:26:06 +0200

    fix(sui-bundler): now sass is linked too

--- packages/sui-bundler/loaders/linkLoaderConfigBuilder.js
+++ packages/sui-bundler/loaders/linkLoaderConfigBuilder.js
@@ -1,8 +1,17 @@
 const path = require('path')
+const LoaderUniversalOptionsPlugin = require('../plugins/loader-options')
+const loadersOptions = require('../shared/loader-options')
 
 module.exports = ({config, packagesToLink}) => {
   if (packagesToLink.length === 0) {
     return config
+  }
+
+  const removePlugin = name => plugins => {
+    const pos = plugins
+      .map(p => p.constructor.toString())
+      .findIndex(string => string.match(name))
+    return [...plugins.slice(0, pos), ...plugins.slice(pos + 1)]
   }
 
   const entryPoints = packagesToLink.reduce((acc, packagePath) => {
@@ -13,22 +22,38 @@ module.exports = ({config, packagesToLink}) => {
 
   const nextConfig = {
     ...config,
+    plugins: [
+      ...removePlugin('LoaderUniversalOptionsPlugin')(config.plugins),
+      new LoaderUniversalOptionsPlugin({
+        ...loadersOptions,
+        'sass-loader': {
+          ...loadersOptions['sass-loader'],
+          importer: [
+            ...loadersOptions['sass-loader'].importer,
+            (url, prev) => {
+              if (Object.keys(entryPoints).find(pkg => url.match(pkg))) {
+                let [org, name] = url.split(/\//)
+                const pkg = [org.replace('~', ''), name].join('/')
+
+                const absoluteUrl = url.replace(
+                  new RegExp(`~?${pkg}(\\/lib)?`, 'g'),
+                  entryPoints[pkg]
+                )
+                return {file: absoluteUrl}
+              }
+              return null
+            }
+          ]
+        }
+      })
+    ],
     module: {
       ...config.module,
       rules: [
         ...config.module.rules,
         {
-          test: /(\.css|\.scss)$/,
-          use: {
-            loader: 'link-loader',
-            options: {
-              entryPoints
-            }
-          }
-        },
-        {
           test: /\.jsx?$/,
-          // TODO: That is crap, I need better options
+          // TODO: This is crappy, I need better options
           exclude: new RegExp(
             `node_modules(?!${path.sep}@s-ui${path.sep}studio(${
               path.sep
