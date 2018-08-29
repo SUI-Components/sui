ab40d77188b1e1bd1bfb91971acd630b29e08424
Miguel Ángel Durán <miduga@gmail.com>
2018-08-29 11:34:45 +0200

    feat(babel-preset-sui): move to Babel 7

--- packages/babel-preset-sui/lib/index.js
+++ packages/babel-preset-sui/lib/index.js
@@ -1,42 +1,52 @@
 const isInstalled = require('./is-installed')
 const cleanList = require('./clean-list')
 
-module.exports = {
-  'presets': cleanList([
-    ['env', {
-      'debug': false,
-      'targets': {
-        'node': '6.0.0',
-        'browsers': [
-          '> 0.25%',
-          'Firefox ESR',
-          'Safari >= 8',
-          'iOS >= 8',
-          'ie >= 11',
-          'not op_mini all'
-        ]
+module.exports = () => ({
+  presets: cleanList([
+    [
+      '@babel/preset-env',
+      {
+        debug: false,
+        loose: true,
+        targets: {
+          node: '6.0.0',
+          browsers: [
+            '> 0.25%',
+            'Firefox ESR',
+            'Safari >= 8',
+            'iOS >= 8',
+            'ie >= 11',
+            'not op_mini all'
+          ]
+        }
       }
-    }],
-    isInstalled(['preact', 'react'], 'babel-preset-react'),
-    isInstalled('flow-bin', 'babel-preset-flow')
+    ],
+    isInstalled(['preact', 'react'], '@babel/preset-react')
   ]),
-  'plugins': [
-    require('babel-plugin-transform-async-generator-functions'),
-    require('babel-plugin-transform-decorators-legacy').default,
-    require('babel-plugin-transform-class-properties'),
-    require('babel-plugin-transform-object-rest-spread'),
-    require('babel-plugin-transform-runtime'),
-    require('babel-plugin-syntax-dynamic-import'),
-    require('babel-plugin-transform-export-extensions'),
-    [require('babel-plugin-transform-react-remove-prop-types').default, {
-      mode: 'wrap'
-    }]
+  plugins: [
+    require('@babel/plugin-proposal-async-generator-functions'),
+    [require('@babel/plugin-proposal-decorators'), {legacy: true}],
+    [require('@babel/plugin-proposal-class-properties'), {loose: true}],
+    [
+      require('@babel/plugin-proposal-object-rest-spread'),
+      {loose: true, useBuiltIns: true}
+    ],
+    require('@babel/plugin-transform-runtime'),
+    require('@babel/plugin-syntax-dynamic-import'),
+    require('@babel/plugin-proposal-export-default-from'),
+    require('@babel/plugin-proposal-export-namespace-from'),
+    [
+      require('babel-plugin-transform-react-remove-prop-types').default,
+      {
+        mode: 'wrap'
+      }
+    ]
   ],
-  'env': {
-    'development': {
-      'plugins': cleanList([
+  env: {
+    development: {
+      plugins: cleanList([
         isInstalled(['preact', 'react'], 'react-hot-loader/babel')
       ])
     }
   }
-}
+})
