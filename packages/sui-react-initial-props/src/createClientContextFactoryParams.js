7617c7c4bf706effa581bcaa0030d61539b0807b
Beto Esposito <alberto.esposito@scmspain.com>
2018-08-30 17:58:27 +0200

    feat(sui-react-initial-props): add appConfig to context factory params

--- packages/sui-react-initial-props/src/createClientContextFactoryParams.js
+++ packages/sui-react-initial-props/src/createClientContextFactoryParams.js
@@ -1,5 +1,6 @@
 export default function createClientContextFactoryParams() {
   return {
+    appConfig: window.__APP_CONFIG__,
     cookies: document.cookie,
     isClient: true,
     pathName: window.location.pathname,
