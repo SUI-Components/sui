e7087ccec940010986436dda3422eee646f04b59
Miguel Ángel Durán <miduga@gmail.com>
2018-08-31 13:10:22 +0200

    feat(babel-preset-sui): transpile Webpack Dynamic Imports

--- packages/babel-preset-sui/lib/index.js
+++ packages/babel-preset-sui/lib/index.js
@@ -2,39 +2,46 @@ const isInstalled = require('./is-installed')
 const cleanList = require('./clean-list')
 
 module.exports = {
-  'presets': cleanList([
-    ['env', {
-      'debug': false,
-      'targets': {
-        'node': '6.0.0',
-        'browsers': [
-          '> 0.25%',
-          'Firefox ESR',
-          'Safari >= 8',
-          'iOS >= 8',
-          'ie >= 11',
-          'not op_mini all'
-        ]
+  presets: cleanList([
+    [
+      'env',
+      {
+        debug: false,
+        targets: {
+          node: '6.0.0',
+          browsers: [
+            '> 0.25%',
+            'Firefox ESR',
+            'Safari >= 8',
+            'iOS >= 8',
+            'ie >= 11',
+            'not op_mini all'
+          ]
+        }
       }
-    }],
+    ],
     isInstalled(['preact', 'react'], 'babel-preset-react'),
     isInstalled('flow-bin', 'babel-preset-flow')
   ]),
-  'plugins': [
+  plugins: [
     require('babel-plugin-transform-async-generator-functions'),
     require('babel-plugin-transform-decorators-legacy').default,
     require('babel-plugin-transform-class-properties'),
     require('babel-plugin-transform-object-rest-spread'),
     require('babel-plugin-transform-runtime'),
+    require('babel-plugin-dynamic-import-webpack'),
     require('babel-plugin-syntax-dynamic-import'),
     require('babel-plugin-transform-export-extensions'),
-    [require('babel-plugin-transform-react-remove-prop-types').default, {
-      mode: 'wrap'
-    }]
+    [
+      require('babel-plugin-transform-react-remove-prop-types').default,
+      {
+        mode: 'wrap'
+      }
+    ]
   ],
-  'env': {
-    'development': {
-      'plugins': cleanList([
+  env: {
+    development: {
+      plugins: cleanList([
         isInstalled(['preact', 'react'], 'react-hot-loader/babel')
       ])
     }
