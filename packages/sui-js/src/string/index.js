7741019b25675715390592a241a58ca3a98d1d56
Joan Claret <dpam23@gmail.com>
2018-10-02 17:43:24 +0200

    feat(string): code refactor, export parseQueryString instead of parseQueryUtility

--- packages/sui-js/src/string/index.js
+++ packages/sui-js/src/string/index.js
@@ -1,12 +1,4 @@
-import queryString from 'query-string'
+export {parse as parseQueryString} from 'query-string'
 export {fromSnakeToCamelCase, fromCamelToSnakeCase} from './snake-case'
 export {has as hasAccents, remove as removeAccents} from 'remove-accents'
-export {default as toKebabCase} from 'lodash.kebabcase'
-
-const parse = data => queryString.parse(data)
-const stringify = data => queryString.stringify(data)
-
-export const queryStringUtility = {
-  parse,
-  stringify
-}
+export toKebabCase from 'lodash.kebabcase'
