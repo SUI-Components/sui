2ea6397ae32779ee86a4236cf4962f7da97ffdd5
Miguel Ángel Durán <miduga@gmail.com>
2018-10-03 16:58:53 +0200

    feat(sui-studio): remove all about compliant in sui-studio as not longer necessary

--- packages/sui-studio/src/components/icons/index.js
+++ packages/sui-studio/src/components/icons/index.js
@@ -32,16 +32,3 @@ export const iconClose = (
     <path d="M0 0h24v24H0z" fill="none" />
   </svg>
 )
-
-export const iconStart = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="burlywood"
-    height="18"
-    viewBox="0 0 18 18"
-    width="18"
-  >
-    <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" />
-    <path d="M0 0h18v18H0z" fill="none" />
-  </svg>
-)
