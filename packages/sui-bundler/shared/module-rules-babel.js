6a152d832d0b9d39089d70dd1cf68cae1b15eea8
Miguel Ángel Durán <miduga@gmail.com>
2018-10-22 12:42:26 +0200

    feat(sui-bundler): update to new options from babel-loader

--- packages/sui-bundler/shared/module-rules-babel.js
+++ packages/sui-bundler/shared/module-rules-babel.js
@@ -3,9 +3,13 @@ const {sep} = require('path')
 module.exports = {
   test: /\.jsx?$/,
   exclude: new RegExp(`node_modules(?!${sep}@s-ui${sep}studio${sep}src)`),
-  loader: 'babel-loader',
-  query: {
-    babelrc: false,
-    presets: ['sui']
+  use: {
+    loader: 'babel-loader',
+    options: {
+      babelrc: false,
+      cacheDirectory: false,
+      compact: true,
+      presets: ['sui']
+    }
   }
 }
