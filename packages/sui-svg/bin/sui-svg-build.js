5eeee00deda92e47c108e3eca2ad0fcd5b2ffd04
Julian Gernun <julian.gernun@schibsted.com>
2018-11-06 11:14:44 +0100

    refactor(sui-svg): add shell env

--- packages/sui-svg/bin/sui-svg-build.js
+++ packages/sui-svg/bin/sui-svg-build.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* eslint no-console:0 */
 const Walker = require('walker')
 const svgr = require('@svgr/core').default
--- packages/sui-svg/bin/sui-svg-demo.js
+++ packages/sui-svg/bin/sui-svg-demo.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* eslint no-console:0 */
 const program = require('commander')
 const path = require('path')
