a5013ea8b1fcfa13ea2593fbfd91a2acbc6ea95e
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-12-13 09:51:37 +0100

    fix(sui-widget-embedder): fix wrong comment for CLI

--- packages/sui-widget-embedder/bin/sui-widget-embedder.js
+++ packages/sui-widget-embedder/bin/sui-widget-embedder.js
@@ -12,7 +12,10 @@ program.command('dev', 'Start a development server for a page').alias('d')
 program.command('build', 'Build all statics by page').alias('b')
 
 program
-  .command('generate', 'Generate a new widget folder and main files')
+  .command(
+    'generate',
+    'Generate a new page folder and main files with your first widget'
+  )
   .alias('g')
 
 program.parse(process.argv)
