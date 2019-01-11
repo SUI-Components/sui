dc4fce1df2f315f2129a494d5b98bac153f2f7ee
JuanMa Garrido <juanma.garrido@gmail.com>
2019-01-11 13:04:54 +0100

    feat(sui-js): added helpers needed for several sui-components

---
+++ packages/sui-js/src/dom/index.js
@@ -0,0 +1,10 @@
+const getCurrentElementFocused = () => document.activeElement
+
+export const getFocusedItemIndex = domItems => {
+  const currentElementFocused = getCurrentElementFocused()
+  return domItems.reduce(
+    (focusedItemIndex, item, index) =>
+      item === currentElementFocused ? index : focusedItemIndex,
+    0
+  )
+}
