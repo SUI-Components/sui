61aea8cc68fa887ea2714c12f836a180fa1e8324
ignacio.calleja <ignacio.calleja@schibsted.com>
2019-01-15 13:04:07 +0100

    feat(sui-bundler): remove unnecessary comment from uglifyjs config

--- packages/sui-bundler/shared/uglify.js
+++ packages/sui-bundler/shared/uglify.js
@@ -3,5 +3,5 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = new UglifyJsPlugin({
   cache: true,
   parallel: true,
-  sourceMap: true // set to true if you want JS source maps
+  sourceMap: true
 })
