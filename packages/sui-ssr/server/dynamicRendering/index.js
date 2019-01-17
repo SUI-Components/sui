3ac5f693e6bb8a90b4fc925eb97358172ebe4475
Carlos Villuendas <carlosvillu@gmail.com>
2019-01-17 17:02:33 +0100

    refactor(sui-ssr): remove ternary operator

--- packages/sui-ssr/server/dynamicRendering/index.js
+++ packages/sui-ssr/server/dynamicRendering/index.js
@@ -6,12 +6,16 @@ const INDEX_HTML_PATH = path.join(process.cwd(), 'public', 'index.html')
 
 export default function dynamicRendering(fallback, dynamicsURLS = []) {
   return function middleware(req, resp, next) {
-    if (!dynamicsURLS.length) {
+    if (!dynamicsURLS.length || seoBotDetect(req)) {
       return fallback.call(this, req, resp, next)
     }
 
-    return seoBotDetect(req) ? fallback.call(this, req, resp, next) : // eslint-disable-line
-           dynamicsURLS.some(url => req.url.match(new RegExp(url))) ? fs.createReadStream(INDEX_HTML_PATH).pipe(resp) : // eslint-disable-line
-           fallback.call(this, req, resp, next) // eslint-disable-line
+    const enabledDynamicRendering = dynamicsURLS.some(url =>
+      req.url.match(new RegExp(url))
+    )
+
+    return enabledDynamicRendering
+      ? fs.createReadStream(INDEX_HTML_PATH).pipe(resp)
+      : fallback.call(this, req, resp, next)
   }
 }
