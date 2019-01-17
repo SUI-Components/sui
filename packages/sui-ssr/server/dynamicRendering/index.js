18aa35a096271eec8b3fb46f1dd930afbc3e4f1f
Carlos Villuendas <carlosvillu@gmail.com>
2019-01-17 16:40:21 +0100

    feat(sui-ssr): add Dynamic Rendering by pages

--- packages/sui-ssr/server/dynamicRendering/index.js
+++ packages/sui-ssr/server/dynamicRendering/index.js
@@ -2,17 +2,16 @@ import path from 'path'
 import fs from 'fs'
 import seoBotDetect from './seoBotDetect'
 
-const {DYNAMIC_RENDERING = false} = process.env
 const INDEX_HTML_PATH = path.join(process.cwd(), 'public', 'index.html')
 
-export default function dinamycRendering(fallback) {
+export default function dinamycRendering(fallback, dynamicsURLS = []) {
   return function middleware(req, resp, next) {
-    if (!DYNAMIC_RENDERING) {
+    if (!dynamicsURLS.length) {
       return fallback.call(this, req, resp, next)
     }
 
-    return seoBotDetect(req)
-      ? fallback.call(this, req, resp, next)
-      : fs.createReadStream(INDEX_HTML_PATH).pipe(resp)
+    return seoBotDetect(req) ? fallback.call(this, req, resp, next) : // eslint-disable-line
+           dynamicsURLS.some(url => req.url.match(new RegExp(url))) ? fs.createReadStream(INDEX_HTML_PATH).pipe(resp) : // eslint-disable-line
+           fallback.call(this, req, resp, next) // eslint-disable-line
   }
 }
