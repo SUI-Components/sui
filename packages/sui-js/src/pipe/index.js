4f0a3f310918ae5379c73568ced869f00ba7c114
Rafael Moral Pareja <rafael.moral@schibsted.com>
2019-02-14 10:43:43 +0100

    feat(sui-js): add async pipe fn

--- packages/sui-js/src/pipe/index.js
+++ packages/sui-js/src/pipe/index.js
@@ -1,3 +1,5 @@
 const pipe = (...funcs) => arg => funcs.reduce((acc, value) => value(acc), arg)
-
 export default pipe
+
+export const asyncPipe = (...funcs) => arg =>
+  funcs.reduce((acc, value) => acc.then(value), Promise.resolve(arg))
