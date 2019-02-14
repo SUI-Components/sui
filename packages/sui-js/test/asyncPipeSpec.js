6e811b354bf22483b6b7f0d00cac27d00564fb75
Rafael Moral Pareja <rafael.moral@schibsted.com>
2019-02-14 10:45:33 +0100

    feat(sui-js): add async pipe fn
    ISSUES CLOSED: #488

--- packages/sui-js/test/asyncPipeSpec.js
+++ packages/sui-js/test/asyncPipeSpec.js
@@ -12,10 +12,12 @@ describe('@s-ui/js', () => {
       const result = await asyncPipe(textToUpperCase, textToArray)(title)
       expect(result).to.be.an('array')
     })
+
     it('should be the same length as the title ', async () => {
       const result = await asyncPipe(textToUpperCase, textToArray)(title)
       expect(result).to.have.lengthOf(title.length)
     })
+
     it('should return the title in camelcase and array form ', async () => {
       const result = await asyncPipe(textToUpperCase, textToArray)(title)
       const expectedResult = ['S', 'C', 'H', 'I', 'B', 'S', 'T', 'E', 'D']
