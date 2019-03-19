d4ec207d1c72a54f8191098956cc50f25824d2c9
midudev <miduga@gmail.com>
2019-03-19 14:06:14 +0100

    chore(META): checkout to master and get latest changes

--- .travis-release.sh
+++ .travis-release.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# define names
 git config --global user.email "cloud-accounts@scmspain.com"
 git config --global user.name "sui-bot"
-
 # Remove existing "origin"
 git remote rm origin
 # Add new "origin" with access token in the git URL for authentication
 git remote add origin https://sui-bot:${GH_TOKEN}@github.com/SUI-Components/sui.git > /dev/null 2>&1
-
+# move to master before release
+git checkout master
+# get the latest master changes
+git pull origin master
 # release new packages
 npm run release
