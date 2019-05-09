742aa5d3aed2dbae142d45a16ff54cda95507fd7
Julian Gernun <julian.gernun@schibsted.com>
2019-05-09 14:43:21 +0200

    fix(sui-analytics): pass props to segment event

--- packages/sui-analytics/src/index.js
+++ packages/sui-analytics/src/index.js
@@ -1,11 +1,11 @@
 export default {
-  identify: args => {
-    window.analytics.identify(args)
+  identify: (...args) => {
+    window.analytics.identify(...args)
   },
-  reset: args => {
-    window.analytics.reset(args)
+  reset: (...args) => {
+    window.analytics.reset(...args)
   },
-  track: args => {
-    window.analytics.track(args)
+  track: (...args) => {
+    window.analytics.track(...args)
   }
 }
