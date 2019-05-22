ec3f21db1b3cbbf401c08d09c61595f1cc33a2b0
midudev <miduga@gmail.com>
2019-05-22 12:33:41 +0200

    feat(babel-preset-sui): work to new babel preset configuration to support custom targets and improve

--- packages/babel-preset-sui/src/index.js
+++ packages/babel-preset-sui/src/index.js
@@ -1,6 +1,25 @@
 const cleanList = require('./clean-list')
+const {
+  DEFAULT_LEGACY_TARGETS,
+  DEFAULT_MODERN_TARGETS,
+  DEFAULT_SERVER_TARGETS,
+  SELECTIVE_LOOSE_REACT_HOOKS
+} = require('./defaults')
+
+function getTargets({isModern, isServer, targets}) {
+  const {
+    legacy = DEFAULT_LEGACY_TARGETS,
+    modern = DEFAULT_MODERN_TARGETS,
+    server = DEFAULT_SERVER_TARGETS
+  } = targets
+
+  if (isServer) return server
+  if (isModern) return modern
+  return legacy
+}
 
 function plugins(api, opts = {}) {
+  const {isModern} = opts
   return cleanList([
     require('@babel/plugin-syntax-dynamic-import').default,
     require('@babel/plugin-syntax-export-default-from').default,
@@ -10,10 +29,10 @@ function plugins(api, opts = {}) {
     [
       require('babel-plugin-transform-react-remove-prop-types').default,
       {
-        wrap: true
+        removeImport: true
       }
     ],
-    [
+    !isModern && [
       require('@babel/plugin-proposal-object-rest-spread').default,
       {useBuiltIns: true} // asume Object.assign is available by browser or polyfill
     ],
@@ -21,43 +40,44 @@ function plugins(api, opts = {}) {
       require('@babel/plugin-transform-runtime').default,
       {
         corejs: false,
-        regenerator: true
+        useESModules: isModern
+      }
+    ],
+    [
+      require('@babel/plugin-transform-destructuring').default,
+      {
+        // Use loose mode for performance
+        loose: false,
+        selectiveLoose: SELECTIVE_LOOSE_REACT_HOOKS
       }
     ]
   ])
 }
 
 function presets(api, opts) {
+  const {isModern, isServer, targets} = opts
+
   return [
     [
       require('@babel/preset-env').default,
       {
-        debug: false,
+        corejs: 3,
+        debug: true, // remove true here
         ignoreBrowserslistConfig: true,
         loose: true,
         modules: false,
         // Exclude transforms that make all code slower
         exclude: ['transform-typeof-symbol'],
-        targets: {
-          node: '6.0.0',
-          browsers: [
-            '> 0.25%',
-            'Firefox ESR',
-            'Safari >= 8',
-            'iOS >= 8',
-            'ie >= 11',
-            'not op_mini all',
-            'not dead'
-          ]
-        },
-        useBuiltIns: false
+        targets: getTargets({isModern, isServer, targets}),
+        useBuiltIns: 'entry'
       }
     ],
     [require('@babel/preset-react').default, {useBuiltIns: true}]
   ]
 }
 
-module.exports = (api, opts) => ({
-  presets: presets(api, opts),
-  plugins: plugins(api, opts)
-})
+module.exports = (api, opts) =>
+  console.log('*******************', opts) || {
+    presets: presets(api, opts),
+    plugins: plugins(api, opts)
+  }
