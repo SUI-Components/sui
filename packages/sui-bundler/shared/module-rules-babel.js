8a3745a8197ec63ec97f0eb4eed01bb07c7dc40e
midudev <miduga@gmail.com>
2019-05-22 12:34:41 +0200

    feat(sui-bundler): transform babel rules to method to support different targets

--- packages/sui-bundler/shared/module-rules-babel.js
+++ packages/sui-bundler/shared/module-rules-babel.js
@@ -1,17 +1,27 @@
 const {sep} = require('path')
+const {config} = require('./shared')
 
-module.exports = {
+module.exports = ({isModern, isServer} = {}) => ({
   test: /\.jsx?$/,
-  exclude: new RegExp(`node_modules(?!${sep}@s-ui${sep}studio${sep}src)`),
+  exclude: isModern
+    ? undefined
+    : new RegExp(`node_modules(?!${sep}@s-ui${sep}studio${sep}src)`),
   use: [
     {
       loader: require.resolve('babel-loader'),
       options: {
         babelrc: false,
         compact: true,
-        presets: [require.resolve('babel-preset-sui')]
+        presets: [
+          [require.resolve('babel-preset-sui')],
+          {
+            isModern,
+            isServer,
+            targets: config.targets
+          }
+        ]
       }
     },
     require.resolve('source-map-loader')
   ]
-}
+})
