78a97d7ee6bb6b21a753b46c20a37d72ffb2361d
midudev <miduga@gmail.com>
2019-05-22 12:36:43 +0200

    feat(sui-bundler): support bundle analyzer to support both bundles

--- packages/sui-bundler/bin/sui-bundler-analyzer.js
+++ packages/sui-bundler/bin/sui-bundler-analyzer.js
@@ -5,25 +5,29 @@ const webpack = require('webpack')
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin')
 const chalk = require('chalk')
-const config = require('../webpack.config.prod')
+const configs = require('../webpack.config.prod')
 
 console.log('🔎  Bundler Analyzer')
 
 // Don't show ugly deprecation warnings that mess with the logging
 process.noDeprecation = true
 
-config.plugins.push(new BundleAnalyzerPlugin())
-config.plugins.push(
-  new DuplicatePackageCheckerPlugin({
-    // Also show module that is requiring each duplicate package
-    verbose: true,
-    // Emit errors instead of warnings
-    emitError: false
-  })
-)
+let analyzerPort = 8888
+
+configs.forEach(config => {
+  config.plugins.push(new BundleAnalyzerPlugin({analyzerPort: ++analyzerPort}))
+  config.plugins.push(
+    new DuplicatePackageCheckerPlugin({
+      // Also show module that is requiring each duplicate package
+      verbose: true,
+      // Emit errors instead of warnings
+      emitError: false
+    })
+  )
+})
 
 console.log(chalk.cyan('Building and analyzing...\n'))
-webpack(config).run((error, stats) => {
+webpack(configs).run((error, stats) => {
   if (error) {
     console.log(chalk.red('Error analyzing the build'))
     throw new Error(error)
