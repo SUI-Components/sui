7bc26daef14ccda53ca1a8f0976b0b8b9621c4eb
midudev <miduga@gmail.com>
2019-05-22 12:43:15 +0200

    fix(babel-preset-sui): add module.exports instead export

--- packages/babel-preset-sui/src/defaults.js
+++ packages/babel-preset-sui/src/defaults.js
@@ -1,28 +1,27 @@
-export const DEFAULT_LEGACY_TARGETS = {
-  chrome: '41',
-  ie: '11',
-  safari: '8',
-  firefox: 'esr',
-  ios: '8'
+module.exports = {
+  DEFAULT_LEGACY_TARGETS: {
+    chrome: '41',
+    ie: '11',
+    safari: '8',
+    firefox: 'esr',
+    ios: '8'
+  },
+  DEFAULT_MODERN_TARGETS: {
+    esmodules: true
+  },
+  DEFAULT_SERVER_TARGETS: {
+    node: '6.0.0'
+  },
+  SELECTIVE_LOOSE_REACT_HOOKS: [
+    'useState',
+    'useEffect',
+    'useContext',
+    'useReducer',
+    'useCallback',
+    'useMemo',
+    'useRef',
+    'useImperativeHandle',
+    'useLayoutEffect',
+    'useDebugValue'
+  ]
 }
-
-export const DEFAULT_MODERN_TARGETS = {
-  esmodules: true
-}
-
-export const DEFAULT_SERVER_TARGETS = {
-  node: '6.0.0'
-}
-
-export const SELECTIVE_LOOSE_REACT_HOOKS = [
-  'useState',
-  'useEffect',
-  'useContext',
-  'useReducer',
-  'useCallback',
-  'useMemo',
-  'useRef',
-  'useImperativeHandle',
-  'useLayoutEffect',
-  'useDebugValue'
-]
