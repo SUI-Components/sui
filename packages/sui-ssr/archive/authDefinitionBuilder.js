5187023febdbe1fdc38e7a56d65bfa0a9c63422b
Carlos Villuendas <carlosvillu@gmail.com>
2019-06-11 11:38:54 +0200

    fix(sui-ssr): remove console logs when generate the zip file

--- packages/sui-ssr/archive/authDefinitionBuilder.js
+++ packages/sui-ssr/archive/authDefinitionBuilder.js
@@ -1,14 +1,14 @@
 module.exports = ([username, password]) => {
-  console.log('')
-  console.log('->'.bold, ' AUTH'.blue.bold, 'option setted'.bold)
-  console.log(
-    '-> Web server will run over'.bold,
-    ' authentication 🔐'.blue.bold
-  )
-  console.log('')
-  console.log('         -> USERNAME:'.bold, username.cyan)
-  console.log('         -> PASSWORD:'.bold, password.replace(/(.?)/g, '*').cyan)
-  console.log('')
+  // console.log('')
+  // console.log('->'.bold, ' AUTH'.blue.bold, 'option setted'.bold)
+  // console.log(
+  //   '-> Web server will run over'.bold,
+  //   ' authentication 🔐'.blue.bold
+  // )
+  // console.log('')
+  // console.log('         -> USERNAME:'.bold, username.cyan)
+  // console.log('         -> PASSWORD:'.bold, password.replace(/(.?)/g, '*').cyan)
+  // console.log('')
 
   return `ENV AUTH_USERNAME ${username}\nENV AUTH_PASSWORD ${password}`
 }
