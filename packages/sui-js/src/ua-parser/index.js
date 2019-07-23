6831686b18ced52bd06936ea195b38e3adad4442
Emilio Manuel Villuendas Zambrana <emilio.villuendas@AFE081386.local>
2019-07-23 12:45:28 +0200

    add browser to ua-parser

--- packages/sui-js/src/ua-parser/index.js
+++ packages/sui-js/src/ua-parser/index.js
@@ -4,6 +4,7 @@ export const stats = userAgent => {
   const ua = bowser._detect(userAgent)
   return {
     isMobile: !!ua.mobile,
-    osName: ua.osname
+    osName: ua.osname,
+    bowser: ua.browser
   }
 }
