fad8f37986ada7ad7c3ede124cf2fac3257242eb
David G <david.garcia@scmspain.com>
2019-08-21 11:26:05 +0200

    feat(sui-bundler): remove dirname in regEx to get manifestURL

--- packages/sui-bundler/loaders/ExternalsManifestLoader.js
+++ packages/sui-bundler/loaders/ExternalsManifestLoader.js
@@ -34,13 +34,12 @@ async function externalsManifestLoader(source) {
   }
 
   try {
-    const dirname = path.dirname(manifestURL)
     const manifest = await getRemoteManifest(manifestURL)
     const entries = Object.entries(manifest)
     const nextSource = entries.reduce((acc, entry) => {
       const [dest, src] = entry
-      const regex = new RegExp(`${dirname}${dest}`, 'g')
-      return acc.replace(regex, `${dirname}${src}`)
+      const regex = new RegExp(`${dest}`, 'g')
+      return acc.replace(regex, `${src}`)
     }, source)
 
     cb(null, nextSource)
