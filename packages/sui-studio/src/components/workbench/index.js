cc1e3b8e7bd48e7b300003e3a3961b9931243e89
midudev <miduga@gmail.com>
2019-09-16 16:49:50 +0200

    feat(sui-studio): Show UX definition button only when needed

--- packages/sui-studio/src/components/workbench/index.js
+++ packages/sui-studio/src/components/workbench/index.js
@@ -1,12 +1,18 @@
+import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
-import React from 'react'
 import {Link} from 'react-router'
+
+import {FILES} from '../../constants'
+import {tryRequireMarkdown} from '../tryRequire'
 
 const TAB_CLASS = 'sui-StudioTabs-tab'
 const LINK_CLASS = 'sui-StudioTabs-link'
 const ACTIVE_CLASS = LINK_CLASS + '--active'
 
 export default function Workbench({children, params}) {
+  const [showUX, setShowUX] = useState(false)
+  const {category, component} = params
+
   const Tab = ({name, path}) => ( // eslint-disable-line
     <li className={TAB_CLASS}>
       <Link
@@ -19,7 +25,17 @@ export default function Workbench({children, params}) {
     </li>
   )
 
-  const {category, component} = params
+  useEffect(
+    function() {
+      // check if ux definition files exist to show the button
+      tryRequireMarkdown({category, component, file: FILES.UX_DEFINITION}).then(
+        content => {
+          setShowUX(Boolean(content))
+        }
+      )
+    },
+    [category, component]
+  )
 
   return (
     <div className="sui-StudioWorkbench">
@@ -29,7 +45,7 @@ export default function Workbench({children, params}) {
           <Tab name="Api" path="documentation/api" />
           <Tab name="Readme" path="documentation/readme" />
           <Tab name="Changelog" path="documentation/changelog" />
-          <Tab name="UX Definition" path="documentation/uxdef" />
+          {showUX && <Tab name="UX Definition" path="documentation/uxdef" />}
         </ul>
       </nav>
       <div className="sui-StudioWorkbench-content">{children}</div>
