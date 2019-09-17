a9bd22b3919d77ccc784ebf0a94b2b760860736a
JuanMa Garrido <juanma.garrido@gmail.com>
2019-09-17 10:09:21 +0200

    feat(sui-hoc): withSwitchValue

---
+++ packages/sui-hoc/src/withSwitchValue.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+
+const withSwitchValue = BaseComponent => {
+  const displayName = BaseComponent.displayName
+
+  const BaseComponentWithState = ({
+    value: valueFromProps = false,
+    onChange: onChangeFromProps,
+    ...props
+  }) => {
+    const [value, setValue] = useState(valueFromProps)
+
+    const onChange = e => {
+      const innerValue = !value
+      setValue(innerValue)
+      onChangeFromProps(e, {value: innerValue})
+    }
+
+    return (
+      <BaseComponent
+        {...props}
+        checked={value}
+        value={value}
+        onChange={onChange}
+      />
+    )
+  }
+
+  BaseComponentWithState.displayName = `withSwitchValue(${displayName})`
+
+  BaseComponentWithState.propTypes = {
+    /** value */
+    value: PropTypes.any,
+
+    /** onChange callback  */
+    onChange: PropTypes.func
+  }
+
+  BaseComponentWithState.defaultProps = {
+    onChange: () => {}
+  }
+
+  return BaseComponentWithState
+}
+
+export default withSwitchValue
