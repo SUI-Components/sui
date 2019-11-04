33cb455496fe0524dbab9cc5e11b80eb391cea1b
midudev <miduga@gmail.com>
2019-11-04 12:27:00 +0100

    feat(sui-svg): install needed dependencies, better readability, less dependencies
    BREAKING CHANGE:
    Now icons are wrapped with AtomIcon

--- packages/sui-svg/bin/sui-svg-build.js
+++ packages/sui-svg/bin/sui-svg-build.js
@@ -5,11 +5,13 @@ const svgr = require('@svgr/core').default
 const fs = require('fs-extra')
 const program = require('commander')
 const {join} = require('path')
-const template = require('../templates/pure-component')
 const toCamelCase = require('lodash.camelcase')
 const babel = require('@babel/core')
+const {getSpawnPromise} = require('@s-ui/helpers/cli')
 
+const template = require('../templates/icon-component')
 const BASE_DIR = process.cwd()
+const ATOM_ICON_VERSION = 1
 const LIB_FOLDER = join(BASE_DIR, '.', 'lib')
 const SVG_FOLDER = join(BASE_DIR, '.', 'src')
 
@@ -26,64 +28,58 @@ program
   })
   .parse(process.argv)
 
-const camelCase = function(fileName) {
+const camelCase = fileName => {
   const camelFile = toCamelCase(fileName)
   return `${camelFile[0].toUpperCase()}${camelFile.slice(1)}`
 }
 
-const getLibFile = function(file) {
+const getLibFile = file => {
   const [, rawPath, rawfileName] = file.match('^.*/src/(.+/)*(.*).svg$')
   const fileName = camelCase(rawfileName)
   const path = rawPath || ''
   return `${LIB_FOLDER}/${path}${fileName}.js`
 }
 
-function directoryExists(path) {
-  try {
-    return fs.statSync(path).isDirectory()
-  } catch (error) {
-    return false
-  }
+const getAllSrcSvgFiles = () => fg([`${SVG_FOLDER}/**/*.svg`])
+
+const transfomrSvgToReact = svg =>
+  svgr(svg, {
+    template,
+    expandProps: false,
+    removeTitle: true
+  })
+
+const transformCodeWithBabel = jsCode =>
+  babel.transformAsync(jsCode, {
+    presets: [require.resolve('babel-preset-sui')]
+  })
+
+const installNeededDependencies = () =>
+  getSpawnPromise('npm', [
+    'install',
+    `@s-ui/react-atom-icon@${ATOM_ICON_VERSION}`,
+    '--save-exact'
+  ])
+
+const copyStylesFile = () => {
+  fs.copy(
+    require.resolve('../templates/icon-styles.scss'),
+    `${LIB_FOLDER}/index.scss`
+  )
 }
 
-function ensureDirectoryExistence(dirname) {
-  if (directoryExists(dirname)) {
-    return true
-  }
-  fs.mkdirSync(dirname)
-}
-
-ensureDirectoryExistence(LIB_FOLDER)
-
-fg([`${SVG_FOLDER}/**/*.svg`]).then(entries =>
-  entries.forEach(file => {
-    console.log(`Converting ${file}`)
-    fs.readFile(file, (err, data) => {
-      if (err) throw err
-      svgr(
-        data,
-        {
-          template,
-          expandProps: false,
-          removeTitle: true
-        },
-        {componentName: 'SVGComponent'}
-      )
-        .then(jsCode =>
-          babel.transformAsync(jsCode, {
-            presets: ['sui']
-          })
-        )
-        .then(result => {
-          fs.outputFile(getLibFile(file), result.code, function(error) {
-            if (error) {
-              throw error
-            }
-          })
-        })
+fs.emptyDir(LIB_FOLDER)
+  .then(installNeededDependencies)
+  .then(getAllSrcSvgFiles)
+  .then(entries =>
+    entries.forEach(file => {
+      fs.readFile(file, 'utf8')
+        .then(transfomrSvgToReact)
+        .then(transformCodeWithBabel)
+        .then(result => fs.outputFile(getLibFile(file), result.code))
         .catch(error => {
           console.error(error)
         })
     })
-  })
-)
+  )
+  .then(copyStylesFile)
