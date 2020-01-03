280b72aa172fd72424df9ffdaea2ff41c95e82f0
midudev <miduga@gmail.com>
2020-01-03 14:04:43 +0100

    feat(sui-studio): move component to function

--- packages/sui-studio/workbench/src/components/Select/index.js
+++ packages/sui-studio/workbench/src/components/Select/index.js
@@ -1,41 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
-class Select extends React.PureComponent {
-  static propTypes = {
-    initValue: PropTypes.string,
-    label: PropTypes.string,
-    onChange: PropTypes.func.isRequired,
-    options: PropTypes.object.isRequired
+export default function Select({initValue, label, onChange, options}) {
+  const [value, setValue] = useState(initValue)
+
+  const handleChangeSelect = event => {
+    const {value} = event.target
+    setValue(value)
+    onChange(value)
   }
 
-  state = {value: this.props.initValue}
-
-  render() {
-    const {value} = this.state
-    const {options, label} = this.props
-    if (!Object.keys(options).length) {
-      return null
-    }
-    return (
-      <label className="Select">
-        <span className="Select-label">{label}</span>
-        <select value={value} onChange={this.handleChangeSelect}>
-          {Object.keys(options).map(option => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-      </label>
-    )
+  if (!Object.keys(options).length) {
+    return null
   }
 
-  handleChangeSelect = event => {
-    const nextValue = event.target.value
-    this.setState({value: nextValue})
-    this.props.onChange(nextValue)
-  }
+  return (
+    <label className="Select">
+      <span className="Select-label">{label}</span>
+      <select value={value} onChange={handleChangeSelect}>
+        {Object.keys(options).map(option => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </label>
+  )
 }
 
-export default Select
+Select.propTypes = {
+  initValue: PropTypes.string,
+  label: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.object.isRequired
+}
