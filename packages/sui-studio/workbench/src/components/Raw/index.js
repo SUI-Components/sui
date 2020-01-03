4bf27bca2816b717fd01ce01ad792a50802abf01
midudev <miduga@gmail.com>
2020-01-03 15:32:16 +0100

    feat(sui-studio): move Raw to function and remove not needed code and states

--- packages/sui-studio/workbench/src/components/Raw/index.js
+++ packages/sui-studio/workbench/src/components/Raw/index.js
@@ -23,74 +23,60 @@ try {
   playground = require('!raw-loader!demo/playground')
 } catch (e) {}
 
-const EMPTY = 0
 const nonDefault = removeDefaultContext(named)
 
-class Raw extends React.PureComponent {
-  static propTypes = {
-    actualContext: PropTypes.string,
-    actualStyle: PropTypes.string,
-    contexts: PropTypes.object,
-    demo: PropTypes.node,
-    demoStyles: PropTypes.string,
-    themes: PropTypes.object
-  }
+export default function Raw({
+  actualContext = 'default',
+  actualStyle = 'default',
+  contexts = {},
+  demo: DemoComponent,
+  demoStyles,
+  themes
+}) {
+  const context =
+    Object.keys(contexts).length && createContextByType(contexts, actualContext)
 
-  state = {
-    playground,
-    actualContext: this.props.actualContext || 'default',
-    actualStyle: this.props.actualStyle || 'default'
-  }
+  // check if is a normal component or it's wrapped with a React.memo method
+  const ComponentToRender = Component.type ? Component.type : Component
+  const Enhance = pipe(withContext(context, context))(ComponentToRender)
 
-  handleChangeCodeMirror = playground => this.setState({playground})
+  const EnhanceDemoComponent =
+    DemoComponent && pipe(withContext(context, context))(DemoComponent)
 
-  render() {
-    const {playground, actualContext, actualStyle} = this.state
-    const {contexts = {}, themes, demo: DemoComponent, demoStyles} = this.props
+  return (
+    <div className="Raw">
+      <Style>{themes[actualStyle]}</Style>
+      <Style>{demoStyles}</Style>
 
-    const context =
-      Object.keys(contexts).length !== EMPTY &&
-      createContextByType(contexts, actualContext)
-
-    // check if is a normal component or it's wrapped with a React.memo method
-    const ComponentToRender = Component.type ? Component.type : Component
-    const Enhance = pipe(withContext(context, context))(ComponentToRender)
-
-    const EnhanceDemoComponent =
-      DemoComponent && pipe(withContext(context, context))(DemoComponent)
-
-    return (
-      <div className="Raw">
-        <Style>{themes[actualStyle]}</Style>
-        <Style>{demoStyles}</Style>
-
-        <div className="Raw-center">
-          <When value={!EnhanceDemoComponent && playground}>
-            {() => (
-              <>
-                <Preview
-                  scope={{
-                    context,
-                    React,
-                    [`${cleanDisplayName(Enhance.displayName)}`]: Enhance,
-                    ...nonDefault
-                  }}
-                  code={playground}
-                />
-              </>
-            )}
-          </When>
-          <When value={EnhanceDemoComponent}>
-            {() => (
-              <SUIContext.Provider value={context}>
-                <EnhanceDemoComponent />
-              </SUIContext.Provider>
-            )}
-          </When>
-        </div>
+      <div className="Raw-center">
+        <When value={!EnhanceDemoComponent && playground}>
+          {() => (
+            <Preview
+              scope={{
+                context,
+                React,
+                [cleanDisplayName(Enhance.displayName)]: Enhance,
+                ...nonDefault
+              }}
+              code={playground}
+            />
+          )}
+        </When>
+        {EnhanceDemoComponent && (
+          <SUIContext.Provider value={context}>
+            <EnhanceDemoComponent />
+          </SUIContext.Provider>
+        )}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-export default Raw
+Raw.propTypes = {
+  actualContext: PropTypes.string,
+  actualStyle: PropTypes.string,
+  contexts: PropTypes.object,
+  demo: PropTypes.node,
+  demoStyles: PropTypes.string,
+  themes: PropTypes.object
+}
