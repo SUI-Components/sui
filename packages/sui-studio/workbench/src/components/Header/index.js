d100a90cd9911d6295c1064470f5d06da3858b18
midudev <miduga@gmail.com>
2020-01-03 13:56:16 +0100

    feat(sui-studio): add a friendly way to open the iframe

--- packages/sui-studio/workbench/src/components/Header/index.js
+++ packages/sui-studio/workbench/src/components/Header/index.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Header = ({children, componentID}) => (
+const Header = ({children, componentID, iframeSrc}) => (
   <header className="Header">
-    <h1 className="Header-title">{componentID}</h1>
+    <h1 className="Header-title">
+      {componentID}
+      <a
+        className="Header-link"
+        href={iframeSrc}
+        rel="noopener noreferrer"
+        target="_blank"
+        title="Open Iframe in a new window"
+      >
+        ↗️
+      </a>
+    </h1>
     <div className="Header-content">{children}</div>
   </header>
 )
 
 Header.propTypes = {
   children: PropTypes.array,
-  componentID: PropTypes.string
+  componentID: PropTypes.string,
+  iframeSrc: PropTypes.string
 }
+
 export default Header
