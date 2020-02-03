86ef5af32bd0ffacc2372f01e861e439c7305daf
Carlos Villuendas <carlosvillu@gmail.com>
2020-02-03 11:01:56 +0100

    feat(sui-studio): remove wrapper promise

--- packages/sui-studio/scripts/postinstall.js
+++ packages/sui-studio/scripts/postinstall.js
@@ -25,32 +25,26 @@ const showError = msg => {
   process.exit(1)
 }
 
-const writeFile = (path, body) => {
-  return new Promise((resolve, reject) => {
-    fse.outputFile(path, body, err => {
-      if (err) {
-        showError(`Fail modifying ${path}`)
-        reject(err)
-      } else {
-        console.log(`Modified ${path}`)
-        resolve()
-      }
+const writeFile = async (path, body) => {
+  return fse
+    .outputFile(path, body)
+    .then(() => {
+      console.log(`Modified ${path}`)
     })
-  })
+    .catch(() => {
+      showError(`Fail modifying ${path}`)
+    })
 }
 
 const createDir = path => {
-  return new Promise((resolve, reject) => {
-    fse.mkdirp(path, err => {
-      if (err) {
-        showError(`Fail creating ${path}`)
-        reject(err)
-      } else {
-        console.log(`Created ${path}`)
-        resolve()
-      }
+  return fse
+    .mkdirp(path)
+    .then(() => {
+      console.log(`Created ${path}`)
     })
-  })
+    .catch(() => {
+      showError(`Fail creating ${path}`)
+    })
 }
 
 const BODY_TEST = `/**
