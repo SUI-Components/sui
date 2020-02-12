35ca64a6faaf10db68b5bec46b2642940362bdc3
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-02-12 10:18:22 +0100

    docs(sui-js): add deprecated jsdoc for fromArrayToCommaQueryString

--- packages/sui-js/src/string/index.js
+++ packages/sui-js/src/string/index.js
@@ -2,6 +2,14 @@ import toQueryString from './to-query-string'
 
 export {default as parseQueryString} from './parse-query-string'
 export {default as toQueryString} from './to-query-string'
+
+/**
+ * @deprecated since version 2.9.0
+ * will be deleted in version 3
+ *
+ * use toQueryString(queryParams, {arrayFormat: 'comma'}) instead
+ * it may not be very convenient to decode with decodeURIComponent knowing that it will be used in the url
+ */
 export const fromArrayToCommaQueryString = query =>
   decodeURIComponent(toQueryString(query, {arrayFormat: 'comma'}))
 
