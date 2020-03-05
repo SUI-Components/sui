704090c512e19792e6cfafb03193ccbe5dd9727d
Ignacio Calleja <ignacio.calleja@schibsted.com>
2020-03-05 14:27:44 +0100

    feat(sui-decorators): import new tracer decorator in index file

--- packages/sui-decorators/src/index.js
+++ packages/sui-decorators/src/index.js
@@ -1,5 +1,6 @@
 import cache from './decorators/cache'
 import streamify from './decorators/streamify'
 import inlineError from './decorators/error'
+import tracer from './decorators/tracer'
 
-export {cache, streamify, inlineError}
+export {cache, streamify, inlineError, tracer}
