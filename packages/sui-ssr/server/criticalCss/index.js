7ea27dd6aa12001e9013fe26b83a18348ac5ec04
Carlos Villuendas <carlosvillu@gmail.com>
2020-03-20 08:39:16 +0100

    feat(sui-ssr): use @s-ui/react-router for criticalcss

--- packages/sui-ssr/server/criticalCss/index.js
+++ packages/sui-ssr/server/criticalCss/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import routes from 'routes'
-import {match} from 'react-router'
+import match from '@s-ui/react-router/lib/match'
 import https from 'https'
 import parser from 'ua-parser-js'
 
