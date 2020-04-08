1339789c883dd8b4d46dd83ae129e4ec22a42f8e
midudev <miduga@gmail.com>
2020-04-08 19:34:24 +0200

    feat(sui-react-router): document propTypes of Route

--- packages/sui-react-router/src/Route.js
+++ packages/sui-react-router/src/Route.js
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types'
-import invariant from 'invariant'
-
-import {component, components} from './InternalPropTypes'
+import invariant from './utils/invariant'
 
 const Route = () => {
   invariant(
@@ -12,9 +10,24 @@ const Route = () => {
 
 Route.displayName = 'Route'
 Route.propTypes = {
-  components,
-  component,
+  /**
+   * The child elements or routes to be rendered
+   **/
+  children: PropTypes.object,
+  /**
+   * A single component to be rendered when the route matches the URL. It can
+   * be rendered by the parent route component with `props.children`.
+   **/
+  component: PropTypes.object,
+  /**
+   * Same as `component` but asynchronous, useful for code-splitting.
+   */
   getComponent: PropTypes.func,
+  /**
+   * The path used in the URL.
+   * It will concat with the parent route's path unless it starts with `/`, making it an absolute path.
+   * If left undefined, the router will try to match the child routes.
+   */
   path: PropTypes.string
 }
 
