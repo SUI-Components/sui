2470b268f2c763f7d00db947ac91e7bbd6189d28
midudev <miduga@gmail.com>
2020-04-14 12:42:34 +0200

    refactor(sui-react-router): add from file and comments

--- packages/sui-react-router/src/index.js
+++ packages/sui-react-router/src/index.js
@@ -1,9 +1,17 @@
-export {default as match} from './match'
+// from: https://github.com/ReactTraining/react-router/blob/v3/modules/index.js
+// as @s-ui/react-router is a subset of react-router@3, some exports are missing
 
-export {default as browserHistory} from './browserHistory'
-
-export {default as Link} from './Link'
-export {default as Redirect} from './Redirect'
+/* components */
 export {default as Router} from './Router'
+export {default as Link} from './Link'
+
+/* components (configuration) */
+export {default as Redirect} from './Redirect'
 export {default as Route} from './Route'
 export {default as IndexRoute} from './IndexRoute'
+
+/* utils */
+export {default as match} from './match'
+
+/* histories */
+export {default as browserHistory} from './browserHistory'
