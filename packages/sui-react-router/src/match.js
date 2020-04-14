7cabe7bc24a9f55c2bb870479a64d6039ded1c78
midudev <miduga@gmail.com>
2020-04-14 10:08:56 +0200

    docs(sui-react-router): document match file

--- packages/sui-react-router/src/match.js
+++ packages/sui-react-router/src/match.js
@@ -1,22 +1,40 @@
+// from: https://github.com/ReactTraining/react-router/blob/v3/modules/match.js
+
 import {REPLACE} from 'history/lib/Actions'
 import {fromReactTreeToJSON} from './utils/react-utils'
 import {createTransitionManager} from './createTransitionManager'
 import {createRouterObject} from './utils/RouterUtils'
 
-const match = async ({routes, history, location}, cb) => {
+/**
+ * A high-level API to be used for server-side rendering.
+ *
+ * This function matches a location to a set of routes and calls
+ * callback(error, redirectLocation, renderProps) when finished.
+ *
+ * Note: You probably don't want to use this in a browser unless you're using
+ * server-side rendering with async routes.
+ * @param {{ routes: Array, history, location }} options
+ * @param {Function} callback
+ */
+export default async ({routes, history, location}, callback) => {
   const jsonRoutes = fromReactTreeToJSON(routes)
   const transitionManager = createTransitionManager({
     history,
     jsonRoutes
   })
 
-  const match = await transitionManager.match(location)
-  const {components, redirectLocation, routeInfo} = match
+  const {
+    components,
+    redirectLocation,
+    routeInfo
+  } = await transitionManager.match(location)
+
   // TODO: Maybe I need do something more here.
   // https://github.com/ReactTraining/react-router/blob/v3/modules/RouterUtils.js#L1
-  const router = createRouterObject(history, transitionManager, routeInfo)
+  const {isActive} = transitionManager
+  const router = createRouterObject(history, isActive, routeInfo)
 
-  return cb(
+  return callback(
     null,
     redirectLocation && history.createLocation(redirectLocation, REPLACE),
     {
@@ -27,5 +45,3 @@ const match = async ({routes, history, location}, cb) => {
     }
   )
 }
-
-export default match
