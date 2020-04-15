d9751a31e35f62f4b342f87c2a57b0c1f7d046e1
midudev <miduga@gmail.com>
2020-04-15 16:49:13 +0200

    feat(sui-react-router): add hooks

---
+++ packages/sui-react-router/src/hooks.js
@@ -0,0 +1,14 @@
+import {useContext} from 'react'
+import Context from './Context'
+
+const useRouterContext = () => {
+  const context = useContext(Context)
+  if (context === undefined) {
+    throw new Error('@s-ui/react-router must be used within a <Router />')
+  }
+  return context
+}
+
+export const useParams = () => useRouterContext().router.params
+export const useLocation = () => useRouterContext().location
+export const useRouter = () => useRouterContext().router
