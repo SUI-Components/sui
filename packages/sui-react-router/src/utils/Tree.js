3e859b62f4dd8032c46a161521423ae31d0e8a60
midudev <miduga@gmail.com>
2020-04-20 13:33:39 +0200

    refactor(sui-react-router): remove not needed code on Tree file

--- packages/sui-react-router/src/utils/Tree.js
+++ packages/sui-react-router/src/utils/Tree.js
@@ -1,8 +1,10 @@
 // from: https://jrsinclair.com/articles/2019/functional-js-traversing-trees-with-recursive-reduce/
 
-const curry = (fn, arity = fn.length, ...args) =>
-  arity <= args.length ? fn(...args) : curry.bind(null, fn, arity, ...args)
-
+/**
+ * Check if there're still children to process in the actual node
+ * @param {{ children: Array }} node Object with route info
+ * @returns {Boolean}
+ */
 function hasChildren(node) {
   return (
     typeof node === 'object' &&
@@ -12,25 +14,15 @@ function hasChildren(node) {
 }
 
 export const Tree = {
-  reduce: curry(function reduce(reducerFn, init, node) {
+  reduce: function reduce(reducerFn, init, node) {
+    // Calculate the reduced value for this node
     const acc = reducerFn(init, node)
-    if (!hasChildren(node)) {
-      return acc
-    }
-    return node.children.reduce(Tree.reduce(reducerFn), acc)
-  }),
-  map: curry(function map(mapFn, node) {
-    const newNode = mapFn(node)
-    if (hasChildren(node)) {
-      return newNode
-    }
-    newNode.children = node.children.map(Tree.map(mapFn))
-    return newNode
-  }),
-  forEach: curry(function forEach(eachFn, node) {
-    eachFn(node)
-    if (hasChildren(node)) {
-      node.children.forEach(Tree.forEach(eachFn))
-    }
-  })
+    // Stop processing tree structure if no further children
+    if (!hasChildren(node)) return acc
+    // Recursive transverse tree structure
+    return node.children.reduce(
+      (acc, currentValue) => Tree.reduce(reducerFn, acc, currentValue),
+      acc
+    )
+  }
 }
