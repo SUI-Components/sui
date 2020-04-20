6f6b056014c06f779aec8ff31a8a38d58d0cc753
midudev <miduga@gmail.com>
2020-04-20 13:30:49 +0200

    fix(sui-react-router): avoid browserHistory to break when imported from server

--- packages/sui-react-router/src/browserHistory.js
+++ packages/sui-react-router/src/browserHistory.js
@@ -1,17 +1,15 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 
 // from: https://github.com/ReactTraining/react-router/blob/v3/modules/useRouterHistory.js
-
+import canUseDOM from './utils/canUseDOM'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 import useBasename from 'history/lib/useBasename'
 import useQueries from 'history/lib/useQueries'
+import {createMemoryHistory} from 'history'
 
-const canUseDOM = !!(
-  typeof window !== 'undefined' &&
-  window.document &&
-  window.document.createElement
-)
+/** If we're on the server, we must be sure importing this file doesn't break anything */
+const browserHistory = canUseDOM
+  ? useQueries(useBasename(createBrowserHistory))()
+  : createMemoryHistory()
 
-export default function() {
-  return canUseDOM ? useQueries(useBasename(createBrowserHistory))() : undefined
-}
+export default browserHistory
