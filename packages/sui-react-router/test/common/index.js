a4ed553c2255898856ae918e05c3b9df9c6e4fd9
midudev <miduga@gmail.com>
2020-04-20 19:35:38 +0200

    test(sui-react-router): add new tests for createMemoryHistory

---
+++ packages/sui-react-router/test/common/index.js
@@ -0,0 +1,12 @@
+import './hooksSpec'
+import './IndexRouteSpec'
+import './LinkSpec'
+import './RedirectSpec'
+import './RouteSpec'
+import './createMemoryHistorySpec'
+
+/* utils */
+import './getParamsSpec'
+import './isActiveSpec'
+import './matchPatternSpec'
+import './utilsSpec'
--- packages/sui-react-router/test/server/commonSpec.js
+++ packages/sui-react-router/test/server/commonSpec.js
@@ -1,11 +1,1 @@
-import '../common/hooksSpec'
-import '../common/IndexRouteSpec'
-import '../common/LinkSpec'
-import '../common/RedirectSpec'
-import '../common/RouteSpec'
-
-/* utils */
-import '../common/getParamsSpec'
-import '../common/isActiveSpec'
-import '../common/matchPatternSpec'
-import '../common/utilsSpec'
+import '../common'
