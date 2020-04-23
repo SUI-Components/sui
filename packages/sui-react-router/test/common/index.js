371c77dcebaca26405dc9895e8ea6466b79ad8e9
midudev <miduga@gmail.com>
2020-04-23 16:37:30 +0200

    test(sui-react-router): add withRouter to the common suite

--- packages/sui-react-router/test/common/index.js
+++ packages/sui-react-router/test/common/index.js
@@ -1,12 +1,13 @@
+import './createMemoryHistorySpec'
 import './hooksSpec'
 import './IndexRouteSpec'
 import './LinkSpec'
 import './RedirectSpec'
 import './RouteSpec'
-import './createMemoryHistorySpec'
 
 /* utils */
 import './getParamsSpec'
 import './isActiveSpec'
 import './matchPatternSpec'
 import './utilsSpec'
+import './withRouterSpec'
