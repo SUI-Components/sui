f87f8f92ad9c4f23c4966d5fb573a148064ddce2
midudev <miduga@gmail.com>
2020-05-05 12:16:05 +0200

    docs(sui-react-router): typings for hooks

--- packages/sui-react-router/src/hooks.js
+++ packages/sui-react-router/src/hooks.js
@@ -1,6 +1,9 @@
 import {useContext} from 'react'
 import Context from './internal/Context'
 
+/**
+ * @returns {{router: import('./types').Router}}
+ */
 const useRouterContext = () => {
   const context = useContext(Context)
   if (context === undefined) {
@@ -9,6 +12,17 @@ const useRouterContext = () => {
   return context
 }
 
+/**
+ * @returns {import('./types').Params}
+ */
 export const useParams = () => useRouterContext().router.params
+
+/**
+ * @returns {import('./types').Location}
+ */
 export const useLocation = () => useRouterContext().router.location
+
+/**
+ * @returns {import('./types').Router}
+ */
 export const useRouter = () => useRouterContext().router
