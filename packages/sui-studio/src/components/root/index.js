1d394a996a83625130bc8160fa77d20334ce5102
midudev <miduga@gmail.com>
2020-05-05 18:13:45 +0200

    Revert "Merge pull request #813 from SUI-Components/feat/move-studio-to-new-sui-react-router"
    This reverts commit 503bcc8e0188e14f5ce3eb81c723ce0c3d77d4b7, reversing
    changes made to 4a60980eacb490c03cdb0b0d3fdefa78841d700d.

--- packages/sui-studio/src/components/root/index.js
+++ packages/sui-studio/src/components/root/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Router} from '@s-ui/react-router'
+import {Router, browserHistory} from 'react-router'
 import routes from '../../routes'
 import '../../index.scss'
 
-export default () => <Router routes={routes} />
+export default () => <Router routes={routes} history={browserHistory} />
