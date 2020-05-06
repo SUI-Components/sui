cadf8fe385f250c5fa0979426b9ef229034327ae
Joan León | @nucliweb <joan.leon@gmail.com>
2020-05-06 22:10:19 +0200

    feat(sui-studio): improve sui-studio generate documentation

--- packages/sui-studio/bin/sui-studio-generate.js
+++ packages/sui-studio/bin/sui-studio-generate.js
@@ -179,10 +179,22 @@ $ npm install ${packageName}
 ## Usage
 
 ### Basic usage
+
+#### Import package and use the component
+
 \`\`\`js
+// Javascript
 import ${componentInPascal} from '${packageName}'
 
 return (<${componentInPascal} />)
+\`\`\`
+
+#### Import the styles (Sass)
+
+\`\`\`css
+@import '~@s-ui/theme/lib/index';
+// @import 'your theme';
+@import '~${packageName}/lib/index';
 \`\`\`
 
 
