381a2fcfcc2307dc08a73049f67eae2083457841
Tomas Madariaga <critimass@gmail.com>
2020-05-26 16:15:00 +0200

    test(sui-decorators): cache server testing

--- packages/sui-decorators/test/server/cacheSpec.js
+++ packages/sui-decorators/test/server/cacheSpec.js
@@ -3,51 +3,136 @@
 import {expect} from 'chai'
 
 import cache from '../../src/decorators/cache'
+import inlineError from '../../src/decorators/error'
 
-describe('Cache', () => {
-  it('should ignore the cache in Node by default', () => {
-    class Buz {
-      constructor() {
-        this.rnd = () => Math.random()
+describe('Cache in the server', () => {
+  describe('decorating a sync method', () => {
+    it('should not apply cache in Node by default', () => {
+      class Buz {
+        constructor() {
+          this.rnd = () => Math.random()
+        }
+
+        @cache()
+        @inlineError
+        syncRndNumber() {
+          return this.rnd()
+        }
+      }
+      const buz = new Buz()
+      expect(buz.syncRndNumber()).to.be.not.eql(buz.syncRndNumber())
+    })
+
+    it('should apply cache if server param is true', () => {
+      class Buz1 {
+        constructor() {
+          this.rnd = () => Math.random()
+        }
+
+        @cache({server: true})
+        syncRndNumber() {
+          return this.rnd()
+        }
+      }
+      const buz = new Buz1()
+      expect(buz.syncRndNumber()).to.be.eql(buz.syncRndNumber())
+    })
+
+    it('witth inlineError should apply cache if server param is true', () => {
+      class Buz2 {
+        constructor() {
+          this.rnd = () => Math.random()
+        }
+
+        @cache({server: true})
+        @inlineError
+        syncRndNumber() {
+          return this.rnd()
+        }
+      }
+      const buz = new Buz2()
+      expect(buz.syncRndNumber()).to.be.eql(buz.syncRndNumber())
+    })
+
+    it('should not apply cache for inlineError decorated error response', () => {
+      let shouldReturnError = true
+
+      class Yummy {
+        @cache({server: true})
+        @inlineError
+        rndNumber() {
+          if (shouldReturnError) {
+            throw new Error('Error')
+          }
+
+          return Math.random()
+        }
       }
 
-      @cache()
-      syncRndNumber() {
-        return this.rnd()
-      }
-    }
-    const buz = new Buz()
-    expect(buz.syncRndNumber()).to.be.not.eql(buz.syncRndNumber())
+      const yummy = new Yummy()
+      const responseFirst = yummy.rndNumber()
+      shouldReturnError = false
+      const responseSecond = yummy.rndNumber()
+      const responseThird = yummy.rndNumber()
+
+      expect(responseFirst[0]).to.be.not.null
+      expect(responseSecond[1]).to.be.eql(responseThird[1])
+    })
   })
 
-  it('should have a cache if the force the cache in node', () => {
-    class Buz {
-      constructor() {
-        this.rnd = () => Math.random()
+  describe('decorating an async method', () => {
+    it('should return twice the same random number without params', done => {
+      class Dummy {
+        @cache({server: true})
+        @inlineError
+        asyncRndNumber() {
+          return new Promise(resolve => setTimeout(resolve, 100, Math.random()))
+        }
+      }
+      const dummy = new Dummy()
+      Promise.all([dummy.asyncRndNumber(), dummy.asyncRndNumber()]).then(
+        ([firstCall, secondCall]) => {
+          expect(firstCall).to.be.eql(secondCall)
+          done()
+        }
+      )
+    })
+
+    it('should not apply cache for inlineError decorated error response', async () => {
+      let shouldReturnError = true
+
+      class YummyAsync {
+        @cache({
+          server: true,
+          ttl: '1 minute'
+        })
+        @inlineError
+        async asyncRndNumber() {
+          if (shouldReturnError) {
+            return Promise.reject(new Error('Error'))
+          }
+
+          return Math.random()
+        }
       }
 
-      @cache({server: true})
-      syncRndNumber() {
-        return this.rnd()
-      }
-    }
-    const buz = new Buz()
-    expect(buz.syncRndNumber()).to.be.eql(buz.syncRndNumber())
-  })
+      const yummyAsync = new YummyAsync()
+      // Error response, it should not be cached
+      const responseFirst = await yummyAsync.asyncRndNumber()
 
-  it('return twice the same random number without params', done => {
-    class Dummy {
-      @cache({server: true})
-      asyncRndNumber() {
-        return new Promise(resolve => setTimeout(resolve, 100, Math.random()))
-      }
-    }
-    const dummy = new Dummy()
-    Promise.all([dummy.asyncRndNumber(), dummy.asyncRndNumber()]).then(
-      ([firstCall, secondCall]) => {
-        expect(firstCall).to.be.eql(secondCall)
-        done()
-      }
-    )
+      // Ok response, it should be cached
+      shouldReturnError = false
+      const responseSecond = await yummyAsync.asyncRndNumber()
+      const responseThird = await yummyAsync.asyncRndNumber()
+
+      // Here we force an error and expect not response error because it is cached
+      shouldReturnError = true
+      const responseFourth = await yummyAsync.asyncRndNumber()
+
+      expect(responseFirst[0]).to.be.not.null
+      expect(responseSecond[1]).to.be.eql(responseThird[1])
+      expect(responseFourth[0]).to.be.null
+      expect(responseSecond[1]).to.be.eql(responseFourth[1])
+    })
   })
 })
