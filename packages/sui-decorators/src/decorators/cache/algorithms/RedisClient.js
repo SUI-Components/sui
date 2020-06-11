3a44875036f65f8f7992ccd37fba1c3f834c7f93
Tomas Madariaga <critimass@gmail.com>
2020-06-11 11:38:46 +0200

    refactor(sui-decorators): redisClient

--- packages/sui-decorators/src/decorators/cache/algorithms/RedisClient.js
+++ packages/sui-decorators/src/decorators/cache/algorithms/RedisClient.js
@@ -1,12 +1,11 @@
 import redis from 'redis'
 import redisMock from 'redis-mock'
 
-const useRedis = process.env.USE_REDIS_IN_SUI_DECORATORS_CACHE
-let instance
-
 export default class RedisClient {
+  static instance
+
   constructor({redisConnection}) {
-    this._redisClient = useRedis
+    this._redisClient = process.env.USE_REDIS_IN_SUI_DECORATORS_CACHE
       ? redis.createClient({
           port: redisConnection.port,
           host: redisConnection.host,
@@ -31,18 +30,18 @@ export default class RedisClient {
         })
       : redisMock.createClient()
 
-    !useRedis && console.log('Using Mock Client')
-
     this._redisClient.on('error', function() {
-      console.error(`[sui-decorators/cache]:RedisClient Error creating client`)
+      console.error(
+        `[sui-decorators/cache]:RedisClient Error creating Redis client`
+      )
     })
   }
 
   static getInstance({redisConnection = {host: '127.0.0.1', port: 6379}}) {
-    if (!instance) {
-      instance = new RedisClient({redisConnection})
+    if (!RedisClient.instance) {
+      RedisClient.instance = new RedisClient({redisConnection})
     }
-    return instance
+    return RedisClient.instance
   }
 
   get client() {
