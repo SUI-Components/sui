bc91d863b0d03b4a017cb28191030bf6ce9fa762
Carlos Villuendas <carlosvillu@gmail.com>
2020-07-03 13:01:16 +0200

    feat(sui-ssr): remove FRONTEND_ env vars

--- packages/sui-ssr/archive/start-app.sh
+++ packages/sui-ssr/archive/start-app.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-FILTER="MS_"
+FILTER="^MS_|^FRONTEND_"
 
-for var in $(printenv); do
-    test "${var#*$FILTER}" != "$var" && unset "$var"
+for var in $(printenv | grep -E "$FILTER"); do
+    unset "$var"
 done
 
 echo "System Env variables after filter:"
