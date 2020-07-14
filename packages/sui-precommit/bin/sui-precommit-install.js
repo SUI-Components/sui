d736751bf29b1126c1fb1f67546ca98e9497d13d
midudev <miduga@gmail.com>
2020-07-14 15:54:54 +0200

    feat(sui-precommit): adapt to newer husky script

--- packages/sui-precommit/bin/sui-precommit-install.js
+++ packages/sui-precommit/bin/sui-precommit-install.js
@@ -5,10 +5,14 @@ const path = require('path')
 const fs = require('fs')
 const pkgPath = path.join(process.cwd(), 'package.json')
 
+const DEFAULT_PKG_FIELD = 'scripts'
+const HUSKY_VERSION = '4.2.5'
+
 installHuskyIfNotInstalled()
   .then(function() {
-    installScript('lint', 'sui-lint js && sui-lint sass')
-    installScript('precommit', 'sui-precommit run')
+    addToPackageJson('lint', 'sui-lint js && sui-lint sass')
+    addToPackageJson('pre-commit', 'sui-precommit run', 'hooks')
+    removeFromPackageJson('precommit')
   })
   .catch(function(err) {
     log(err.message)
@@ -23,15 +27,50 @@ function log(...args) {
 }
 
 /**
- * Install script on package.json where command was executed
- * @param  {String} name   script name
- * @param  {String} script command to execute
+ * Read package.json file
+ * @returns {object} Package.json content in JSON format
  */
-function installScript(name, script) {
-  const pkg = JSON.parse(fs.readFileSync(pkgPath, {encoding: 'utf8'}))
-  pkg.scripts = pkg.scripts || {}
-  pkg.scripts[name] && log('Script "' + name + '" already set. Overwritting...')
-  pkg.scripts[name] = script
+function readPackageJson() {
+  return JSON.parse(fs.readFileSync(pkgPath, {encoding: 'utf8'}))
+}
+
+/**
+ * Add script on package.json where command was executed
+ * @param  {string}  name   script name
+ * @param  {string}  script command to execute
+ * @param  {string?}  field  field where the script must be added
+ **/
+function addToPackageJson(name, script, field = DEFAULT_PKG_FIELD) {
+  const pkg = readPackageJson()
+  pkg[field] = pkg[field] || {}
+
+  pkg[field][name] = script
+  log(`Writing "${name}" on "${field}"...`)
+
+  writePackageJson(pkg)
+}
+
+/**
+ * Add script on package.json where command was executed
+ * @param  {string}  name   script name
+ * @param  {string}  field  field where the script is
+ **/
+function removeFromPackageJson(name, field = DEFAULT_PKG_FIELD) {
+  const pkg = readPackageJson()
+  pkg[field] = pkg[field] || {}
+
+  const {[name]: remove, ...rest} = pkg[field]
+  pkg[field] = rest
+  log(`"${name}" removed from "${field}". Writing changes...`)
+
+  writePackageJson(pkg)
+}
+
+/**
+ * Write package.json file where command was executed
+ * @param  {object}  pkg New package content to be write on the file
+ */
+function writePackageJson(pkg) {
   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), {encoding: 'utf8'})
 }
 
@@ -44,7 +83,7 @@ function installHuskyIfNotInstalled() {
     log('husky will be installed to allow git hook integration with node')
     return getSpawnPromise('npm', [
       'install',
-      'husky@0.13.4',
+      `husky@${HUSKY_VERSION}`,
       '--save-dev',
       '--save-exact'
     ])
@@ -54,10 +93,11 @@ function installHuskyIfNotInstalled() {
 }
 
 /**
- * Get if husky is already installed
+ * Get if husky is already installed with the expected version
  * @return {Boolean}
  */
 function isHuskyInstalled() {
   const pkg = JSON.parse(fs.readFileSync(pkgPath, {encoding: 'utf8'}))
-  return pkg.devDependencies && pkg.devDependencies.husky
+  const huskyDependency = pkg.devDependencies && pkg.devDependencies.husky
+  return huskyDependency === HUSKY_VERSION
 }
