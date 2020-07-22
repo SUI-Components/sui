1196e14043758db4edb82ad49dec2d7a04bc0645
midudev <miduga@gmail.com>
2020-07-22 13:51:38 +0200

    test(sui-ci): add mockGitHubRequest util for testing

---
+++ packages/sui-ci/test/server/utils.js
@@ -0,0 +1,15 @@
+import nock from 'nock'
+
+export const mockGitHubRequest = (executeExpectations) => {
+  const nockedRequest = nock('https://api.github.com')
+    .post(/.*/)
+    .reply(200)
+
+  nockedRequest.on('request', (req, _, rawBody) => {
+    const {method, options} = req
+    const {href} = options
+    const body = JSON.parse(rawBody)
+    executeExpectations({body, method, href})
+    nockedRequest.done()
+  })
+}
