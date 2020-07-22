86735cb98eed9a209ac939f245e8bfb8799f2151
midudev <miduga@gmail.com>
2020-07-22 12:25:29 +0200

    feat(sui-ci): add a new way to get the topic from args

--- packages/sui-ci/bin/sui-ci-update-commit-status.js
+++ packages/sui-ci/bin/sui-ci-update-commit-status.js
@@ -5,7 +5,7 @@ const updateCommitStatus = require('../src/index')
 
 const {
   GH_TOKEN: auth,
-  SUI_TOPIC: topic,
+  SUI_CI_TOPIC: topicFromEnv,
   TRAVIS_BUILD_WEB_URL: buildUrl,
   TRAVIS_COMMIT,
   TRAVIS_PULL_REQUEST_SHA,
@@ -15,10 +15,11 @@ const {
 program
   .option('-s, --state <stateKey>', 'State of the commit. Accepted values:"OK", "KO", "RUN"', 'KO')
   .option('-u, --url <targetUrl>', 'Url where the details link navigates to', buildUrl)
+  .option('-t, --topic <ciTopic>', 'Topic telling what is the commit about', topicFromEnv)
   .parse(process.argv)
 
 const commit = TRAVIS_PULL_REQUEST_SHA || TRAVIS_COMMIT
-const {state: stateKey, url: targetUrl} = program
+const {state: stateKey, topic, url: targetUrl} = program
 
 updateCommitStatus({
   auth,
