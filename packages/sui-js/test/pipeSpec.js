c7e80ec105bdf9093d3a7b65742f59a961972c8d
Carlos Villuendas <carlosvillu@gmail.com>
2020-07-23 12:19:39 +0200

    refactor(sui-js): Fix lint error

--- packages/sui-js/test/pipeSpec.js
+++ packages/sui-js/test/pipeSpec.js
@@ -11,37 +11,30 @@ describe('@s-ui/js', () => {
 
   describe('when we use the pipe method ', () => {
     it('should add several incs', async () => {
-      const result = await pipe(
-        inc,
-        inc,
-        inc
-      )(0)
+      const result = await pipe(inc, inc, inc)(0)
       expect(result).to.be.eql(3)
     })
 
     xit('should convert title to array', () => {
-      expect(
-        pipe(
-          textToUpperCase,
-          textToArray
-        )(title)
-      ).to.be.an('array')
+      expect(pipe(textToUpperCase, textToArray)(title)).to.be.an('array')
     })
     xit('should be the same length as the title ', () => {
-      expect(
-        pipe(
-          textToUpperCase,
-          textToArray
-        )(title)
-      ).to.have.lengthOf(title.length)
+      expect(pipe(textToUpperCase, textToArray)(title)).to.have.lengthOf(
+        title.length
+      )
     })
     xit('should return the title in camelcase and array form ', () => {
-      expect(
-        pipe(
-          textToUpperCase,
-          textToArray
-        )(title)
-      ).to.deep.equal(['S', 'C', 'H', 'I', 'B', 'S', 'T', 'E', 'D'])
+      expect(pipe(textToUpperCase, textToArray)(title)).to.deep.equal([
+        'S',
+        'C',
+        'H',
+        'I',
+        'B',
+        'S',
+        'T',
+        'E',
+        'D'
+      ])
     })
   })
 })
