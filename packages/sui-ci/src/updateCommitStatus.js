35ccd004b6ba218c5428fa5dfb19dd6da0659990
midudev <miduga@gmail.com>
2020-09-01 10:58:28 +0200

    feat(sui-ci): add support to Github Enterprise

--- packages/sui-ci/src/updateCommitStatus.js
+++ packages/sui-ci/src/updateCommitStatus.js
@@ -1,9 +1,26 @@
 const {Octokit} = require('@octokit/core')
+const parseGitUrl = require('git-url-parse')
+const getGitRemoteOriginUrl = require('git-remote-origin-url')
+
 const {
   STATUS_CONTEXT,
   STATUS_DESCRIPTION,
   STATUS_STATES
 } = require('./constants')
+
+/**
+ * Get the needed API Base Url depending on the remote origin url
+ */
+async function getApiBaseUrl() {
+  const gitRemoteOriginUrl = await getGitRemoteOriginUrl().catch(
+    () => 'git@github.com:sui/remote-url.git'
+  )
+  const {resource} = parseGitUrl(gitRemoteOriginUrl)
+
+  return resource === 'github.com'
+    ? 'https://api.github.com'
+    : `https://${resource}/api/v3`
+}
 
 /**
  * Update GitHub commit status
@@ -16,7 +33,7 @@ const {
  * @param {string} params.repoSlug Slug with the format ${owner}/${repositoryName}
  * @returns {Promise}
  */
-module.exports = function updateCommitStatus({
+module.exports = async function updateCommitStatus({
   auth,
   commit,
   stateKey,
@@ -27,7 +44,10 @@ module.exports = function updateCommitStatus({
   const [owner, repo] = repoSlug.split('/')
   const state = STATUS_STATES[stateKey]
 
+  const baseUrl = await getApiBaseUrl()
+
   const requestParams = {
+    baseUrl,
     context: `${STATUS_CONTEXT} (${topic})`,
     description: STATUS_DESCRIPTION[topic][state],
     owner,
