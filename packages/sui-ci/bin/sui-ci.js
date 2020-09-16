bdbcd0a351ea3e1e084caa849ef03a4aa2358d18
midudev <miduga@gmail.com>
2020-09-16 12:04:34 +0200

    fix(sui-ci): add release command

--- packages/sui-ci/bin/sui-ci.js
+++ packages/sui-ci/bin/sui-ci.js
@@ -6,5 +6,6 @@ const {version} = require('../package.json')
 program
   .version(version)
   .command('update-commit-status', 'update a GitHub Commit Status')
+  .command('release', 'release packages on Travis CI')
 
 program.parse(process.argv)
