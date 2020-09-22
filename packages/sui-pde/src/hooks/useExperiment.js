f266acbf52e0a71fed5288d80f9e3c3f2dd2ba74
Julian Gernun <julian.gernun@adevinta.com>
2020-09-22 10:17:21 +0200

    feat(sui-pde): rename options to attributes

--- packages/sui-pde/src/hooks/useExperiment.js
+++ packages/sui-pde/src/hooks/useExperiment.js
@@ -4,15 +4,15 @@ import PdeContext from '../contexts/PdeContext'
 /**
  * Hook to use a experiment
  * @param {string} experimentName
- * @param {object} options
+ * @param {object} attributes
  * @return {{variation: string}}
  */
-export default function useExperiment(experimentName, options) {
+export default function useExperiment(experimentName, attributes) {
   const {pde} = useContext(PdeContext)
   if (pde === null) throw new Error('[useExperiment] pde is required to work')
   const variation = useMemo(
-    () => pde.activateExperiment({name: experimentName, options}),
-    [experimentName, pde, options]
+    () => pde.activateExperiment({name: experimentName, attributes}),
+    [experimentName, pde, attributes]
   )
 
   return {variation}
