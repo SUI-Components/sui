96115afc0d3513ab4dade4662258280ea00c6257
midudev <miduga@gmail.com>
2020-09-23 18:52:12 +0200

    refactor(sui-studio): simplify workbench component

--- packages/sui-studio/src/components/workbench/index.js
+++ packages/sui-studio/src/components/workbench/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+
 import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import {Link} from '@s-ui/react-router'
@@ -5,45 +7,43 @@ import cx from 'classnames'
 
 import {FILES} from '../../constants'
 import {tryRequireMarkdown} from '../tryRequire'
-import When from '../when'
 
 const TAB_CLASS = 'sui-StudioTabs-tab'
 const LINK_CLASS = 'sui-StudioTabs-link'
 const ACTIVE_CLASS = LINK_CLASS + '--active'
 
+const SPALink = ({name, to}) => (
+  <Link activeClassName={ACTIVE_CLASS} className={LINK_CLASS} to={to}>
+    {name}
+  </Link>
+)
+
+const AnchorLink = ({name, to}) => {
+  const isActive = window.location.href.match(to)
+  const className = cx(LINK_CLASS, {
+    [ACTIVE_CLASS]: isActive
+  })
+  return (
+    <a className={className} href={to}>
+      {name}
+    </a>
+  )
+}
+
 export default function Workbench({children, params}) {
   const [showUX, setShowUX] = useState(false)
   const {category, component} = params
 
-  const Tab = ({name, path, forceReload = false}) => ( // eslint-disable-line
-    <li className={TAB_CLASS}>
-      <When value={!forceReload}>
-        {() => (
-          <Link
-            activeClassName={ACTIVE_CLASS}
-            className={LINK_CLASS}
-            to={`/workbench/${category}/${component}/${path}`}
-          >
-            {name}
-          </Link>
-        )}
-      </When>
-      <When value={forceReload}>
-        {() => {
-          const href = `/workbench/${category}/${component}/${path}`
-          const isActive = window.location.href.match(href)
-          const className = cx(LINK_CLASS, {
-            [ACTIVE_CLASS]: isActive
-          })
-          return (
-            <a className={className} href={href}>
-              {name}
-            </a>
-          )
-        }}
-      </When>
-    </li>
-  )
+  const Tab = ({name, path, forceReload = false}) => {
+    const to = `/workbench/${category}/${component}/${path}`
+    const Link = forceReload ? AnchorLink : SPALink
+
+    return (
+      <li className={TAB_CLASS}>
+        <Link to={to} name={name} />
+      </li>
+    )
+  }
 
   useEffect(
     function() {
