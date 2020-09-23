82557fcdeb783d94acaca351bfb15b107ba413cd
midudev <miduga@gmail.com>
2020-09-23 18:51:50 +0200

    refactor(sui-studio): remove not needed When component

--- packages/sui-studio/src/components/when/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-const When = ({value, children}) => {
-  return value ? <>{children()}</> : null
-}
-When.propTypes = {value: PropTypes.any, children: PropTypes.func}
-
-export default When
