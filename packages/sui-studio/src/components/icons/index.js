21741679b891790c8ccfd8c89c30cc01dfdc9dba
midudev <miduga@gmail.com>
2020-09-23 19:40:00 +0200

    refactor(sui-studio): remove not needed xmlns

--- packages/sui-studio/src/components/icons/index.js
+++ packages/sui-studio/src/components/icons/index.js
@@ -1,33 +1,33 @@
 import React from 'react'
 
 export const iconFullScreenExit = (
-  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 24 24">
     <path d="M0 0h24v24H0z" fill="none" />
     <path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z" />
   </svg>
 )
 export const iconFullScreen = (
-  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 24 24">
     <path d="M0 0h24v24H0z" fill="none" />
     <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z" />
   </svg>
 )
 export const iconCode = (
-  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 24 24">
     <path d="M0 0h24v24H0V0z" fill="none" />
     <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z" />
   </svg>
 )
 
 export const iconMenu = (
-  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 24 24">
     <path d="M0 0h24v24H0z" fill="none" />
     <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
   </svg>
 )
 
 export const iconClose = (
-  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 24 24">
     <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
     <path d="M0 0h24v24H0z" fill="none" />
   </svg>
