0c8bade69ef0548cd8830d62b3ad004fd0e6437d
midudev <miduga@gmail.com>
2020-09-25 23:21:38 +0200

    feat(sui-studio): move test page to workbench, only way to use it in browser

---
+++ packages/sui-studio/workbench/src/components/Test/index.js
@@ -0,0 +1,77 @@
+import React, {useState, useEffect} from 'react'
+import PropTypes from 'prop-types'
+
+import cx from 'classnames'
+import hoistNonReactStatics from 'hoist-non-react-statics'
+
+import SUIContext from '@s-ui/react-context'
+import withContext from '../../../../src/components/demo/HoC/withContext'
+import {addSetupEnvironment} from '../../../../src/environment-mocha/setupEnvironment'
+
+addSetupEnvironment(window)
+
+const BASE_CLASSNAME = 'sui-Test'
+
+const Test = ({open, importTest, importComponent, contexts}) => {
+  const [failures, setFailures] = useState(0)
+  const [notFoundTest, setNotFoundTest] = useState(false)
+
+  const classnames = cx(BASE_CLASSNAME, {
+    [`${BASE_CLASSNAME}--open`]: open,
+    [`${BASE_CLASSNAME}--failures`]: failures
+  })
+
+  useEffect(() => {
+    importComponent().then(async module => {
+      const Component = module.default || module
+
+      // extract displayName for the Component
+      // until React 17, we need a workaround for React.memo exported components
+      // https://github.com/facebook/react/issues/18026#issuecomment-675900452
+      const displayName = Component.displayName || Component.type.displayName
+
+      const nextContexts =
+        typeof contexts !== 'function' ? contexts : await contexts()
+
+      window.__STUDIO_CONTEXTS__ = nextContexts
+      window.__STUDIO_COMPONENT__ = Component
+
+      const EnhanceComponent = withContext(
+        nextContexts.default,
+        nextContexts
+      )(Component)
+
+      !displayName && console.error('[sui-Test] Component without displayName') // eslint-disable-line
+
+      const NextComponent = props => (
+        <SUIContext.Provider value={nextContexts.default}>
+          <EnhanceComponent {...props} />
+        </SUIContext.Provider>
+      )
+      hoistNonReactStatics(NextComponent, Component)
+
+      NextComponent.displayName = displayName
+      window[displayName] = NextComponent
+
+      importTest()
+        .then(() => window.mocha.run(setFailures))
+        .catch(() => setNotFoundTest(true))
+    })
+  }, [contexts, importComponent, importTest])
+
+  if (notFoundTest) return <h1>No tests found</h1>
+
+  return (
+    <div className={classnames}>
+      <div id="mocha" />
+    </div>
+  )
+}
+
+Test.propTypes = {
+  contexts: PropTypes.object,
+  importComponent: PropTypes.func,
+  importTest: PropTypes.func,
+  open: PropTypes.bool
+}
+export default Test
