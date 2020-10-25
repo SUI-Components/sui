71622229f60b7618dcecdafed5edd775901419dc
midudev <miduga@gmail.com>
2020-10-25 20:31:43 +0100

    feat(sui-bundler): refactor how to get config

--- packages/sui-bundler/shared/config.js
+++ packages/sui-bundler/shared/config.js
@@ -1,6 +1,11 @@
-const {config} = require('./')
+/* Extract sui-bundler from package.json -> "config": {"sui-bundler": { ... }} */
+const {
+  config: packageJsonConfig = {}
+} = require(`${process.cwd()}/package.json`)
+const {'sui-bundler': config = {}} = packageJsonConfig
 
 const {extractComments, sourcemaps} = config
 
+exports.config = config
 exports.extractComments = extractComments || false
-exports.sourceMap = sourcemaps && sourcemaps.prod ? sourcemaps.prod : 'none'
+exports.sourceMap = (sourcemaps && sourcemaps.prod) || 'none'
