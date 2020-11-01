2e519385b926377e6e7f6392dff5c249f7d701ab
midudev <miduga@gmail.com>
2020-11-01 19:23:40 +0100

    Use correct state for new location

--- packages/sui-react-router/src/internal/createLocation.js
+++ packages/sui-react-router/src/internal/createLocation.js
@@ -1,8 +1,8 @@
 import {createLocation as createLocationFromHistory} from 'history'
 import parseLocationSearch from './parseLocationSearch'
 
-export default function createLocation(locationAsString) {
-  const location = createLocationFromHistory(locationAsString)
+export default function createLocation(locationAsString, locationState) {
+  const location = createLocationFromHistory(locationAsString, locationState)
   location.query = parseLocationSearch(location)
   return location
 }
