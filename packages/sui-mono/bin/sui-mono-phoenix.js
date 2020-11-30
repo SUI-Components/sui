2143dd68c1effd87f65bcdcd1a14fa4729fbdb74
midudev <miduga@gmail.com>
2020-11-30 17:57:13 +0100

    feat(sui-mono): use new @s-ui/helpers to reuse code and upgrade dependencies

--- packages/sui-mono/bin/sui-mono-phoenix.js
+++ packages/sui-mono/bin/sui-mono-phoenix.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
 /* eslint no-console:0 */
-require('colors')
 const program = require('commander')
-const figures = require('figures')
-const {basename} = require('path')
-const {default: Queue} = require('p-queue')
-const logUpdate = require('log-update')
-const execa = require('execa')
+const {getSpawnPromise, parallelSpawn, showError} = require('@s-ui/helpers/cli')
+
 const config = require('../src/config')
 
 const DEFAULT_CHUNK = 5
@@ -62,16 +58,22 @@ program
 const {
   audit = true,
   chunk = DEFAULT_CHUNK,
-  ci = false,
+  ci = Boolean(process.env.CI),
   progress = true,
   production = false,
   root = true,
   scope: scopeArgument
 } = program
 
+const NPM_BIN = 'npm'
 const NPM_CMD = [
-  'npm',
-  ['install', audit ? '' : '--no-audit', production ? '--production' : '']
+  NPM_BIN,
+  [
+    'install',
+    audit ? '' : '--no-audit',
+    production ? '--production' : '',
+    progress ? '' : '--no-progress'
+  ]
 ]
 const RIMRAF_CMD = [
   require.resolve('rimraf/bin'),
@@ -81,78 +83,50 @@ const RIMRAF_CMD = [
 console.info(`[sui-mono] CI mode enabled: ${ci}`)
 
 /**
- * Execute needed commands to install packages
- * @param {Object} params
- * @param {string=} params.cwd
- * @param {string=} params.stdin
+ * Create needed commands to install packages
+ * @param {string} cwd
  */
-const installPackages = ({cwd = undefined, stdin = 'inherit'} = {}) => {
-  const executionParams = {cwd, stdin}
+const createInstallPackagesCommand = cwd => {
+  const executionParams = {cwd}
   if (ci) {
-    const [npm] = NPM_CMD
-    return execute(
-      [npm, ['install', ...CI_FLAGS, production ? '--production' : '']],
-      executionParams
-    )
+    const commandArgs = [
+      'install',
+      ...CI_FLAGS,
+      production ? '--production' : ''
+    ]
+    return [NPM_BIN, commandArgs, executionParams]
   }
 
-  return execute(RIMRAF_CMD, executionParams).then(() =>
-    execute(NPM_CMD, executionParams)
-  )
+  return [...NPM_CMD, executionParams]
 }
 
 /**
- * Execute command abstraction
- * @param {Array<string | string[]>} cmd Array with the executable and the arguments to pass
- * @param {object} params
- * @param {string=} params.cwd
- * @param {string=} params.stdin
+ * Install root packages
+ * @return {Promise}
  */
-const execute = (cmd, {cwd, stdin}) => {
-  const [command, args] = cmd
-  return execa(command, args, {cwd, stdin, stderr: 'inherit'})
-}
+const installRootPackages = () => {
+  if (!root) return Promise.resolve()
 
-const concurrency = Number(chunk)
-const queue = new Queue({concurrency})
-console.log(`[sui-mono] Using concurrency of: ${concurrency}`)
+  console.log(`Installing root packages...`)
+  return Promise.resolve()
+  // return getSpawnPromise(createInstallPackagesCommand(process.cwd()))
+}
 
 const executePhoenixOnPackages = () => {
-  // if we're on a monorepo, then we don't have packages to install
   if (config.isMonoPackage()) return
-
   const scopes = config.getScopesPaths(scopeArgument)
-  logUpdate(`${figures.play} Preparing ${scopes.length} packages to install...`)
 
-  scopes.map(cwd => {
-    const packageName = basename(cwd).grey
-    queue
-      .add(() => installPackages({cwd}))
-      .then(() => {
-        if (progress && !ci) {
-          const {size, pending} = queue
-          const totalPackages = scopes.length
-          const pendingPackages = size + pending
-          const installedPackages = totalPackages - pendingPackages
-          logUpdate(
-            `${figures.play} ${packageName}: ${installedPackages} of ${totalPackages} packages installed`
-          )
-        }
-      })
-      .catch(err => {
-        console.error(`Error installing ${packageName}:`)
-        console.error(err)
-      })
-  })
+  const removePackagesCommands = scopes.map(cwd => [...RIMRAF_CMD, {cwd}])
+  const installPackagesCommands = scopes.map(createInstallPackagesCommand)
 
-  return queue
-    .onIdle()
-    .then(() => logUpdate(`${figures.tick} Installed all packages`))
+  return parallelSpawn(removePackagesCommands, {
+    chunk,
+    title: 'rimraf'
+  }).then(() =>
+    parallelSpawn(installPackagesCommands, {chunk, title: 'npm install'})
+  )
 }
 
-logUpdate(`${figures.play} Installing root packages...`)
-;(root ? installPackages({stdin: 'inherit'}) : Promise.resolve())
+installRootPackages()
   .then(executePhoenixOnPackages)
-  .catch(error => {
-    console.error(error)
-  })
+  .catch(showError)
