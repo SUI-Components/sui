5341db8dba1c2ea5f4abaa9246e3099b3f400256
midudev <miduga@gmail.com>
2020-12-01 11:23:54 +0100

    fix(sui-domain): disable line for linter

--- packages/sui-domain/src/inherits.js
+++ packages/sui-domain/src/inherits.js
@@ -1,5 +1,5 @@
 export default function inheritsLoose(subClass, superClass) {
   subClass.prototype = Object.create(superClass.prototype)
   subClass.prototype.constructor = subClass
-  subClass.__proto__ = superClass
+  subClass.__proto__ = superClass // eslint-disable-line no-proto
 }
