d893bc496988794721000cf06b2bf56b46bb7310
midudev <miduga@gmail.com>
2020-12-01 11:08:21 +0100

    fix(sui-domain): avoid transforming with Babel to avoid problems with sui-test

--- packages/sui-domain/src/Entity.js
+++ packages/sui-domain/src/Entity.js
@@ -1,3 +1,14 @@
+import inherits from './inherits'
 import AnemicModel from './AnemicModel'
 
-export default class Entity extends AnemicModel {}
+const Entity = (function(_AnemicModel) {
+  inherits(Entity, _AnemicModel)
+
+  function Entity() {
+    return _AnemicModel.apply(this, arguments) || this
+  }
+
+  return Entity
+})(AnemicModel)
+
+export {Entity as default}
