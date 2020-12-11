738ac95e380bf407c2449b37b880bfb8bed04545
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2020-12-11 11:51:19 +0100

    Update packages/sui-ssr/server/criticalCss/index.js
    Co-authored-by: Ismael Ramon <desko27@gmail.com>

--- packages/sui-ssr/server/criticalCss/index.js
+++ packages/sui-ssr/server/criticalCss/index.js
@@ -8,7 +8,7 @@ let __CACHE__ = {}
 const __RETRYS_BY_HASH__ = {}
 const __MAX_RETRYS_BY_HASH__ = 3
 const CRITICAL_CSS_SERVICE_DOMAIN =
-  'https://critical-css-service.es-global-pro.schip.io/'
+  'https://critical-css-service.es-global-pro.schip.io'
 
 const generateMinimalCSSHash = routes => {
   return routes.reduce((acc, route) => {
