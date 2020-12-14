c46ae37cd059121b06c45d652544540b79c5d016
Joan León | @nucliweb <joan.leon@gmail.com>
2020-12-14 12:46:15 +0100

    fix(sui-hoc): fix isOpen initial state to withOpenToggle HOC

--- packages/sui-hoc/src/withOpenToggle.js
+++ packages/sui-hoc/src/withOpenToggle.js
@@ -6,7 +6,7 @@ export default BaseComponent => {
 
   return class WithOpenToggle extends Component {
     state = {
-      isOpen: false
+      isOpen: Boolean(this.props.isOpen)
     }
 
     static displayName = `WithOpenToggle(${displayName})`
