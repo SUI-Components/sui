fc31c637984835cd0ebc308df2decfa7c37df73f
Andrés Álvarez <a.alvarez.sor@gmail.com>
2020-12-18 10:30:03 +0100

    fix(sui-js): fix bowser map

--- packages/sui-js/src/ua-parser/index.js
+++ packages/sui-js/src/ua-parser/index.js
@@ -1,13 +1,12 @@
-import bowser, {PLATFORMS_MAP} from 'bowser'
+import bowser from 'bowser'
 
 export const stats = userAgent => {
   const ua = bowser.parse(userAgent)
-
   return {
-    isMobile: ua.platform.type === PLATFORMS_MAP.mobile,
+    isMobile: ua.platform.type === bowser.PLATFORMS_MAP.mobile,
     osName: ua.os.name,
     browserName: ua.browser.name,
     browserVersion: ua.browser.version,
-    isTablet: ua.platform.type === PLATFORMS_MAP.tablet
+    isTablet: ua.platform.type === bowser.PLATFORMS_MAP.tablet
   }
 }
