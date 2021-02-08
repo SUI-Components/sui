42b5dbbc4d721e7828aa4c5d9c1fe09c1777ba14
Julian Gernun <julian.gernun@adevinta.com>
2021-02-08 12:16:04 +0100

    chore(sui-ssr): html template test

---
+++ packages/sui-ssr/test/server/templateHtmlBuilderSpec.js
@@ -0,0 +1,19 @@
+import {expect} from 'chai'
+import {HtmlBuilder} from '../../server/template'
+
+describe('template builder', () => {
+  it('should hidrate config and initial props ', () => {
+    const html = HtmlBuilder.buildBody({
+      bodyAttributes: false,
+      bodyTplPart: '<body></body>',
+      reactString: false,
+      appConfig: {appConfig: 'appConfig'},
+      initialProps: {initialProps: 'initialProps'},
+      performance: false
+    })
+
+    expect(html).to.equal(
+      '<body><script>window.__APP_CONFIG__ = JSON.parse("{\\"appConfig\\":\\"appConfig\\"}");</script><script>window.__INITIAL_PROPS__ = JSON.parse("{\\"initialProps\\":\\"initialProps\\"}");</script></body>'
+    )
+  })
+})
