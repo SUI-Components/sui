c75f47d55aac91408a9d1f4d99de892451b4840c
Julian Gernun <julian.gernun@adevinta.com>
2021-02-08 15:38:49 +0100

    test(sui-ssr): add getInitialContextValue tests

--- packages/sui-ssr/test/server/initialContextValueSpec.js
+++ packages/sui-ssr/test/server/initialContextValueSpec.js
@@ -1,7 +1,32 @@
 import {expect} from 'chai'
+import {getInitialContextValue} from '../../server/initialContextValue'
 
-describe('initial context value', () => {
-  it('should', () => {
-    expect(true).to.equal(true)
+describe('#getInitialContextValue', () => {
+  const context = {
+    domain: {fakeDomain: 'fakeDomain'},
+    i18n: {i18n: 'fakeI18n'},
+    pde: {
+      getInitialValue: () => ({initialValue: 'initialValue'})
+    },
+    anotherContextWithoutInitialValue: {
+      key: 'value'
+    },
+    anotherContextWithInitialValue: {
+      key: 'withInitialValue',
+      getInitialValue: () => ({
+        some: 'data',
+        any: 2
+      })
+    }
+  }
+
+  it('should return an object with the output of the initialValue fn for every context', () => {
+    expect(getInitialContextValue(context)).to.deep.equal({
+      pde: {initialValue: 'initialValue'},
+      anotherContextWithInitialValue: {
+        some: 'data',
+        any: 2
+      }
+    })
   })
 })
