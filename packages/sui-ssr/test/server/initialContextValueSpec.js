0456b8a7af5baf28d280ca8207c555f27761d2c4
Julian Gernun <julian.gernun@adevinta.com>
2021-02-08 16:12:13 +0100

    test(sui-ssr): add initial prop even though its empty

--- packages/sui-ssr/test/server/initialContextValueSpec.js
+++ packages/sui-ssr/test/server/initialContextValueSpec.js
@@ -2,25 +2,25 @@ import {expect} from 'chai'
 import {getInitialContextValue} from '../../server/initialContextValue'
 
 describe('#getInitialContextValue', () => {
-  const context = {
-    domain: {fakeDomain: 'fakeDomain'},
-    i18n: {i18n: 'fakeI18n'},
-    pde: {
-      getInitialValue: () => ({initialValue: 'initialValue'})
-    },
-    anotherContextWithoutInitialValue: {
-      key: 'value'
-    },
-    anotherContextWithInitialValue: {
-      key: 'withInitialValue',
-      getInitialValue: () => ({
-        some: 'data',
-        any: 2
-      })
-    }
-  }
-
   it('should return an object with the output of the initialValue fn for every context', () => {
+    const context = {
+      domain: {fakeDomain: 'fakeDomain'},
+      i18n: {i18n: 'fakeI18n'},
+      pde: {
+        getInitialValue: () => ({initialValue: 'initialValue'})
+      },
+      anotherContextWithoutInitialValue: {
+        key: 'value'
+      },
+      anotherContextWithInitialValue: {
+        key: 'withInitialValue',
+        getInitialValue: () => ({
+          some: 'data',
+          any: 2
+        })
+      }
+    }
+
     expect(getInitialContextValue(context)).to.deep.equal({
       pde: {initialValue: 'initialValue'},
       anotherContextWithInitialValue: {
@@ -28,5 +28,14 @@ describe('#getInitialContextValue', () => {
         any: 2
       }
     })
+  })
+
+  it('should return nothing if no getInitialValue is set', () => {
+    const context = {
+      domain: {fakeDomain: 'fakeDomain'},
+      i18n: {i18n: 'fakeI18n'}
+    }
+
+    expect(getInitialContextValue(context)).to.deep.equal({})
   })
 })
--- packages/sui-ssr/test/server/templateHtmlBuilderSpec.js
+++ packages/sui-ssr/test/server/templateHtmlBuilderSpec.js
@@ -2,7 +2,7 @@ import {expect} from 'chai'
 import {HtmlBuilder} from '../../server/template'
 
 describe('template builder', () => {
-  it('should hydrate config and initial props ', () => {
+  it('should hydrate config and initial props', () => {
     const html = HtmlBuilder.buildBody({
       bodyAttributes: false,
       bodyTplPart: '<body></body>',
@@ -13,7 +13,7 @@ describe('template builder', () => {
     })
 
     expect(html).to.equal(
-      '<body><script>window.__APP_CONFIG__ = JSON.parse("{\\"appConfig\\":\\"appConfig\\"}");</script><script>window.__INITIAL_PROPS__ = JSON.parse("{\\"initialProps\\":\\"initialProps\\"}");</script></body>'
+      '<body><script>window.__APP_CONFIG__ = JSON.parse("{\\"appConfig\\":\\"appConfig\\"}");</script><script>window.__INITIAL_PROPS__ = JSON.parse("{\\"initialProps\\":\\"initialProps\\"}");</script><script>window.__INITIAL_CONTEXT_VALUE__ = JSON.parse("{}");</script></body>'
     )
   })
 
