b9dd7fb8aca844a752821e029fb45dd428c024ad
Julian Gernun <julian.gernun@adevinta.com>
2021-02-10 11:08:46 +0100

    feat(sui-ssr): use getInitialData

--- packages/sui-ssr/server/initialContextValue/index.js
+++ packages/sui-ssr/server/initialContextValue/index.js
@@ -1,7 +1,7 @@
 /**
  * In order to inject data into the html needed by any context provider
  * that later on needs that data to load as initial value when client side rendering,
- * sui-ssr will check and execute the context.getInitialValue fn
+ * sui-ssr will check and execute the context.getInitialData fn
  * and will inject it as window.__INITIAL_CONTEXT_VALUE__.[your context key]
  */
 
@@ -9,15 +9,15 @@ export const INITIAL_CONTEXT_VALUE = '__INITIAL_CONTEXT_VALUE__'
 
 /**
  * @param {Object[]} context
- * @param {function=} context.getInitialValue
+ * @param {function=} context.getInitialData
  * @returns {Object}
  */
 export const getInitialContextValue = context =>
   Object.keys(context)
     .filter(
-      contextKey => typeof context[contextKey].getInitialValue === 'function'
+      contextKey => typeof context[contextKey].getInitialData === 'function'
     )
     .reduce((acc, contextKey) => {
-      acc[contextKey] = context[contextKey].getInitialValue()
+      acc[contextKey] = context[contextKey].getInitialData()
       return acc
     }, {})
