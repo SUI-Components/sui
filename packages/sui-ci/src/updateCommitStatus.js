bdbee6021cc4590ed5d91de0f8246204303c03c9
midudev <miduga@gmail.com>
2021-05-19 13:37:35 +0200

    feat(packages/sui-ci): Get more info on GitHub Actions

--- packages/sui-ci/src/updateCommitStatus.js
+++ packages/sui-ci/src/updateCommitStatus.js
@@ -23,6 +23,8 @@ async function getApiBaseUrl() {
     `git config --get remote.origin.url`
   ).catch(() => 'git@github.com:sui/remote-url.git')
 
+  console.log({gitRemoteOriginUrl})
+
   const domain = getDomain(gitRemoteOriginUrl)
 
   return domain === 'github.com'
