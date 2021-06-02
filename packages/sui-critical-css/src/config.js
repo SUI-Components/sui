bc2f72e0822d350c9f5feb57291e6a2da00b7aaa
Rafael Moral Pareja <rafael.moral@adevinta.com>
2021-06-02 10:09:38 +0200

    feat(packages/sui-critical-css): use bot UA in order to prevent dynamic rendering strategies issues

--- packages/sui-critical-css/src/config.js
+++ packages/sui-critical-css/src/config.js
@@ -12,19 +12,19 @@ export const blockedResourceTypes = [
 export const devices = {
   m: {
     userAgent:
-      'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Mobile Safari/537.36',
+      'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
     width: 360,
     height: 640
   },
   t: {
     userAgent:
-      'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1',
+      'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
     width: 768,
     height: 1024
   },
   d: {
     userAgent:
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
     width: 1024,
     height: 768
   }
