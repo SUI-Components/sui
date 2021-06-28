a086041f80e2b07dd582bec1c0ed30bbeab107a3
midudev <miduga@gmail.com>
2021-06-28 16:07:01 +0200

    feat(packages/sui-react-context): Migrate @s-ui/react-context to TS

---
+++ packages/sui-react-context/src/index.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import hoistNonReactStatics from 'hoist-non-react-statics'
+
+type SUIContextType = React.Context<any>
+& {
+  wrapper?: (Component: React.ComponentType<any>, displayName: string) => React.ComponentType<any>
+}
+
+const SUIContext: SUIContextType = React.createContext({})
+
+SUIContext.wrapper = (Component, displayName): React.ComponentType<any> => {
+  const WrappedComponent = (props: any): JSX.Element => (
+    <SUIContext.Consumer>
+      {context => <Component {...context} {...props} />}
+    </SUIContext.Consumer>
+  )
+
+  WrappedComponent.displayName = Component.displayName !== undefined
+    ? Component.displayName
+    : displayName
+
+  return hoistNonReactStatics(WrappedComponent, Component)
+}
+
+export default SUIContext
