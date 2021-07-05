fb34b05cf175026c341ffd96d28d74e0837b27c9
midudev <miduga@gmail.com>
2021-07-05 16:00:38 +0200

    feat(packages/sui-lint): Disable prop-types rules for TSX

--- packages/sui-lint/eslintrc.js
+++ packages/sui-lint/eslintrc.js
@@ -109,7 +109,8 @@ module.exports = {
         project: './tsconfig.json'
       },
       rules: {
-        'no-return-await': 'off',
+        'no-return-await': RULES.OFF,
+        'react/prop-types': RULES.OFF,
         'prettier/prettier': RULES.OFF
       }
     }
