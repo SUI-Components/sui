52eea8d30e20ebe86d3a4b989ad5422495dbdae7
midudev <miduga@gmail.com>
2021-08-04 12:03:04 +0200

    feat(packages/sui-test): Remove e2e from @s-ui/test
    BREAKING CHANGES:
    
    Remove cypress dependency and code so e2e is not longer supported in this package

--- packages/sui-test/bin/sui-test.js
+++ packages/sui-test/bin/sui-test.js
@@ -10,6 +10,4 @@ program.command('browser', 'Run tests in the browser').alias('b')
 
 program.command('server', 'Run tests in node').alias('s')
 
-program.command('e2e', 'Run end-to-end tests').alias('e')
-
 program.parse(process.argv)
