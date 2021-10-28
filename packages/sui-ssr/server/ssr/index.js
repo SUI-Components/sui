075db29c58e1dbae37515353ad01f32ef4af3831
Nacho Torrella <ignacio.torrella@schibsted.com>
2021-10-28 14:12:20 +0200

    feat(packages/sui-ssr): http redirect headers

--- packages/sui-ssr/server/ssr/index.js
+++ packages/sui-ssr/server/ssr/index.js
@@ -168,11 +168,15 @@ export default async (req, res, next) => {
 
   const {__HTTP__} = initialProps
   if (__HTTP__) {
-    const {redirectTo, redirectStatusCode, httpCookie} = __HTTP__
+    const {redirectTo, redirectStatusCode, httpCookie, headers} = __HTTP__
 
     if (httpCookie) {
       const [[field, value]] = Object.entries(httpCookie)
       res.cookie(field, value, {httpOnly: true})
+    }
+
+    if (headers) {
+      res.set(headers)
     }
 
     if (redirectTo) {
