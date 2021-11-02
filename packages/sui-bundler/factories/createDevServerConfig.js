2b7c28bae36b3496e375d47450634631046a4ace
Miguel Ángel Durán <miduga@gmail.com>
2021-11-02 11:54:44 +0100

    feat(packages/sui-bundler): Remove open for now

--- packages/sui-bundler/factories/createDevServerConfig.js
+++ packages/sui-bundler/factories/createDevServerConfig.js
@@ -29,7 +29,6 @@ module.exports = config => ({
   historyApiFallback: {
     disableDotRule: true
   },
-  open: true,
   onAfterSetupMiddleware(devServer) {
     // This service worker file is effectively a 'no-op' that will reset any
     // previous service worker registered for the same host:port combination.
