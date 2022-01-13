2262f90dd111ec3d796642d9d0700e7fd52eb969
Marco Antonio Ghiani <marco.ghiani@adevinta.com>
2022-01-13 17:37:17 +0100

    feat(packages/sui-widget-embedder): refactor rendering logic
    BREAKING CHANGE: refactor rendering widgets to prevent adding unused dom elements

--- packages/sui-widget-embedder/src-react/render.js
+++ packages/sui-widget-embedder/src-react/render.js
@@ -3,12 +3,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-export default function render(root, id = 'root') {
-  if (!document.getElementById(id)) {
-    const element = document.createElement('div')
-    element.setAttribute('id', id)
-    document.body.appendChild(element)
+export default function render(...widgets) {
+  widgets.forEach(renderSingleWidget)
+}
+
+/*******************************/
+
+function renderSingleWidget(widget) {
+  const [root, {selector, renderMultiple = false}] = widget
+
+  // Assert a selector is provided for the widget
+  if (!selector) {
+    return console.warn(`[Widget] You must define a selector to use the Widget`)
   }
 
-  ReactDOM.render(<div>{root}</div>, document.getElementById(id))
+  // Assert at least a node exists for the provided selector
+  const nodes = document.querySelectorAll(selector)
+  if (!nodes.length) {
+    return console.warn(`[Widget] unable find nodes using selector ${selector}`)
+  }
+
+  /**
+   * Depending on renderMultiple, get the full array or only the first one
+   */
+  const nodesToRender = renderMultiple ? Array.from(nodes) : [nodes[0]]
+
+  nodesToRender.forEach(node => ReactDOM.render(root, node))
 }
