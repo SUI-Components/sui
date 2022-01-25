2cc138d05e8e785a24e60026b9d745a32d08229b
Marco Antonio Ghiani <marco.ghiani@adevinta.com>
2022-01-25 10:22:18 +0100

    Revert "feat(packages/sui-widget-embedder): refactor rendering logic"
    This reverts commit 2262f90dd111ec3d796642d9d0700e7fd52eb969.

--- packages/sui-widget-embedder/src-react/Widget.js
+++ packages/sui-widget-embedder/src-react/Widget.js
@@ -1,15 +1,51 @@
+/* eslint-disable no-console */
+
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import Context from '@s-ui/react-context'
+import {useMount} from '@s-ui/react-hooks'
 
-export default function Widget({children, context = {}, isVisible = true}) {
-  return (
-    isVisible && <Context.Provider value={context}>{children}</Context.Provider>
+function renderWidgetOnDOM({children, context, node}) {
+  ReactDOM.render(
+    <Context.Provider value={context}>{children}</Context.Provider>,
+    node
   )
+}
+
+export default function Widget({
+  children,
+  context = {},
+  isVisible = true,
+  selector,
+  renderMultiple = false
+}) {
+  useMount(function() {
+    if (!selector) {
+      return console.warn(
+        `[Widget] You must define a selector to use the Widget`
+      )
+    }
+
+    const nodes = document.querySelectorAll(selector)
+    if (!nodes.length) {
+      return console.warn(
+        `[Widget] unable find nodes using selector ${selector}`
+      )
+    }
+
+    // depending on renderMultiple, get the full array or only the first one
+    const nodesToRender = renderMultiple ? [].slice.call(nodes) : [nodes[0]]
+
+    isVisible &&
+      nodesToRender.map(node => renderWidgetOnDOM({children, context, node}))
+  })
+
+  return null
 }
 
 Widget.propTypes = {
   children: PropTypes.element.isRequired,
   context: PropTypes.object,
-  isVisible: PropTypes.bool
+  isVisible: PropTypes.bool,
+  selector: PropTypes.string.isRequired
 }
