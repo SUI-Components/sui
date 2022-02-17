4046c43ce622eb666af3556bf1d6f81b151af07a
Miguel Ángel Durán <miduga@gmail.com>
2022-02-17 14:38:56 +0100

    refactor(packages/sui-test-contract): Fix linter error

--- packages/sui-test-contract/utils/index.js
+++ packages/sui-test-contract/utils/index.js
@@ -1,7 +1,4 @@
 import childProcess from 'child_process'
 
 export const exec = command =>
-  childProcess
-    .execSync(command)
-    .toString()
-    .trim()
+  childProcess.execSync(command).toString().trim()
