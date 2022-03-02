d4cdff467db581bcbd08ec3a9a2543e6b76265d3
Marco Antonio Ghiani <marco.ghiani@adevinta.com>
2022-03-02 17:30:25 +0100

    feat(packages/sui-js-compiler): add ignore option

--- packages/sui-js-compiler/index.js
+++ packages/sui-js-compiler/index.js
@@ -3,6 +3,7 @@
 'use strict'
 
 const {transformFile} = require('@swc/core')
+const program = require('commander')
 const fg = require('fast-glob')
 const fs = require('fs-extra')
 const defaultConfig = require('./swc-config.js')
@@ -13,10 +14,33 @@ const compileFile = async file => {
   fs.outputFile(outputPath, code)
 }
 
+const commaSeparatedList = value => value.split(',')
+
+const DEFAULT_SRC = './src/**/*.{js,jsx}'
+
+program
+  .option(
+    '--ignore [glob]',
+    'List of patterns to ignore during the compilation',
+    commaSeparatedList
+  )
+  .on('--help', () => {
+    console.log('  Examples:')
+    console.log('')
+    console.log('    $ sui-js-compiler')
+    console.log('    $ sui-js-compiler ./custom-folder')
+    console.log('    $ sui-js-compiler --ignore="./src/**/*Spec.js"')
+    console.log('')
+  })
+  .parse(process.argv)
+
+const {ignore = []} = program.opts()
+const pattern = program.args[0] || DEFAULT_SRC
+
 ;(async () => {
   console.time('[sui-js-compiler]')
 
-  const files = await fg('./src/**/*.{js,jsx}')
+  const files = await fg(pattern, {ignore})
   files.forEach(compileFile)
 
   console.timeEnd('[sui-js-compiler]')
