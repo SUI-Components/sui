657ff482184fe41b0a75308f6193e57a7d44258b
Miguel Ángel Durán <miduga@gmail.com>
2022-03-03 11:39:28 +0100

    feat(packages/sui-js-compiler): Adapt changes with ignore files

--- packages/sui-js-compiler/bin.js
+++ packages/sui-js-compiler/bin.js
@@ -1,7 +1,28 @@
+import program from 'commander'
 import fg from 'fast-glob'
 import {compileAndOutputFile} from './index.js'
 
+const commaSeparatedList = value => value.split(',')
+
+program
+  .option(
+    '--ignore [glob]',
+    'List of patterns to ignore during the compilation',
+    commaSeparatedList
+  )
+  .on('--help', () => {
+    console.log('  Examples:')
+    console.log('')
+    console.log('    $ sui-js-compiler')
+    console.log('    $ sui-js-compiler ./custom-folder')
+    console.log('    $ sui-js-compiler --ignore="./src/**/*Spec.js"')
+    console.log('')
+  })
+  .parse(process.argv)
+
+const {ignore = []} = program.opts()
+
 console.time('[sui-js-compiler]')
-const files = await fg('./src/**/*.{js,jsx}')
+const files = await fg('./src/**/*.{js,jsx}', {ignore})
 files.forEach(compileAndOutputFile)
 console.timeEnd('[sui-js-compiler]')
