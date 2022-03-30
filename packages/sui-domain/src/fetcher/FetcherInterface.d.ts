18069b258b840a1d441fcb1e018bd44a43418cef
Oriol Egea <oriol.egea@adevinta.com>
2022-03-30 10:19:46 +0200

    feat(packages/sui-domain): Add a way to globally intercept errors on all axios instances

--- packages/sui-domain/src/fetcher/FetcherInterface.d.ts
+++ packages/sui-domain/src/fetcher/FetcherInterface.d.ts
@@ -1,7 +1,9 @@
 export default interface FetcherInterface {
+  delete: (url: string, options: object) => Promise<any>
   get: (url: string, options: object) => Promise<any>
+  patch: (url: string, body: string, options: object) => Promise<any>
   post: (url: string, body: string, options: object) => Promise<any>
   put: (url: string, body: string, options: object) => Promise<any>
-  patch: (url: string, body: string, options: object) => Promise<any>
-  delete: (url: string, options: object) => Promise<any>
+  setErrorInterceptor: (callback: Function) => void
+  setResponseInterceptor: (callback: Function) => void
 }
