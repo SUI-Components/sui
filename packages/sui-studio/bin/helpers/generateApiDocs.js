4c6a2c87d1c4679dfc1e8a3250d61c7e68e78b96
Miguel Ángel Durán <miduga@gmail.com>
2022-06-07 15:55:58 +0200

    feat(packages/sui-studio): Install huge dependency only on-demand

--- packages/sui-studio/bin/helpers/generateApiDocs.js
+++ packages/sui-studio/bin/helpers/generateApiDocs.js
@@ -1,33 +1,33 @@
 const path = require('node:path')
-const fg = require('fast-glob')
-const fs = require('fs-extra')
-const reactDocs = require('react-docgen')
+const {promisify} = require('node:util')
 
-module.exports = function generateApiDocs() {
+const fg = require('fast-glob')
+const exec = promisify(require('child_process').exec)
+
+module.exports = async function generateApiDocs() {
   console.log('[sui-studio] Generating API documentation for components...')
   console.time('[sui-studio] API generation took')
 
   const components = fg.sync('components/*/*/src/index.js', {deep: 4})
 
-  components.forEach(file => {
-    const source = fs.readFileSync(file, 'utf-8')
-    let docs = {}
-
-    try {
-      docs = reactDocs.parse(
-        source,
-        reactDocs.resolver.findAllComponentDefinitions
-      )
-    } catch (e) {
-      console.warn(`[sui-studio] Couldn't generate API docs for ${file}`)
-    }
-
+  const promises = components.map(file => {
     const outputFile = file.replace('index.js', 'definitions.json')
-    fs.writeFileSync(
-      path.resolve(process.cwd(), `public/${outputFile}`),
-      JSON.stringify(docs, null, 2)
-    )
+    const fullOutputFile = path.resolve(process.cwd(), `public/${outputFile}`)
+
+    return exec(
+      `npx --yes react-docgen@5 ${file} --resolver findAllComponentDefinitions -o ${fullOutputFile}`
+    ).catch(() => {
+      console.warn(
+        `[sui-studio] Error generating API documentation for ${file}`
+      )
+    })
   })
 
-  console.timeEnd('[sui-studio] API generation took')
+  console.log(
+    `[sui-studio] Generated API documentation for ${components.length} components`
+  )
+
+  return Promise.all(promises).then(() => {
+    console.timeEnd('[sui-studio] API generation took')
+  })
 }
