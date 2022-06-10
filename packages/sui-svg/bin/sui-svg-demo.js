6edeb4d54ad73d103dba3999fa972dd607246ab4
Miguel Ángel Durán <miduga@gmail.com>
2022-06-10 13:01:03 +0200

    feat(packages/sui-svg): Better logging for Vite

--- packages/sui-svg/bin/sui-svg-demo.js
+++ packages/sui-svg/bin/sui-svg-demo.js
@@ -8,30 +8,33 @@ const {pathname: root} = new URL('../src', import.meta.url)
 
 const icons = resolve(process.cwd(), 'lib', '_demo.js')
 
-;(async () => {
-  const server = await createServer({
-    root,
-    optimizeDeps: {
-      include: [
-        'classnames',
-        'prop-types',
-        'react',
-        'react/jsx-runtime',
-        'react-dom'
-      ]
-    },
-    resolve: {
-      alias: [
-        {
-          find: '@s-ui/svg-icons',
-          replacement: () => icons
-        },
-        {
-          find: /^~.+/,
-          replacement: val => val.replace(/^~/, '')
-        }
-      ]
-    }
-  })
-  await server.listen()
-})()
+console.log('[sui-svg] Preparing server with icons...')
+
+const server = await createServer({
+  root,
+  optimizeDeps: {
+    include: [
+      'classnames',
+      'prop-types',
+      'react',
+      'react/jsx-runtime',
+      'react-dom'
+    ]
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@s-ui/svg-icons',
+        replacement: () => icons
+      },
+      {
+        find: /^~.+/,
+        replacement: val => val.replace(/^~/, '')
+      }
+    ]
+  }
+})
+await server.listen()
+
+console.log('[sui-svg] Server is ready! ✅')
+server.printUrls()
