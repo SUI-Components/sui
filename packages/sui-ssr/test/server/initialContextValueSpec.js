ece2c8d2ce754666ac1502bbed077b981cb83605
Miguel Ángel Durán <miduga@gmail.com>
2022-06-10 11:43:42 +0200

    refactor(Root): Fix linter errors and warnings

--- packages/sui-ssr/test/server/initialContextValueSpec.js
+++ packages/sui-ssr/test/server/initialContextValueSpec.js
@@ -1,4 +1,5 @@
 import {expect} from 'chai'
+
 import {getInitialContextValue} from '../../server/initialContextValue'
 
 describe('#getInitialContextValue', () => {
--- packages/sui-ssr/test/server/templateHtmlBuilderSpec.js
+++ packages/sui-ssr/test/server/templateHtmlBuilderSpec.js
@@ -1,4 +1,5 @@
 import {expect} from 'chai'
+
 import {HtmlBuilder} from '../../server/template'
 
 describe('template builder', () => {
