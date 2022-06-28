35aeb73bfc06d97c40224aed91c2e9222de3656b
Ferran Simon <ferran.simon@adevinta.com>
2022-06-28 11:28:27 +0200

    feat(packages/sui-js): inmutable shuffle array

--- packages/sui-js/src/array/index.js
+++ packages/sui-js/src/array/index.js
@@ -1,9 +1,12 @@
 function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
+  const result = [...array] // Shallow copy, values are copied by reference
+  for (let i = result.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
-    const temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
+    const temp = result[i]
+    result[i] = result[j]
+    result[j] = temp
   }
+
+  return result
 }
 export {shuffle}
--- packages/sui-js/test/arraySpec.js
+++ packages/sui-js/test/arraySpec.js
@@ -1,14 +1,19 @@
 /* eslint-env mocha */
 import {expect} from 'chai'
+import sinon from 'sinon'
 
 import {shuffle} from '../src/array/index.js'
 
 describe('@s-ui/js', () => {
   describe('array:shuffleArray', () => {
+    before(() => {
+      sinon.stub(Math, 'random').returns(0.5)
+    })
+
     it('should shuffle an array', () => {
-      const params = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-      shuffle(params)
-      expect(params).to.be.not.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+      const params = [1, 2, 3, 4, 5]
+      expect(shuffle(params)).to.be.equal([1, 4, 2, 5, 3])
+      expect(params).to.be.equal([1, 2, 3, 4, 5])
     })
   })
 })
