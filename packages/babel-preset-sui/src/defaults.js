a0d6d990ab3736fa2d5959d20fea3b2d0b551534
Miguel Ángel Durán <miduga@gmail.com>
2022-07-19 13:06:11 +0200

    feat(packages/babel-preset-sui): Compile for target node 16

--- packages/babel-preset-sui/src/defaults.js
+++ packages/babel-preset-sui/src/defaults.js
@@ -1,9 +1,20 @@
 module.exports = {
-  DEFAULT_TARGETS: {
+  DEFAULT_LEGACY_BROWSER_TARGETS: {
     chrome: '41', // previous googlebot version
     firefox: '60', // esr
     ie: '11',
     ios: '10',
     safari: '10'
+  },
+  DEFAULT_BROWSER_TARGETS: {
+    chrome: '72',
+    edge: '79',
+    safari: '14',
+    firefox: '80',
+    opera: '60',
+    ios: '14'
+  },
+  DEFAULT_SERVER_TARGETS: {
+    node: '16'
   }
 }
