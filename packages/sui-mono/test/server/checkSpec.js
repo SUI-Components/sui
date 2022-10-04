e2574017e62cd2719ccfb31b325594a788d6bf62
jordi.munoz@adevinta.com <jordi.munoz@adevinta.com>
2022-10-04 19:49:15 +0200

    test(packages/sui-mono): add specs to test trigger of releases

--- packages/sui-mono/test/server/checkSpec.js
+++ packages/sui-mono/test/server/checkSpec.js
@@ -1,6 +1,9 @@
 import {expect} from 'chai'
 
-import {isCommitBreakingChange} from '../../src/check.js'
+import {
+  isCommitBreakingChange,
+  isCommitReleaseTrigger
+} from '../../src/check.js'
 
 describe('check', () => {
   describe('isCommitBreakingChange', () => {
@@ -10,6 +13,32 @@ describe('check', () => {
 
     it('returns false if commit is not breaking change', () => {
       expect(isCommitBreakingChange({footer: ''})).to.equal(false)
+    })
+  })
+
+  describe('isCommitReleaseTrigger', () => {
+    it('returns true if commit is of type feat', () => {
+      expect(isCommitReleaseTrigger({type: 'feat'})).to.equal(true)
+    })
+
+    it('returns true if commit is of type fix', () => {
+      expect(isCommitReleaseTrigger({type: 'fix'})).to.equal(true)
+    })
+
+    it('returns true if commit is of type perf', () => {
+      expect(isCommitReleaseTrigger({type: 'perf'})).to.equal(true)
+    })
+
+    it('returns true if commit is of type refactor', () => {
+      expect(isCommitReleaseTrigger({type: 'refactor'})).to.equal(true)
+    })
+
+    it('returns false if commit is of type chore', () => {
+      expect(isCommitReleaseTrigger({type: 'chore'})).to.equal(false)
+    })
+
+    it('returns false if commit is of type test', () => {
+      expect(isCommitReleaseTrigger({type: 'test'})).to.equal(false)
     })
   })
 
