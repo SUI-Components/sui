2c39a4add4addf74b5c4ede8fca889b2b8783a89
Arturo López Sanabria <arturo.lopez@adevinta.com>
2022-10-13 15:28:08 +0200

    feat(packages/sui-studio): Remove title of the Themes tabs
    Remove redundant information from studio

--- packages/sui-studio/src/components/demo/Tabs.js
+++ packages/sui-studio/src/components/demo/Tabs.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 export default function Tabs({children, title}) {
   return (
     <ul className="sui-StudioTabs sui-StudioTabs--small">
-      <li className="sui-StudioTabs-title">{title}</li>
       {children}
     </ul>
   )
