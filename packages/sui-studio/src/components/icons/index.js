c553b73534aaae10f31344e836413ebd189a712b
Arturo López Sanabria <arturo.lopez@adevinta.com>
2022-10-15 23:35:37 +0200

    fix(packages/sui-studio): Add a fill to the buttons icon
    ISSUES CLOSED: #1500

--- packages/sui-studio/src/components/icons/index.js
+++ packages/sui-studio/src/components/icons/index.js
@@ -20,7 +20,7 @@ export const iconCode = (
 export const iconMenu = (
   <svg viewBox="0 0 24 24">
     <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="#444" />
   </svg>
 )
 
