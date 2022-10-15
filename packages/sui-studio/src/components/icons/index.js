8495fe7ec576162f9b7dceeeffb8e2c4150596f3
Arturo López Sanabria <arturo.lopez@adevinta.com>
2022-10-16 01:21:29 +0200

    fix(packages/sui-studio): change display from flex to block
    ISSUES CLOSED: #1500

--- packages/sui-studio/src/components/icons/index.js
+++ packages/sui-studio/src/components/icons/index.js
@@ -18,9 +18,16 @@ export const iconCode = (
 )
 
 export const iconMenu = (
-  <svg viewBox="0 0 24 24">
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="#444" />
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="3" y="6" width="18" height="2" fill="#444444" />
+    <rect x="3" y="11" width="18" height="2" fill="#444444" />
+    <rect x="3" y="16" width="18" height="2" fill="#444444" />
   </svg>
 )
 
