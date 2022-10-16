e7263059aa492da08c89a06c8d2a4ee055592f88
Arturo López Sanabria <arturo.lopez@adevinta.com>
2022-10-16 10:53:22 +0200

    fix(packages/sui-studio): adjust icon size
    adjust icon size and remove unnecesary code lines
    
    ISSUES CLOSED: #1500

--- packages/sui-studio/src/components/icons/index.js
+++ packages/sui-studio/src/components/icons/index.js
@@ -18,13 +18,7 @@ export const iconCode = (
 )
 
 export const iconMenu = (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <svg viewBox="0 0 24 24">
     <rect x="3" y="6" width="18" height="2" fill="#444444" />
     <rect x="3" y="11" width="18" height="2" fill="#444444" />
     <rect x="3" y="16" width="18" height="2" fill="#444444" />
