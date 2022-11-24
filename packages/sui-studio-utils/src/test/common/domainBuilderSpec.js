64be5082244910455ee8d17e7f073527a1d262b9
Ferran Simon <ferran.simon@MacBook-Pro-de-Ferran.local>
2022-11-24 15:22:10 +0100

    feat(packages/sui-studio-utils): Be able to mock a domain config

---
+++ packages/sui-studio-utils/src/test/common/domainBuilderSpec.js
@@ -0,0 +1,30 @@
+/* eslint-env mocha */
+import {expect} from 'chai'
+
+import {DomainBuilder} from '../../index.js'
+
+describe('DomainBuilder', () => {
+  describe('when a useCase is mocked', () => {
+    const domainBuilder = DomainBuilder.extend({domain: {get: () => {}}})
+    it('should return the mocked response', async () => {
+      const domain = domainBuilder
+        .for({useCase: 'get_products'})
+        .respondWith({success: 'mocked-response'})
+        .build()
+      expect(await domain.get('get_products').execute()).to.equal(
+        'mocked-response'
+      )
+    })
+  })
+
+  describe('when config is mocked', () => {
+    const domainBuilder = DomainBuilder.extend({
+      domain: {get: () => {}},
+      config: 'moked-config'
+    })
+    it('should return the mocked response', async () => {
+      const domain = domainBuilder.build()
+      expect(await domain.get('config')).to.equal('moked-config')
+    })
+  })
+})
