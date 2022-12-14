a00b2517583a6ca7b8f87de7b777ba983b375f36
Giorgio de la Barrera <giodelabarrera@gmail.com>
2022-12-14 09:03:10 +0100

    feat(packages/sui-test): add default sui jest config

--- packages/sui-test/bin/sui-test-jest.js
+++ packages/sui-test/bin/sui-test-jest.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
+const {hasFile} = require('./jest/utils.js')
+
+process.env.BABEL_ENV = 'test'
+process.env.NODE_ENV = 'test'
 
 const args = process.argv.slice(2)
 
-const config = []
+const config =
+  !args.includes('--config') && !hasFile('jest.config.js')
+    ? ['--config', JSON.stringify(require('./jest/config.js'))]
+    : []
 
 require('jest').run([...config, ...args])
