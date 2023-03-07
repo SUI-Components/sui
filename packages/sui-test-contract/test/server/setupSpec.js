5be5d2c79a1908d0be58621e3d5da561a4e1128c
arnau-rius <arnau.rius.contact@gmail.com>
2023-03-07 14:51:37 +0100

    test(sui-test-contract): when response is not 2XX

--- packages/sui-test-contract/test/server/setupSpec.js
+++ packages/sui-test-contract/test/server/setupSpec.js
@@ -32,6 +32,7 @@ const getAppleHandler = rest.get(
     return res(ctx.status(200), ctx.json(response))
   }
 )
+const notFoundResponse = {code: 'not-found'}
 
 setupContractTests({
   apiUrl: 'http://localhost:8181',
@@ -84,6 +85,16 @@ setupContractTests({
         ),
         response: gardenResponse,
         addMatchingRules: true
+      },
+      {
+        endpoint: '/apples/search/garden',
+        description: 'A request for getting a garden that fails',
+        state: 'I have not garden',
+        handler: rest.get(
+          'http://localhost:8181/apples/search/garden',
+          (req, res, ctx) => res(ctx.status(404), ctx.json(notFoundResponse))
+        ),
+        response: notFoundResponse
       }
     ]
   }
