b4d234e980492de24d6bb30450c7586ab64a9cc0
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-04-26 10:41:05 +0200

    feat(packages/sui-js-compiler): add legacy option

--- packages/sui-js-compiler/swc-config.js
+++ packages/sui-js-compiler/swc-config.js
@@ -1,37 +1,55 @@
-export default {
-  minify: true,
-  jsc: {
-    parser: {
-      syntax: 'ecmascript',
-      jsx: true,
+const DEFAULT_LEGACY_BROWSER_TARGETS = {
+  ie: '11'
+}
+
+const DEFAULT_BROWSER_TARGETS = {
+  chrome: '72',
+  edge: '79',
+  safari: '14',
+  firefox: '80',
+  opera: '60',
+  ios: '14'
+}
+
+export const getSWCConfig = ({supportLegacyBrowsers}) => {
+  const targets = supportLegacyBrowsers
+    ? DEFAULT_LEGACY_BROWSER_TARGETS
+    : DEFAULT_BROWSER_TARGETS
+  const target = supportLegacyBrowsers ? 'es5' : 'es2018'
+
+  return {
+    minify: true,
+    jsc: {
+      parser: {
+        syntax: 'ecmascript',
+        jsx: true,
+        dynamicImport: true,
+        privateMethod: true,
+        functionBind: true,
+        exportDefaultFrom: true,
+        exportNamespaceFrom: true,
+        decorators: true,
+        decoratorsBeforeExport: true,
+        topLevelAwait: true,
+        importMeta: true
+      },
+      transform: {
+        legacyDecorator: true,
+        react: {
+          useBuiltins: true,
+          runtime: 'automatic'
+        }
+      },
+      target,
+      loose: true,
+      externalHelpers: true
+    },
+    env: {
+      targets,
       dynamicImport: true,
-      privateMethod: true,
-      functionBind: true,
-      exportDefaultFrom: true,
-      exportNamespaceFrom: true,
-      decorators: true,
-      decoratorsBeforeExport: true,
-      topLevelAwait: true,
-      importMeta: true
-    },
-    transform: {
-      legacyDecorator: true,
-      react: {
-        useBuiltins: true,
-        runtime: 'automatic'
-      }
-    },
-    target: 'es5',
-    loose: true,
-    externalHelpers: true
-  },
-  env: {
-    targets: {
-      ie: '11'
-    },
-    dynamicImport: true,
-    loose: true,
-    mode: 'entry',
-    coreJs: 3
+      loose: true,
+      mode: 'entry',
+      coreJs: 3
+    }
   }
 }
