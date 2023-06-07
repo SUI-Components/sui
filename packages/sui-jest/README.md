f15be4c48d8b1ade07a407c0781c79b181b4a52b
Giorgio de la Barrera <giodelabarrera@gmail.com>
2023-06-07 11:19:14 +0200

    feat(packages/sui-jest): add wrapper for jest

---
+++ packages/sui-jest/README.md
@@ -0,0 +1,9 @@
+# sui-jest
+
+It is mainly a wrapper around [Jest](https://jestjs.io/).
+
+## Installation
+
+```sh
+npm install @s-ui/jest --save-dev
+```
---
+++ packages/sui-jest/bin/sui-jest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const {hasFile} = require('./utils.js')
+
+process.env.BABEL_ENV = 'test'
+process.env.NODE_ENV = 'test'
+
+const args = process.argv.slice(2)
+
+const config =
+  !args.includes('--config') && !hasFile('jest.config.js')
+    ? ['--config', JSON.stringify(require('./config.js'))]
+    : []
+
+require('jest').run([...config, ...args])
