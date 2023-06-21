ca3f4b3b6add3d0464aad407d9100f7ea5992667
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-06-21 11:31:04 +0200

    feat(packages/sui-bundler): add support to typescript

--- packages/sui-bundler/bin/sui-bundler-dev.js
+++ packages/sui-bundler/bin/sui-bundler-dev.js
@@ -67,12 +67,7 @@ const start = async ({
 } = {}) => {
   clearConsole()
   // Warn and crash if required files are missing
-  if (
-    !checkRequiredFiles([
-      path.join(config.context, 'index.html'),
-      path.join(config.context, 'app.js')
-    ])
-  ) {
+  if (!checkRequiredFiles([path.join(config.context, 'index.html')])) {
     log.error(
       `✖ Required files are missing, create and index.html and app.js inside your src folder.`
     )
--- packages/sui-bundler/shared/module-rules-babel.js
+++ packages/sui-bundler/shared/module-rules-babel.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+const fs = require('fs-extra')
 const path = require('path')
 const {config} = require('./index.js')
 
@@ -5,28 +7,97 @@ const EXCLUDED_FOLDERS_REGEXP = new RegExp(
   `node_modules(?!${path.sep}@s-ui(${path.sep}studio)(${path.sep}workbench)?${path.sep}src)`
 )
 
-module.exports = ({isServer = false, supportLegacyBrowsers = true} = {}) => ({
-  test: /\.jsx?$/,
-  exclude: EXCLUDED_FOLDERS_REGEXP,
-  use: [
-    {
-      loader: require.resolve('babel-loader'),
-      options: {
-        cacheDirectory: true,
-        cacheCompression: false,
-        babelrc: false,
-        compact: true,
-        presets: [
-          [
-            require.resolve('babel-preset-sui'),
-            {
-              isServer,
-              isModern: !supportLegacyBrowsers,
-              targets: config.targets
+const getTSConfig = () => {
+  // Get TS config from the package dir.
+  const tsConfigPath = path.join(process.cwd(), 'tsconfig.json')
+  let tsConfig
+
+  try {
+    if (fs.existsSync(tsConfigPath)) {
+      tsConfig = JSON.parse(fs.readFileSync(tsConfigPath, {encoding: 'utf8'}))
+    }
+  } catch (err) {
+    console.error(err)
+  }
+
+  return tsConfig
+}
+
+module.exports = ({isServer = false, supportLegacyBrowsers = true} = {}) => {
+  const tsConfig = getTSConfig()
+  // If TS config exists in root dir, set TypeScript as enabled.
+  const isTypeScriptEnabled = Boolean(tsConfig)
+
+  return isTypeScriptEnabled
+    ? {
+        test: /\.(js|ts)x?$/,
+        exclude: EXCLUDED_FOLDERS_REGEXP,
+        use: [
+          {
+            loader: require.resolve('swc-loader'),
+            options: {
+              minify: true,
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                  dynamicImport: true,
+                  privateMethod: true,
+                  functionBind: true,
+                  exportDefaultFrom: true,
+                  exportNamespaceFrom: true,
+                  decorators: true,
+                  decoratorsBeforeExport: true,
+                  topLevelAwait: true,
+                  importMeta: true
+                },
+                transform: {
+                  legacyDecorator: true,
+                  react: {
+                    useBuiltins: true,
+                    runtime: 'automatic'
+                  }
+                },
+                target: 'es5',
+                loose: true,
+                externalHelpers: true
+              },
+              env: {
+                targets: {
+                  ie: '11'
+                },
+                dynamicImport: true,
+                loose: true,
+                mode: 'entry',
+                coreJs: 3
+              }
             }
-          ]
+          }
         ]
       }
-    }
-  ]
-})
+    : {
+        test: /\.jsx?$/,
+        exclude: EXCLUDED_FOLDERS_REGEXP,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              babelrc: false,
+              compact: true,
+              presets: [
+                [
+                  require.resolve('babel-preset-sui'),
+                  {
+                    isServer,
+                    isModern: !supportLegacyBrowsers,
+                    targets: config.targets
+                  }
+                ]
+              ]
+            }
+          }
+        ]
+      }
+}
