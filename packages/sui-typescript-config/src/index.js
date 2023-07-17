4d8160d0349d635a0960f6519873816478c25ec8
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2023-07-17 14:43:08 +0200

    feat(packages/sui-typescript-config): Create package
    Add several TS configs shared

---
+++ packages/sui-typescript-config/src/index.js
@@ -0,0 +1,56 @@
+const DEFAULT_LEGACY_BROWSER_TARGETS = {
+  ie: '11'
+}
+
+const DEFAULT_BROWSER_TARGETS = {
+  chrome: '98',
+  edge: '107',
+  safari: '14.1',
+  firefox: '108',
+  ios: '14.5'
+}
+
+const getSWCConfig = ({isModern, isTypeScript}) => {
+  const targets = isModern
+    ? DEFAULT_BROWSER_TARGETS
+    : DEFAULT_LEGACY_BROWSER_TARGETS
+  const syntaxOptions = isTypeScript
+    ? {syntax: 'typescript', tsx: true}
+    : {syntax: 'ecmascript', jsx: true}
+
+  return {
+    minify: true,
+    jsc: {
+      parser: {
+        ...syntaxOptions,
+        dynamicImport: true,
+        privateMethod: true,
+        functionBind: true,
+        exportDefaultFrom: true,
+        exportNamespaceFrom: true,
+        decorators: true,
+        decoratorsBeforeExport: true,
+        topLevelAwait: true,
+        importMeta: true
+      },
+      transform: {
+        legacyDecorator: true,
+        react: {
+          useBuiltins: true,
+          runtime: 'automatic'
+        }
+      },
+      loose: true,
+      externalHelpers: true
+    },
+    env: {
+      targets,
+      dynamicImport: true,
+      loose: true,
+      mode: 'entry',
+      coreJs: 3
+    }
+  }
+}
+
+module.exports.getSWCConfig = getSWCConfig
