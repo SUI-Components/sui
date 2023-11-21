c251cd114e6b577e0d31a1df3cc5531656e12a73
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-11-21 18:18:14 +0100

    chore(packages/sui-mockmock): apply new linting rules

--- packages/sui-mockmock/src/http/index.js
+++ packages/sui-mockmock/src/http/index.js
@@ -1,9 +1,6 @@
 import ClientMocker from './clientMocker'
 import ServerMocker from './serverMocker'
 
-const isNode =
-  Object.prototype.toString.call(
-    typeof process !== 'undefined' ? process : 0
-  ) === '[object process]'
+const isNode = Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]'
 
 export default isNode ? ServerMocker : ClientMocker
