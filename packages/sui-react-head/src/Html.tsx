99d0af8d741c7243d4a0582665ad4ebb61301402
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-12-18 10:40:13 +0100

    fix(packages/sui-react-head): fix linting issues

--- packages/sui-react-head/src/Html.tsx
+++ packages/sui-react-head/src/Html.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import { Meta } from 'react-head'
+import {Meta} from 'react-head'
 
 export const HTML_ATTRIBUTES_KEY = 'htmlattributes'
 const isClient = typeof window !== 'undefined'
@@ -9,10 +9,10 @@ interface HtmlProps {
   attributes: object
 }
 
-const Html: React.FC<HtmlProps> = ({ attributes = {} }) => {
+const Html: React.FC<HtmlProps> = ({attributes = {}}) => {
   if (isClient) return null
   // on the server, use the Meta tag to extract later
-  const metaProps = { ...attributes, name: HTML_ATTRIBUTES_KEY }
+  const metaProps = {...attributes, name: HTML_ATTRIBUTES_KEY}
   return <Meta {...metaProps} />
 }
 
