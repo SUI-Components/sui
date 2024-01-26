0d9713745a511890dc4e2e9b2d70b477ae4ccab3
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2024-01-26 09:50:18 +0100

    chore(Root): update script for tagging releases in prs

--- scripts/publish-tagged-packages.mjs
+++ scripts/publish-tagged-packages.mjs
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-/* eslint-disable no-console */
 
+import fs from 'node:fs'
+import path from 'node:path'
 import {program} from 'commander'
+import {$} from 'execa'
+import prettier from 'prettier'
+
+const PACKAGE_REGEX = /packages\/((([a-z]+)-?)+)/ // matches "packages/sui-any-package-name"
 
 program
   .name('publish-tagged-packages')
@@ -10,19 +15,91 @@ program
 
 program
   .option('-t, --tag [tag]', 'Tag used to publish the packages to NPM.')
-  .option('-f, --files [files]', 'JSON-stringified list of added and modified files.')
+  .option('-f, --files [files]', 'Comma-separated list of added and modified files.')
   .on('--help', () => {
     console.log('  Examples:')
     console.log('')
     console.log(
-      '    $ node ./scripts/publish-tagged-packages.mjs --tag \'ongoing-branch\' --files \'["packages/sui-mono/foo.js", "packages/sui-bundler/bar.js"]\''
+      '    $ node ./scripts/publish-tagged-packages.mjs --tag \'ongoing-branch\' --files \'"packages/sui-mono/foo.js","packages/sui-bundler/bar.js"\''
     )
     console.log('')
   })
   .parse(process.argv)
 
-const {tag, files} = program.opts()
+async function getConfig(fileName) {
+  return import(`../${fileName}/package.json`, {
+    assert: {type: 'json'}
+  }).then(module => module.default)
+}
 
-console.log({tag, files: JSON.parse(files)})
+async function getPackageVersion({name, tag}) {
+  let version = ''
+
+  try {
+    // Try to get a previously tagged version.
+    const {stdout: currentTaggedVersion} = await $`npm view ${name}@${tag} version`
+    const [mainVersion, taggedVersion] = currentTaggedVersion.split(`-${tag}.`)
+
+    version = `${mainVersion}-${tag}.${Number(taggedVersion) + 1}`
+  } catch (error) {
+    // If not, create a new tag from the main package version.
+    const {stdout: currentVersion} = await $`npm view ${name} version`
+    const [major, minor] = currentVersion.split('.')
+
+    version = `${major}.${Number(minor) + 1}.0-${tag}.0`
+  }
+
+  return version
+}
+
+async function publishPackages() {
+  const {tag, files = ''} = program.opts()
+
+  if (!tag) {
+    throw new Error('🤨 Tag is mandatory in order to publish new package versions.')
+  }
+
+  // Get needed packages to publish.
+  const packagesToPublish = [
+    // Avoid duplicates.
+    ...new Set(
+      files
+        .split(',')
+        // Filter only packages.
+        .filter(file => file.match(PACKAGE_REGEX))
+        // Return the package path.
+        .map(file => file.match(PACKAGE_REGEX)[0])
+    )
+  ]
+
+  if (!packagesToPublish.length) {
+    console.log('\n🐒 There is no new tags to be published.\n')
+    return
+  }
+
+  packagesToPublish.forEach(async packageName => {
+    const packageConfig = await getConfig(packageName)
+    const {name} = packageConfig
+    const version = await getPackageVersion({name, tag})
+
+    // Set the new tagged version.
+    packageConfig.version = version
+
+    const packagePath = path.join(process.cwd(), packageName)
+    const packageJsonPath = `${packagePath}/package.json`
+    const packageJson = await prettier.format(JSON.stringify(packageConfig), {parser: 'json'})
+
+    // Allow `execa` to execute NPM commands on the cwd of each package.
+    const $$ = $({cwd: packagePath})
+
+    console.log(`\n📦 Publishing new tagged version: ${name}@${version}\n`)
+
+    // fs.writeFileSync(packageJsonPath, packageJson)
+
+    // await $$`npm publish --tag ${tag}`
+  })
+}
+
+await publishPackages()
 
 program.parse(process.argv)
