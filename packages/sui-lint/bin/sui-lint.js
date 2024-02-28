e0fd01378a1f3c381d99802d1feb827d55f0eae3
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2024-02-28 16:15:01 +0100

    fix(packages/sui-lint): Fix lint

--- packages/sui-lint/bin/sui-lint.js
+++ packages/sui-lint/bin/sui-lint.js
@@ -4,6 +4,8 @@ const {version} = require('../package.json')
 
 program.version(version, '    --version')
 
-program.command('js', 'lint javascript files').command('sass', 'lint sass files').command('repository', 'lint repository structure')
+program.command('js', 'lint javascript files')
+  .command('sass', 'lint sass files')
+  .command('repository', 'lint repository structure')
 
 program.parse(process.argv)
