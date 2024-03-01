74751db22552649b6cfb94410c7e4be3194200ed
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2024-03-01 12:09:57 +0100

    test(packages/eslint-plugin-sui): Add more tests

--- packages/eslint-plugin-sui/tests/src/rules/factory-pattern.js
+++ packages/eslint-plugin-sui/tests/src/rules/factory-pattern.js
@@ -1,30 +1,41 @@
-/**
- * @fileoverview Ensure that our classes are using the convetion of has a static create method as factory.
- * @author factory pattern
- */
-'use strict'
-
-// ------------------------------------------------------------------------------
-// Requirements
-// ------------------------------------------------------------------------------
+import dedent from 'dedent'
 import {RuleTester} from 'eslint'
 
 import rule from '../../../src/rules/factory-pattern'
 
 // ------------------------------------------------------------------------------
 // Tests
+// more info: https://eslint.org/docs/latest/integrate/nodejs-api#ruletester
 // ------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester()
+const ruleTester = new RuleTester({parserOptions: {ecmaVersion: 6}})
 ruleTester.run('factory-pattern', rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      code: dedent`
+        class User {
+          static create() { return new User() }
+        }
+      `
+    }
   ],
 
   invalid: [
     {
-      code: "class Model { constructor() { this.name = 'John Doe' }  }",
-      errors: [{message: 'Fill me in.', type: 'Me too'}]
+      code: dedent`
+        class Model {
+          constructor() { this.name = 'John Doe' }
+        }`,
+      errors: [
+        {
+          message: dedent`
+            You have to define at least one static function that return an instance of your class.
+            Avoid to use the 'new' keyword directly in your code.
+            Use always a factory function
+        `,
+          type: 'ClassDeclaration'
+        }
+      ]
     }
   ]
 })
