c18ca58092cca64e4eff22c53ccfd4fef07af9b0
david.nieto <david.nieto@adevinta.com>
2024-03-14 16:05:06 +0100

    feat(packages/sui-bundler): add flag for creating libs with css chunks

--- packages/sui-bundler/webpack.config.lib.js
+++ packages/sui-bundler/webpack.config.lib.js
@@ -1,6 +1,11 @@
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const {cleanList, envVars, MAIN_ENTRY_POINT, config} = require('./shared/index.js')
+const {
+  cleanList,
+  envVars,
+  MAIN_ENTRY_POINT,
+  config
+} = require('./shared/index.js')
 const path = require('path')
 const minifyJs = require('./shared/minify-js.js')
 const definePlugin = require('./shared/define.js')
@@ -9,55 +14,64 @@ const sassRules = require('./shared/module-rules-sass.js')
 const {extractComments, sourceMap, supportLegacyBrowsers} = require('./shared/config.js')
 const {aliasFromConfig} = require('./shared/resolve-alias.js')
 
-const cssFileName = 'styles.css'
+const CWD = process.cwd()
+const PUBLIC_PATH = process.env.CDN || config.cdn || '/'
+const PWD = process.env.PWD ?? ''
 
-module.exports = {
-  mode: 'production',
-  resolve: {
-    alias: {
-      ...aliasFromConfig
+module.exports = ({
+  chunkCss
+} = {}) => {
+  const chunkCssName = config.onlyHash ? '[contenthash:8].css' : '[name].[contenthash:8].css'
+  const cssFileName = chunkCss ? chunkCssName : 'styles.css'
+  return {
+    mode: 'production',
+    context: path.resolve(CWD, 'src'),
+    resolve: {
+      alias: {
+        ...aliasFromConfig
+      },
+      fallback: {
+        assert: false,
+        fs: false,
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        path: false
+      },
+      extensions: ['.js', '.json'],
+      modules: ['node_modules', path.resolve(process.cwd())]
     },
-    fallback: {
-      assert: false,
-      fs: false,
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      path: false
-    },
-    extensions: ['.js', '.json'],
-    modules: ['node_modules', path.resolve(process.cwd())]
-  },
-  entry: config.vendor
-    ? {
+    entry: config.vendor ?
+      {
         app: MAIN_ENTRY_POINT,
         vendor: config.vendor
-      }
-    : MAIN_ENTRY_POINT,
-  target: 'web',
-  output: {
-    filename: 'index.js'
-  },
-  optimization: {
-    // avoid looping over all the modules after the compilation
-    checkWasmTypes: false,
-    minimize: true,
-    minimizer: [minifyJs({extractComments, sourceMap})]
-  },
-  plugins: cleanList([
-    new webpack.ProvidePlugin({
-      process: 'process/browser'
-    }),
-    new MiniCssExtractPlugin({
-      filename: cssFileName,
-      chunkFilename: cssFileName
-    }),
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    }),
-    new webpack.EnvironmentPlugin(envVars(config.env)),
-    definePlugin()
-  ]),
-  module: {
-    rules: [createBabelRules({supportLegacyBrowsers}), sassRules]
+      } :
+      MAIN_ENTRY_POINT,
+    target: 'web',
+    output: {
+      filename: 'index.js'
+    },
+    optimization: {
+      // avoid looping over all the modules after the compilation
+      checkWasmTypes: false,
+      minimize: true,
+      minimizer: [minifyJs({extractComments, sourceMap})]
+    },
+    plugins: cleanList([
+      new webpack.ProvidePlugin({
+        process: 'process/browser'
+      }),
+      new MiniCssExtractPlugin({
+        filename: cssFileName,
+        chunkFilename: cssFileName
+      }),
+      !chunkCss && new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1
+      }),
+      new webpack.EnvironmentPlugin(envVars(config.env)),
+      definePlugin()
+    ]),
+    module: {
+      rules: [createBabelRules({supportLegacyBrowsers}), sassRules]
+    }
   }
 }
