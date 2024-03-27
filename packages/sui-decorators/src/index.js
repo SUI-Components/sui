bc73cffd64b871aa42372c4e34d30d1fa5aaff22
Oriol Puig <oriol.puig@adevinta.com>
2024-03-27 12:14:41 +0100

    feat(packages/sui-decorators): Create AsyncInlineError decorator

--- packages/sui-decorators/src/index.js
+++ packages/sui-decorators/src/index.js
@@ -1,6 +1,7 @@
+import {AsyncInlineError} from './decorators/AsyncInlineError/index.js'
 import {cache, invalidateCache} from './decorators/cache/index.js'
 import inlineError from './decorators/error.js'
 import streamify from './decorators/streamify.js'
 import tracer from './decorators/tracer/index.js'
 
-export {cache, invalidateCache, streamify, inlineError, tracer}
+export {AsyncInlineError, cache, invalidateCache, streamify, inlineError, tracer}
