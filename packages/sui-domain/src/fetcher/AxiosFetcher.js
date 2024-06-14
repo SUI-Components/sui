416357e6b1e02c65fe11c136727806bb601090eb
jordi.munoz@adevinta.com <jordi.munoz@adevinta.com>
2024-06-14 10:51:20 +0200

    refactor(Root): refactor axios fetcher to make it work with circuit breaker

--- packages/sui-domain/src/fetcher/AxiosFetcher.js
+++ packages/sui-domain/src/fetcher/AxiosFetcher.js
@@ -9,14 +9,15 @@ export default class AxiosFetcher {
    */
   constructor({config}) {
     this._axios = axios.create(config)
+    this._config = config
   }
 
   getCircuitBreaker() {
-    return this._config.get('circuitBreaker')
-      ? this._config.get('circuitBreaker')
+    return this._config?.get && this._config?.get('circuitBreaker')
+      ? this._config?.get('circuitBreaker')
       : {
-          fire: (requester, url, options, body) => {
-            return requester.call(requester, url, body ?? options, options)
+          fire: (requester, verb, url, options, body) => {
+            return requester[verb].call(this, url, body ?? options, options)
           }
         }
   }
@@ -28,7 +29,7 @@ export default class AxiosFetcher {
    * @return {Promise<any>}
    */
   get(url, options) {
-    return this.getCircuitBreaker().fire(this._axios.get, url, options)
+    return this.getCircuitBreaker().fire(this._axios, 'get', url, options)
   }
 
   /**
@@ -40,7 +41,7 @@ export default class AxiosFetcher {
    * @return {Promise}
    */
   post(url, body, options) {
-    return this.getCircuitBreaker().fire(this._axios.post, url, options, body)
+    return this.getCircuitBreaker().fire(this._axios, 'post', url, options, body)
   }
 
   /**
@@ -52,7 +53,7 @@ export default class AxiosFetcher {
    * @return {Object}
    */
   put(url, body, options) {
-    return this.getCircuitBreaker().fire(this._axios.put, url, options, body)
+    return this.getCircuitBreaker().fire(this._axios, 'put', url, options, body)
   }
 
   /**
@@ -64,7 +65,7 @@ export default class AxiosFetcher {
    * @return {Object}
    */
   patch(url, body, options) {
-    return this.getCircuitBreaker().fire(this._axios.patch, url, options, body)
+    return this.getCircuitBreaker().fire(this._axios, 'patch', url, options, body)
   }
 
   /**
@@ -75,6 +76,6 @@ export default class AxiosFetcher {
    * @return {Object}
    */
   delete(url, options) {
-    return this.getCircuitBreaker().fire(this._axios.delete, url, options)
+    return this.getCircuitBreaker().fire(this._axios, 'delete', url, options)
   }
 }
