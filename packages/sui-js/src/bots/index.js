331619cbc28749df138cb48f884e7c912f159b09
Ivan Martinez <ivan.martinez@Macbook-Pro-M1-Ivan-Adevinta.local>
2024-06-20 08:45:25 +0200

    feat(packages/sui-js): typo

--- packages/sui-js/src/bots/index.js
+++ packages/sui-js/src/bots/index.js
@@ -1,4 +1,4 @@
-const LEGITIMATE_BOTS_USER_AGENTS = [
+const LEGITIMATE_BOT_USER_AGENTS = [
   'googlebot',
   'google-structured-data-testing-tool',
   'bingbot',
@@ -10,9 +10,9 @@ const LEGITIMATE_BOTS_USER_AGENTS = [
 const checkUserAgentIsBot = (userAgent, botsUserAgents) => {
   const lowerCaseUserAgent = userAgent.toLowerCase()
 
-  const botsUserAgentsList = botsUserAgents || LEGITIMATE_BOTS_USER_AGENTS
+  const botsUserAgentsList = botsUserAgents || LEGITIMATE_BOT_USER_AGENTS
 
   return botsUserAgentsList.some(ua => lowerCaseUserAgent.includes(ua))
 }
 
-export {checkUserAgentIsBot, LEGITIMATE_BOTS_USER_AGENTS}
+export {checkUserAgentIsBot, LEGITIMATE_BOT_USER_AGENTS}
