9ebc5aff5688330918be1db48a98b745031f3514
Oriol Puig <oriol.puig@adevinta.com>
2024-07-03 19:03:12 +0200

    feat(packages/sui-decorators): Create new @Deprecated() decorator

---
+++ packages/sui-decorators/test/server/DeprecatedSpec.js
@@ -0,0 +1,48 @@
+import {expect} from 'chai'
+import sinon from 'sinon'
+
+import {Deprecated} from '../../src/decorators/deprecated/index.js'
+
+describe('Deprecated decorator', () => {
+  beforeEach(() => {
+    sinon.stub(console, 'warn')
+    global.__SUI_DECORATOR_DEPRECATED_REPORTER__ = undefined
+  })
+
+  afterEach(() => {
+    sinon.restore()
+  })
+
+  it('should exist', () => {
+    expect(Deprecated).to.exist
+    expect(Deprecated).to.be.a('function')
+  })
+
+  it('should write a console.warn into console', async () => {
+    class Buzz {
+      @Deprecated({key: 'returnASuccessPromise', message: 'This method is deprecated'})
+      returnASuccessPromise() {
+        return Promise.resolve(true)
+      }
+    }
+
+    const buzz = new Buzz()
+    expect(await buzz.returnASuccessPromise()).to.be.eql(true)
+    expect(console.warn.calledOnce).to.be.true
+  })
+
+  it('should call to listener to enable monitoring', async () => {
+    global.__SUI_DECORATOR_DEPRECATED_REPORTER__ = sinon.spy()
+
+    class Buzz {
+      @Deprecated({key: 'returnASuccessPromise', message: 'This method is deprecated'})
+      returnASuccessPromise() {
+        return Promise.resolve(true)
+      }
+    }
+
+    const buzz = new Buzz()
+    expect(await buzz.returnASuccessPromise()).to.be.eql(true)
+    expect(global.__SUI_DECORATOR_DEPRECATED_REPORTER__.calledOnce).to.be.true
+  })
+})
