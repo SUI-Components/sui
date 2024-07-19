37e382f2783e560f13fafbf9d9a861dbd21b9a9d
aleix-ferrer <aleix.ferrer@adevinta.com>
2024-07-19 13:46:39 +0200

    feat(packages/sui-lint): add new rule to sui config

--- packages/sui-lint/eslintrc.js
+++ packages/sui-lint/eslintrc.js
@@ -239,7 +239,8 @@ module.exports = {
       rules: {
         'sui/factory-pattern': RULES.WARNING,
         'sui/serialize-deserialize': RULES.WARNING,
-        'sui/decorators': RULES.WARNING
+        'sui/decorators': RULES.WARNING,
+        'sui/private-attributes-model': RULES.WARNING
       }
     },
     {
