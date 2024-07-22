ec0e093cb428f7f39f94cdb491167425cd1d9ea6
aleix-ferrer <aleix.ferrer@adevinta.com>
2024-07-22 11:45:07 +0200

    feat(packages/eslint-plugin-sui): check if file is in VO/Entity folder

--- packages/eslint-plugin-sui/src/rules/validate-model.js
+++ packages/eslint-plugin-sui/src/rules/validate-model.js
@@ -4,6 +4,7 @@
 'use strict'
 
 const dedent = require('string-dedent')
+const path = require('path')
 
 // ------------------------------------------------------------------------------
 // Rule Definition
@@ -31,6 +32,16 @@ module.exports = {
   },
 
   create(context) {
+    const filePath = context.getFilename()
+    const relativePath = path.relative(context.getCwd(), filePath)
+
+    // Check if the file is inside requierd folders (useCases, services, repositories, ...)
+    const valueObjectPattern = /valueObjects|valueobjects|ValueObjects|Valueobjects/i
+    const isValueObjectPath = valueObjectPattern.test(relativePath)
+
+    const entityPattern = /entity|Entity/i
+    const isEntityPath = entityPattern.test(relativePath)
+
     return {
       ClassDeclaration(node) {
         const validateMethod = node.body.body.find(i => i.key.name === 'validate')
@@ -41,7 +52,8 @@ module.exports = {
 
         const isDomainModel = allowedWords.some(allowWord => className.includes(allowWord))
 
-        if (!isDomainModel) return // eslint-disable-line
+        if (!isDomainModelName && !isValueObjectPath) return
+        if (!isDomainModelName && !isEntityPath) return
 
         if (!validateMethod)
           return context.report({
