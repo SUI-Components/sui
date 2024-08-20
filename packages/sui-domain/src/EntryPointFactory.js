21eea778ce3dafdd4c398ea74e6d8212c8e6f583
Andrés Alvarez <a.alvarez.sor@gmail.com>
2024-08-20 10:57:20 +0200

    fix(packages/sui-domain): change default constructor

--- packages/sui-domain/src/EntryPointFactory.js
+++ packages/sui-domain/src/EntryPointFactory.js
@@ -13,13 +13,13 @@ export default ({useCases, config, logger, pde}) =>
   class EntryPoint {
     subscribers = {}
 
-    constructor(params = {config: {}, logger: {}, pde: {}}) {
+    constructor(params = {}) {
       // decide to use a static config from the factory
       // or use a config passed to the constructor that could be mutated
-      this._config = params.config || config
+      this._config = params.config || config || {}
       this._useCases = useCases
-      this._logger = params.logger || logger
-      this._pde = params.pde || pde
+      this._logger = params.logger || logger || {}
+      this._pde = params.pde || pde || {}
     }
 
     /**
