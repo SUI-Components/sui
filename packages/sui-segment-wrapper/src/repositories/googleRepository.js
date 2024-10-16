48d8b9e707a74586cec93544ec0a2136bb1892ae
Kiko Ruiz <kikoruiz@icloud.com>
2024-10-16 10:14:32 +0200

    feat(packages/sui-segment-wrapper): map campaign data

--- packages/sui-segment-wrapper/src/repositories/googleRepository.js
+++ packages/sui-segment-wrapper/src/repositories/googleRepository.js
@@ -1,8 +1,28 @@
+import {utils} from '../middlewares/source/pageReferrer.js'
 import {getConfig} from '../config.js'
 
 const FIELDS = {
   clientId: 'client_id',
   sessionId: 'session_id'
+}
+
+const STC = {
+  QUERY: 'stc',
+  SPLIT_SYMBOL: '-',
+  NAME_SPLIT_SYMBOL: ':'
+}
+
+const STC_MEDIUM_TRANSFORMATIONS = {
+  aff: 'affiliate',
+  dis: 'display',
+  em: 'email',
+  met: 'paid-metasearch',
+  sem: 'paid-search',
+  rt: 'display',
+  sm: 'social-media',
+  sp: 'paid-social',
+  pn: 'push-notification',
+  cs: 'cross-sites'
 }
 
 const cachedData = {
@@ -48,6 +68,28 @@ const getGoogleField = async field => {
       resolve(id)
     })
   })
+}
+
+export const getCampaignDetails = ({needsTransformation = true} = {}) => {
+  const search = utils.getActualQueryString()
+  const searchParams = new URLSearchParams(search)
+
+  if (!searchParams.has(STC.QUERY)) return null
+
+  const stc = searchParams.get(STC.QUERY)
+  const [medium, source, originalName, term, content] = stc.split(STC.SPLIT_SYMBOL)
+  const [id, name] = originalName.split(STC.NAME_SPLIT_SYMBOL)
+
+  return {
+    campaign: {
+      medium: (needsTransformation && STC_MEDIUM_TRANSFORMATIONS[medium]) || medium,
+      ...(typeof name !== 'undefined' && {id}),
+      name: name ?? id,
+      source,
+      term,
+      ...(typeof content !== 'undefined' && {content})
+    }
+  }
 }
 
 export const getGoogleClientID = () => getGoogleField(FIELDS.clientId)
