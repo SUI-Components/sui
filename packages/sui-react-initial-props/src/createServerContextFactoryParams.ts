c9307ebc058553267d6eeece54ad56474feb0558
Ferran Simon <ferran.simon@ferransimon-MacBook-Pro-N6YW.local>
2025-02-24 10:45:15 +0100

    feat(packages/sui-react-initial-props): Propagate server response

--- packages/sui-react-initial-props/src/createServerContextFactoryParams.ts
+++ packages/sui-react-initial-props/src/createServerContextFactoryParams.ts
@@ -1,8 +1,9 @@
 import {type ContextFactoryParams} from './types'
 
-export default (req: IncomingMessage.ServerRequest): ContextFactoryParams => ({
+export default (req: IncomingMessage.ServerRequest, res: IncomingMessage.ClientResponse): ContextFactoryParams => ({
   appConfig: req.appConfig,
   req,
+  res,
   cookies: req.headers.cookie,
   isClient: false,
   // that's a native Express path, we might consider use another one instead
