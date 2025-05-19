ac0fff6d31bdff1bf052aa923ae7186c70791bd0
Nacho Torrella <ignacio.torrella@adevinta.com>
2025-05-19 17:08:31 +0200

    feat(packages/sui-svg): refactor accessibility injection in AtomIcon component

--- packages/sui-svg/templates/icon-component.js
+++ packages/sui-svg/templates/icon-component.js
@@ -2,16 +2,16 @@ const template = content => {
   return `import {memo} from 'react'
 import AtomIcon from '@s-ui/react-atom-icon'
 
+const injectAccessibility = ({id, svg, title}) => {
+  if (!title || !id) return svg.replace(/<svg([^>]*)>/, '<svg$1 aria-hidden="true">')
+
+  return svg.replace(
+    /<svg([^>]*)>/,
+    \`<svg$1 role="img" aria-labelledby="\${id}"><title id="\${id}">\${title}</title>\`
+  )
+}
+
 const MemoAtomIcon = memo(({id, title, ...props}) => {
-  const injectAccessibility = ({id, svg, title}) => {
-    if (!title || !id) return svg.replace(/<svg([^>]*)>/, '<svg$1 aria-hidden="true">')
-
-    return svg.replace(
-      /<svg([^>]*)>/,
-      \`<svg$1 role="img" aria-labelledby="\${id}"><title id="\${id}">\${title}</title>\`
-    )
-  }
-
   const safeContent = injectAccessibility({id, svg: \`${content}\`, title})
 
   return (
