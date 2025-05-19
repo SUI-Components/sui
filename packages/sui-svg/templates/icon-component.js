b0d69ab557cf73edd23a2c2cd6cfbcbb6ea44430
Nacho Torrella <ignacio.torrella@adevinta.com>
2025-05-19 15:56:14 +0200

    feat(packages/sui-svg): enhance accessibility for AtomIcon component

--- packages/sui-svg/templates/icon-component.js
+++ packages/sui-svg/templates/icon-component.js
@@ -2,11 +2,24 @@ const template = content => {
   return `import {memo} from 'react'
 import AtomIcon from '@s-ui/react-atom-icon'
 
-const MemoAtomIcon = memo(props =>
-  <AtomIcon {...props}>
-    <span dangerouslySetInnerHTML={{ __html: '${content}' }} />
-  </AtomIcon>
-)
+const MemoAtomIcon = memo(({id, title, ...props}) => {
+  const injectAccessibility = ({id, svg, title}) => {
+    if (!title || !id) return svg.replace(/<svg([^>]*)>/, '<svg$1 aria-hidden="true">')
+
+    return svg.replace(
+      /<svg([^>]*)>/,
+      \`<svg$1 role="img" aria-labelledby="\${titleId}"><title id="\${titleId}">\${title}</title>\`
+    )
+  }
+
+  const safeContent = injectAccessibility({id, svg: \`${content}\`, title})
+
+  return (
+    <AtomIcon {...props}>
+      <span dangerouslySetInnerHTML={{__html: safeContent}} />
+    </AtomIcon>
+  )
+})
 
 MemoAtomIcon.displayName = 'AtomIcon'
 
