0ad6a78dfbfb650d69acae283a9161296403fc7f
Kiko Ruiz <kikoruiz@icloud.com>
2025-07-08 16:03:27 +0200

    feat(packages/sui-mockmock): allow to force server mocker usage

--- packages/sui-mockmock/src/http/index.js
+++ packages/sui-mockmock/src/http/index.js
@@ -2,5 +2,18 @@ import ClientMocker from './clientMocker'
 import ServerMocker from './serverMocker'
 
 const isNode = Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]'
+const useServerMocker = typeof process !== 'undefined' && process.env && process.env.FORCE_SERVER_MOCKER
 
-export default isNode ? ServerMocker : ClientMocker
+let Mocker
+
+if (useServerMocker === 'true') {
+  Mocker = ServerMocker
+} else if (useServerMocker === 'false') {
+  Mocker = ClientMocker
+} else if (isNode) {
+  Mocker = ServerMocker
+} else {
+  Mocker = ClientMocker
+}
+
+export default Mocker
