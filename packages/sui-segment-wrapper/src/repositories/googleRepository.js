351a90f856d27e72d348a46d7ee1bf462b1aa5c7
Manuel Javier Miguel <manuel.miguel@adevinta.com>
2025-08-28 12:00:24 +0200

    feat(packages/sui-segment-wrapper): Refactor segment-wrapper to allow UTM tags

--- packages/sui-segment-wrapper/src/repositories/googleRepository.js
+++ packages/sui-segment-wrapper/src/repositories/googleRepository.js
@@ -24,6 +24,14 @@ const STC = {
   SPLIT_SYMBOL: '-',
   CAMPAIGN_SPLIT_SYMBOL: ':'
 }
+
+const UTM_CAMPAIGN = 'utm_campaign'
+const UTM_MEDIUM = 'utm_medium'
+const UTM_SOURCE = 'utm_source'
+const UTM_CONTENT = 'utm_content'
+const UTM_CAMPAIGN_ID = 'utm_id'
+const UTM_TERM = 'utm_term'
+
 const STC_MEDIUM_TRANSFORMATIONS = {
   aff: 'affiliate',
   dis: 'display',
@@ -38,6 +46,8 @@ const STC_MEDIUM_TRANSFORMATIONS = {
 }
 const STC_INVALID_CONTENT = 'na'
 const DEFAULT_GA_INIT_EVENT = 'sui'
+
+const EMPTY_STC = {medium: null, source: null, campaign: null}
 
 const loadScript = async src =>
   new Promise(function (resolve, reject) {
@@ -71,6 +81,8 @@ const triggerGoogleAnalyticsInitEvent = sessionId => {
   if (!localStorage.getItem(eventKey)) {
     // If not, send it.
     window.gtag('event', eventName)
+
+    // eslint-disable-next-line no-console
     console.log(`Sending GA4 event "${eventName}" for the session "${sessionId}"`)
 
     // And then save a new GA session hit in local storage.
@@ -99,15 +111,9 @@ const getGoogleField = async field => {
 }
 
 export const getCampaignDetails = ({needsTransformation = true} = {}) => {
-  const search = utils.getActualQueryString()
-  const searchParams = new URLSearchParams(search)
+  const {medium, source, campaign, content, term} = readQueryParams()
 
-  if (!searchParams.has(STC.QUERY)) return null
-
-  const stc = searchParams.get(STC.QUERY)
-  const [medium, source, campaign, content, term] = stc.split(STC.SPLIT_SYMBOL)
-
-  if (!campaign) return null
+  if (!medium || !source || !campaign) return null
 
   const [id, name] = campaign.split(STC.CAMPAIGN_SPLIT_SYMBOL)
 
@@ -124,6 +130,49 @@ export const getCampaignDetails = ({needsTransformation = true} = {}) => {
       ...(needsContent && {content}),
       ...(typeof term !== 'undefined' && {term})
     }
+  }
+}
+
+function readQueryParams() {
+  const search = utils.getActualQueryString()
+  const searchParams = new URLSearchParams(search)
+
+  return getConfig('useUtm') ? readFromUtm(searchParams) : readFromStc(searchParams)
+}
+
+function readFromStc(searchParams) {
+  if (!hasStc(searchParams)) return EMPTY_STC
+
+  const stc = searchParams.get(STC.QUERY)
+  const [medium, source, campaign, content, term] = stc.split(STC.SPLIT_SYMBOL)
+  return {medium, source, campaign, content, term}
+}
+
+function hasStc(searchParams) {
+  return searchParams.has(STC.QUERY)
+}
+
+function readFromUtm(searchParams) {
+  const campaignId = searchParams.get(UTM_CAMPAIGN_ID)
+  const campaignName = searchParams.get(UTM_CAMPAIGN)
+  const medium = searchParams.get(UTM_MEDIUM)
+  const source = searchParams.get(UTM_SOURCE)
+  const content = searchParams.get(UTM_CONTENT)
+  const term = searchParams.get(UTM_TERM)
+  const campaign = campaignId ? `${campaignId}${STC.CAMPAIGN_SPLIT_SYMBOL}${campaignName}` : campaignName
+
+  // Fallback to STC if any mandatory UTM fields are missing
+  const missingAnyMandatoryFields = !medium || !source || !campaign
+  if (missingAnyMandatoryFields && hasStc(searchParams)) {
+    return readFromStc(searchParams)
+  }
+
+  return {
+    campaign,
+    medium,
+    source,
+    content,
+    term
   }
 }
 
