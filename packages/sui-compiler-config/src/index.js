2de4d3bea1f0fcbd2cff023ec45a79dd1f6ff1a1
Pablo Rey <pablo.rey@adevinta.com>
2026-03-26 22:30:48 +0100

    feat(packages/sui-compiler-config): add minify parameter defaulting to false

--- packages/sui-compiler-config/src/index.js
+++ packages/sui-compiler-config/src/index.js
@@ -10,13 +10,13 @@ const DEFAULT_BROWSER_TARGETS = {
   ios: '14.5'
 }
 
-const getSWCConfig = ({isModern = false, isTypeScript = false, compileToCJS = false}) => {
+const getSWCConfig = ({isModern = false, isTypeScript = false, compileToCJS = false, minify = false}) => {
   const targets = isModern ? DEFAULT_BROWSER_TARGETS : DEFAULT_LEGACY_BROWSER_TARGETS
   const syntaxOptions = isTypeScript ? {syntax: 'typescript', tsx: true} : {syntax: 'ecmascript', jsx: true}
   const moduleOptions = compileToCJS ? {module: {type: 'commonjs'}} : {}
 
   return {
-    minify: true,
+    minify,
     jsc: {
       parser: {
         ...syntaxOptions,
